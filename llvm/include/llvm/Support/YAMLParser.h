nekoshirro/Alchemist-LLVM

commit 8259638ebb2d9b87998e46fff44f7d283de38eee

Really fix -Wnon-virtual-dtor warnings; gcc needs the dtors to be explicitly marked as virtual.
llvm-svn: 154131
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/include/llvm/Support/YAMLParser.h
@@ -149,7 +149,7 @@ protected:
   OwningPtr<Document> &Doc;
   SMRange SourceRange;
 
-  ~Node();
+  virtual ~Node();
 
 private:
   unsigned int TypeID;
@@ -170,7 +170,7 @@ public:
   }
 
 protected:
-  ~NullNode();
+  virtual ~NullNode();
 };
 
 /// @brief A scalar node is an opaque datum that can be presented as a
@@ -206,7 +206,7 @@ public:
   }
 
 protected:
-  ~ScalarNode();
+  virtual ~ScalarNode();
 
 private:
   StringRef Value;
@@ -256,7 +256,7 @@ public:
   }
 
 protected:
-  ~KeyValueNode();
+  virtual ~KeyValueNode();
 
 private:
   Node *Key;
@@ -375,7 +375,7 @@ public:
   }
 
 protected:
-  ~MappingNode();
+  virtual ~MappingNode();
 
 private:
   MappingType Type;
@@ -441,7 +441,7 @@ public:
   }
 
 protected:
-  ~SequenceNode();
+  virtual ~SequenceNode();
 
 private:
   SequenceType SeqType;
@@ -469,7 +469,7 @@ public:
   }
 
 protected:
-  ~AliasNode();
+  virtual ~AliasNode();
 
 private:
   StringRef Name;

commit 0c2b76e9abe166d455f431db9958700aac4dc9d1

Fix the build breakage introduced by r154131.
The empty 1-argument operator delete is for the benefit of the
destructor. A couple of spot checks of running yaml-bench under
valgrind against a few of the files under test/YAMLParser did
not reveal any leaks introduced by this change.

llvm-svn: 154137
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/include/llvm/Support/YAMLParser.h
@@ -149,7 +149,9 @@ protected:
   OwningPtr<Document> &Doc;
   SMRange SourceRange;
 
-  virtual ~Node();
+  void operator delete(void *) throw() {}
+
+  virtual ~Node() {}
 
 private:
   unsigned int TypeID;
@@ -168,9 +170,6 @@ public:
   static inline bool classof(const Node *N) {
     return N->getType() == NK_Null;
   }
-
-protected:
-  virtual ~NullNode();
 };
 
 /// @brief A scalar node is an opaque datum that can be presented as a
@@ -204,9 +203,6 @@ public:
   static inline bool classof(const Node *N) {
     return N->getType() == NK_Scalar;
   }
-
-protected:
-  virtual ~ScalarNode();
 
 private:
   StringRef Value;
@@ -254,9 +250,6 @@ public:
   static inline bool classof(const Node *N) {
     return N->getType() == NK_KeyValue;
   }
-
-protected:
-  virtual ~KeyValueNode();
 
 private:
   Node *Key;
@@ -374,9 +367,6 @@ public:
     return N->getType() == NK_Mapping;
   }
 
-protected:
-  virtual ~MappingNode();
-
 private:
   MappingType Type;
   bool IsAtBeginning;
@@ -440,9 +430,6 @@ public:
     return N->getType() == NK_Sequence;
   }
 
-protected:
-  virtual ~SequenceNode();
-
 private:
   SequenceType SeqType;
   bool IsAtBeginning;
@@ -467,9 +454,6 @@ public:
   static inline bool classof(const Node *N) {
     return N->getType() == NK_Alias;
   }
-
-protected:
-  virtual ~AliasNode();
 
 private:
   StringRef Name;