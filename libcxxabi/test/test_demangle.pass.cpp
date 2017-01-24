2842f4857b50adde23404fe634b999218017c9bf
Saleem Abdulrasool <compnerd@compnerd.org>
2017-01-24 19:57:05 +0000

    cxa_demangle: fix rvalue ref check
    When checking if the type is a r-value ref, we would not do a complete
    check.  This would result in us treating a trailing parameter reference
    `&)` as a r-value ref, and improperly inject the cv qualifier on the
    type.  We now correctly demangle the type `KFvRmE` as a constant
    function rather than a constant reference.
    
    Fixes PR31741!
    
    llvm-svn: 292973
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- libcxxabi/test/test_demangle.pass.cpp
+++ libcxxabi/test/test_demangle.pass.cpp
@@ -29598,9 +29598,8 @@ const char* cases[][2] =
     // mangled names can include type manglings too, which don't start with _Z:
     {"i", "int"},
 
-    // FIXME(compnerd) this should be void (int &) const
-    {"PKFvRiE", "void (*)(int const&)"},
-    // TODO(compnerd) pretty print this as void (*)(unsigned long&) volatile &&"
+    {"PKFvRiE", "void (*)(int&) const"},
+    // FIXME(compnerd) pretty print this as void (*)(unsigned long &) volatile &&
     {"PVFvRmOE", "void (*)(unsigned long&)  volatile&&"},
     {"PFvRmOE", "void (*)(unsigned long&) &&"},
 };
