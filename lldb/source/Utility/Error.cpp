nekoshirro/Alchemist-LLVM

commit e6f7fa0a4500d347b5b4bbcce0c6dcd9b6cddec5

Finish breaking the dependency from Utility.
Differential Revision: https://reviews.llvm.org/D29964

llvm-svn: 295368
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Utility/Error.cpp
@@ -20,8 +20,8 @@
 #include "llvm/ADT/SmallVector.h"
 
 // Project includes
-#include "lldb/Host/PosixApi.h"
 #include "lldb/Utility/Error.h"
+#include "lldb/Utility/VASPrintf.h"
 
 using namespace lldb;
 using namespace lldb_private;
@@ -233,25 +233,9 @@ int Error::SetErrorStringWithVarArg(const char *format, va_list args) {
     if (Success())
       SetErrorToGenericError();
 
-    // Try and fit our error into a 1024 byte buffer first...
-    llvm::SmallVector<char, 1024> buf;
-    buf.resize(1024);
-    // Copy in case our first call to vsnprintf doesn't fit into our
-    // allocated buffer above
-    va_list copy_args;
-    va_copy(copy_args, args);
-    unsigned length = ::vsnprintf(buf.data(), buf.size(), format, args);
-    if (length >= buf.size()) {
-      // The error formatted string didn't fit into our buffer, resize it
-      // to the exact needed size, and retry
-      buf.resize(length + 1);
-      length = ::vsnprintf(buf.data(), buf.size(), format, copy_args);
-      va_end(copy_args);
-      assert(length < buf.size());
-    }
-    m_string.assign(buf.data(), length);
-    va_end(args);
-    return length;
+    llvm::SmallString<1024> buf;
+    VASprintf(buf, format, args);
+    return buf.size();
   } else {
     m_string.clear();
   }

lldb/unittests/Utility/CMakeLists.txt
@@ -4,9 +4,9 @@ add_lldb_unittest(UtilityTests
   TaskPoolTest.cpp
   TimeoutTest.cpp
   UriParserTest.cpp
+  VASprintfTest.cpp
 
   LINK_LIBS
-      lldbHost
       lldbUtility
     LINK_COMPONENTS
       Support

lldb/source/Host/CMakeLists.txt
@@ -27,6 +27,7 @@ add_host_subdirectory(common
   common/OptionParser.cpp
   common/PipeBase.cpp
   common/ProcessRunLock.cpp
+  common/PseudoTerminal.cpp
   common/Socket.cpp
   common/SocketAddress.cpp
   common/SoftwareBreakpoint.cpp

lldb/source/Utility/CMakeLists.txt
@@ -4,7 +4,6 @@ add_lldb_library(lldbUtility
   JSON.cpp
   LLDBAssert.cpp
   NameMatches.cpp
-  PseudoTerminal.cpp
   Range.cpp
   RegularExpression.cpp
   SelectHelper.cpp
@@ -16,9 +15,10 @@ add_lldb_library(lldbUtility
   StringLexer.cpp
   TaskPool.cpp
   UriParser.cpp
+  VASprintf.cpp
 
   LINK_LIBS
-    lldbHost
+  # lldbUtility cannot have any dependencies
 
   LINK_COMPONENTS
     Support

lldb/include/lldb/Host/PseudoTerminal.h
@@ -7,8 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef liblldb_PseudoTerminal_h_
-#define liblldb_PseudoTerminal_h_
+#ifndef LLDB_HOST_PSEUDOTERMINAL_H
+#define LLDB_HOST_PSEUDOTERMINAL_H
 #if defined(__cplusplus)
 
 #include <fcntl.h>
@@ -19,7 +19,7 @@
 namespace lldb_utility {
 
 //----------------------------------------------------------------------
-/// @class PseudoTerminal PseudoTerminal.h "lldb/Utility/PseudoTerminal.h"
+/// @class PseudoTerminal PseudoTerminal.h "lldb/Host/PseudoTerminal.h"
 /// @brief A pseudo terminal helper class.
 ///
 /// The pseudo terminal class abstracts the use of pseudo terminals on

lldb/include/lldb/Interpreter/ScriptInterpreter.h
@@ -22,7 +22,7 @@
 #include "lldb/Core/StructuredData.h"
 #include "lldb/Utility/Error.h"
 
-#include "lldb/Utility/PseudoTerminal.h"
+#include "lldb/Host/PseudoTerminal.h"
 
 namespace lldb_private {
 

lldb/include/lldb/Target/ProcessLaunchInfo.h
@@ -18,9 +18,9 @@
 
 #include "lldb/Host/FileSpec.h"
 #include "lldb/Host/Host.h"
+#include "lldb/Host/PseudoTerminal.h"
 #include "lldb/Target/FileAction.h"
 #include "lldb/Target/ProcessInfo.h"
-#include "lldb/Utility/PseudoTerminal.h"
 
 namespace lldb_private {
 

lldb/include/lldb/Utility/VASPrintf.h
@@ -1,0 +1,21 @@
+//===-- VASPrintf.h ---------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLDB_UTILITY_VASPRINTF_H
+#define LLDB_UTILITY_VASPRINTF_H
+
+#include "llvm/ADT/SmallVector.h"
+
+#include <cstdarg>
+
+namespace lldb_private {
+bool VASprintf(llvm::SmallVectorImpl<char> &buf, const char *fmt, va_list args);
+}
+
+#endif // #ifdef LLDB_UTILITY_VASPRINTF_H

commit 0f2b5791b222f538d6ffda825bb09fbce0becfec

Fix breakage caused by r295368
Also move the ErrorTest into the Utility package, to follow the class it
is testing.

llvm-svn: 295436
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Utility/Error.cpp
@@ -235,6 +235,7 @@ int Error::SetErrorStringWithVarArg(const char *format, va_list args) {
 
     llvm::SmallString<1024> buf;
     VASprintf(buf, format, args);
+    m_string = buf.str();
     return buf.size();
   } else {
     m_string.clear();

lldb/unittests/Core/CMakeLists.txt
@@ -2,7 +2,6 @@ add_lldb_unittest(LLDBCoreTests
   ArchSpecTest.cpp
   BroadcasterTest.cpp
   DataExtractorTest.cpp
-  ErrorTest.cpp
   ListenerTest.cpp
   LogTest.cpp
   ScalarTest.cpp

lldb/unittests/Utility/CMakeLists.txt
@@ -1,5 +1,6 @@
 add_lldb_unittest(UtilityTests
   ConstStringTest.cpp
+  ErrorTest.cpp
   StringExtractorTest.cpp
   TaskPoolTest.cpp
   TimeoutTest.cpp
@@ -8,6 +9,6 @@ add_lldb_unittest(UtilityTests
 
   LINK_LIBS
       lldbUtility
-    LINK_COMPONENTS
-      Support
+  LINK_COMPONENTS
+    Support
   )