e339bc20562b5623022d83472944025489151cce
Weiming Zhao <weimingz@codeaurora.org>
2017-03-11 20:53:01 +0000

    Revert "[Builtin] Implement lit-test support"
    Due to test failure of check-builtins for aarch64 and armhf.
    This reverts commit r297566.
    
    llvm-svn: 297569
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- compiler-rt/test/builtins/Unit/udivmodti4_test.c
+++ compiler-rt/test/builtins/Unit/udivmodti4_test.c
@@ -1,4 +1,3 @@
-// RUN: %clang_builtins %s %librt -o %t && %run %t
 //===-- udivmodti4_test.c - Test __udivmodti4 -----------------------------===//
 //
 //                     The LLVM Compiler Infrastructure
