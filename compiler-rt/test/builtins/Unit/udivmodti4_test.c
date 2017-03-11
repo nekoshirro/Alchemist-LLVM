f70a0fb3affac6f91f2a471e60105d805f3a134e
Weiming Zhao <weimingz@codeaurora.org>
2017-03-11 19:40:24 +0000

    [Builtin] Implement lit-test support
    Summary:
    This patch implements a initial support of lit test for builtins.
    Unit/arm/call_apsr.S is updated to support thumb1.
    It also fixes a bug in arm/aeabi_uldivmod_test.c
    gcc_personality_test is XFAILED as the framework cannot handle it so far.
    cpu_model_test is also XFAILED for now as it is expected to return non-zero.
    
    Reviewers: rengolin, compnerd, jroelofs, erik.pilkington, arphaman
    
    Reviewed By: jroelofs
    
    Subscribers: jroelofs, aemerson, srhines, nemanjai, llvm-commits, mgorny
    
    Differential Revision: https://reviews.llvm.org/D30802
    
    llvm-svn: 297566
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- compiler-rt/test/builtins/Unit/udivmodti4_test.c
+++ compiler-rt/test/builtins/Unit/udivmodti4_test.c
@@ -1,3 +1,4 @@
+// RUN: %clang_builtins %s %librt -o %t && %run %t
 //===-- udivmodti4_test.c - Test __udivmodti4 -----------------------------===//
 //
 //                     The LLVM Compiler Infrastructure
