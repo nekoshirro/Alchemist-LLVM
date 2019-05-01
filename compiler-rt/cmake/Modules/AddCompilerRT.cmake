nekoshirro/Alchemist-LLVM

commit 68f89ea4f29ca809e48bd4fa6f0f2e34644994d3

[compiler-rt] Pass sysroot and disable pedantic for crtbegin.o/crtend.o
These are needed to make bots happy.

Differential Revision: https://reviews.llvm.org/D61363

llvm-svn: 359646
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

compiler-rt/cmake/Modules/AddCompilerRT.cmake
@@ -276,7 +276,10 @@ function(add_compiler_rt_runtime name type)
     if(type STREQUAL "OBJECT")
       get_property(cflags_${libname} SOURCE ${sources_${libname}} PROPERTY COMPILE_FLAGS)
       if(CMAKE_C_COMPILER_ID MATCHES Clang AND CMAKE_C_COMPILER_TARGET)
-        list(APPEND cflags_${libname} "-target ${CMAKE_C_COMPILER_TARGET}")
+        list(APPEND cflags_${libname} "--target=${CMAKE_C_COMPILER_TARGET}")
+      endif()
+      if(CMAKE_SYSROOT)
+        list(APPEND cflags_${libname} "--sysroot=${CMAKE_SYSROOT}")
       endif()
       string(REPLACE ";" " " extra_cflags_${libname} "${extra_cflags_${libname}}")
       string(REGEX MATCHALL "<[A-Za-z0-9_]*>" substitutions

compiler-rt/cmake/config-ix.cmake
@@ -100,6 +100,7 @@ check_cxx_compiler_flag("-Werror -Wnon-virtual-dtor"   COMPILER_RT_HAS_WNON_VIRT
 check_cxx_compiler_flag("-Werror -Wvariadic-macros"    COMPILER_RT_HAS_WVARIADIC_MACROS_FLAG)
 check_cxx_compiler_flag("-Werror -Wunused-parameter"   COMPILER_RT_HAS_WUNUSED_PARAMETER_FLAG)
 check_cxx_compiler_flag("-Werror -Wcovered-switch-default" COMPILER_RT_HAS_WCOVERED_SWITCH_DEFAULT_FLAG)
+check_cxx_compiler_flag(-Wno-pedantic COMPILER_RT_HAS_WNO_PEDANTIC)
 
 check_cxx_compiler_flag(/W4 COMPILER_RT_HAS_W4_FLAG)
 check_cxx_compiler_flag(/WX COMPILER_RT_HAS_WX_FLAG)

compiler-rt/lib/crt/CMakeLists.txt
@@ -73,6 +73,7 @@ check_cxx_section_exists(".init_array" COMPILER_RT_HAS_INITFINI_ARRAY
 
 append_list_if(COMPILER_RT_HAS_INITFINI_ARRAY -DCRT_HAS_INITFINI_ARRAY CRT_CFLAGS)
 append_list_if(COMPILER_RT_HAS_FPIC_FLAG -fPIC CRT_CFLAGS)
+append_list_if(COMPILER_RT_HAS_NO_PEDANTIC -Wno-pedantic CRT_CFLAGS)
 
 foreach(arch ${CRT_SUPPORTED_ARCH})
   add_compiler_rt_runtime(clang_rt.crtbegin

commit 172f56103aeaa512baaa5c26b82e6c00db9f5e38

[compiler-rt] Cleanup the --target and --sysroot handling
This addresses issue introduced in r359646.

llvm-svn: 359650
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

compiler-rt/cmake/Modules/AddCompilerRT.cmake
@@ -274,12 +274,11 @@ function(add_compiler_rt_runtime name type)
     endif()
 
     if(type STREQUAL "OBJECT")
-      get_property(cflags_${libname} SOURCE ${sources_${libname}} PROPERTY COMPILE_FLAGS)
       if(CMAKE_C_COMPILER_ID MATCHES Clang AND CMAKE_C_COMPILER_TARGET)
-        list(APPEND cflags_${libname} "--target=${CMAKE_C_COMPILER_TARGET}")
+        list(APPEND extra_cflags_${libname} "--target=${CMAKE_C_COMPILER_TARGET}")
       endif()
       if(CMAKE_SYSROOT)
-        list(APPEND cflags_${libname} "--sysroot=${CMAKE_SYSROOT}")
+        list(APPEND extra_cflags_${libname} "--sysroot=${CMAKE_SYSROOT}")
       endif()
       string(REPLACE ";" " " extra_cflags_${libname} "${extra_cflags_${libname}}")
       string(REGEX MATCHALL "<[A-Za-z0-9_]*>" substitutions
@@ -297,7 +296,7 @@ function(add_compiler_rt_runtime name type)
           string(REPLACE "<SOURCE>" "${sources_${libname}}"
                  compile_command_${libname} ${compile_command_${libname}})
         elseif(substitution STREQUAL "<FLAGS>")
-          string(REPLACE "<FLAGS>" "${CMAKE_C_FLAGS} ${cflags_${libname}} ${extra_cflags_${libname}}"
+          string(REPLACE "<FLAGS>" "${CMAKE_C_FLAGS} ${extra_cflags_${libname}}"
                  compile_command_${libname} ${compile_command_${libname}})
         else()
           string(REPLACE "${substitution}" "" compile_command_${libname}