nekoshirro/Alchemist-LLVM

commit 6d2ca36a758db4a391319f2919addac9ba30d128

[profile] Don't publish VMO if there are no counters
If there are no counters, there's no need to publish the VMO.

Differential Revision: https://reviews.llvm.org/D102786
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

compiler-rt/lib/profile/InstrProfilingPlatformFuchsia.c
@@ -126,13 +126,20 @@ void __llvm_profile_initialize(void) {
 
   const __llvm_profile_data *DataBegin = __llvm_profile_begin_data();
   const __llvm_profile_data *DataEnd = __llvm_profile_end_data();
+  const uint64_t *CountersBegin = __llvm_profile_begin_counters();
+  const uint64_t *CountersEnd = __llvm_profile_end_counters();
   const uint64_t DataSize = __llvm_profile_get_data_size(DataBegin, DataEnd);
   const uint64_t CountersOffset =
       sizeof(__llvm_profile_header) + (DataSize * sizeof(__llvm_profile_data));
+  uint64_t CountersSize = CountersEnd - CountersBegin;
+
+  /* Don't publish a VMO if there are no counters. */
+  if (!CounterSize)
+    return;
 
   zx_status_t Status;
 
-  /* Create VMO to hold the profile data. */
+  /* Create a VMO to hold the profile data. */
   zx_handle_t Vmo = ZX_HANDLE_INVALID;
   Status = _zx_vmo_create(0, ZX_VMO_RESIZABLE, &Vmo);
   if (Status != ZX_OK) {

commit 93d44f0d7f1ab235301fc7553e2d6d7844e56121

[profile] Fix variable name
This fixes a bug introduced in 6d2ca36a758db4a391319f2919addac9ba30d128.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

compiler-rt/lib/profile/InstrProfilingPlatformFuchsia.c
@@ -134,7 +134,7 @@ void __llvm_profile_initialize(void) {
   uint64_t CountersSize = CountersEnd - CountersBegin;
 
   /* Don't publish a VMO if there are no counters. */
-  if (!CounterSize)
+  if (!CountersSize)
     return;
 
   zx_status_t Status;