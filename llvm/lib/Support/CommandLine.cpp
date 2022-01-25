nekoshirro/Alchemist-LLVM

commit f632428cbde8f9766e196798e8659e23ac27fc3d

[NFC] Use an llvm::DenseMap instead of std::map in CategorizedHelpPrinter::printOptions Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Support/CommandLine.cpp
@@ -45,7 +45,6 @@
 #include "llvm/Support/VirtualFileSystem.h"
 #include "llvm/Support/raw_ostream.h"
 #include <cstdlib>
-#include <map>
 #include <string>
 using namespace llvm;
 using namespace cl;
@@ -2339,7 +2338,7 @@ public:
 protected:
   void printOptions(StrOptionPairVector &Opts, size_t MaxArgLen) override {
     std::vector<OptionCategory *> SortedCategories;
-    std::map<OptionCategory *, std::vector<Option *>> CategorizedOptions;
+    DenseMap<OptionCategory *, std::vector<Option *>> CategorizedOptions;
 
     // Collect registered option categories into vector in preparation for
     // sorting.
@@ -2350,10 +2349,6 @@ protected:
     assert(SortedCategories.size() > 0 && "No option categories registered!");
     array_pod_sort(SortedCategories.begin(), SortedCategories.end(),
                    OptionCategoryCompare);
-
-    // Create map to empty vectors.
-    for (OptionCategory *Category : SortedCategories)
-      CategorizedOptions[Category] = std::vector<Option *>();
 
     // Walk through pre-sorted options and assign into categories.
     // Because the options are already alphabetically sorted the

commit 9569ceb5b75873975f769d57b07396cc63fc8d2f

Fix build issue in assert mode introduced by f632428cbde8f9766e1
After f632428cbde8f9766e1, one should use SortedCategories to check category
validity.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Support/CommandLine.cpp
@@ -2356,7 +2356,9 @@ protected:
     for (size_t I = 0, E = Opts.size(); I != E; ++I) {
       Option *Opt = Opts[I].second;
       for (auto &Cat : Opt->Categories) {
-        assert(CategorizedOptions.count(Cat) > 0 &&
+        assert(std::binary_search(SortedCategories.begin(),
+                                  SortedCategories.end(), Cat,
+                                  OptionCategoryCompare) &&
                "Option has an unregistered category");
         CategorizedOptions[Cat].push_back(Opt);
       }