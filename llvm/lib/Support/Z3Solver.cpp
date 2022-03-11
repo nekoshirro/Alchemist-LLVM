nekoshirro/Alchemist-LLVM

commit 0da60d9311ee1af9ca1e4eb380ff2109b47a922c

Conditional include of missing headers under Z3 Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,10 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
+#include "llvm/ADT/SmalLString.h"
 #include "llvm/ADT/Twine.h"
+
+#include <set>
 
 #include <z3.h>
 

commit 20fd3d54828234173ee4f74ab6c08797499f13a1

[analyzer] Fix include typo introduced by 0da60d9 Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,7 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
-#include "llvm/ADT/SmalLString.h"
+#include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/Twine.h"
 
 #include <set>