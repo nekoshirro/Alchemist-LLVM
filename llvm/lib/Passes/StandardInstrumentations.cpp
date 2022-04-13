nekoshirro/Alchemist-LLVM

commit 2494f101b2cbacf88a4f6ed97d49820a9a730061

[NFC] Cleanup StandardInstrumentations Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Passes/StandardInstrumentations.cpp
@@ -440,19 +440,11 @@ const Module *getModuleForComparison(Any IR) {
   return nullptr;
 }
 
-} // namespace
-
-template <typename T> ChangeReporter<T>::~ChangeReporter() {
-  assert(BeforeStack.empty() && "Problem with Change Printer stack.");
-}
-
-template <typename T>
-bool ChangeReporter<T>::isInterestingFunction(const Function &F) {
+bool isInterestingFunction(const Function &F) {
   return isFunctionInPrintList(F.getName());
 }
 
-template <typename T>
-bool ChangeReporter<T>::isInterestingPass(StringRef PassID) {
+bool isInterestingPass(StringRef PassID) {
   if (isIgnored(PassID))
     return false;
 
@@ -463,13 +455,18 @@ bool ChangeReporter<T>::isInterestingPass(StringRef PassID) {
 
 // Return true when this is a pass on IR for which printing
 // of changes is desired.
-template <typename T>
-bool ChangeReporter<T>::isInteresting(Any IR, StringRef PassID) {
+bool isInteresting(Any IR, StringRef PassID) {
   if (!isInterestingPass(PassID))
     return false;
   if (any_isa<const Function *>(IR))
     return isInterestingFunction(*any_cast<const Function *>(IR));
   return true;
+}
+
+} // namespace
+
+template <typename T> ChangeReporter<T>::~ChangeReporter<T>() {
+  assert(BeforeStack.empty() && "Problem with Change Printer stack.");
 }
 
 template <typename T>

llvm/include/llvm/Passes/StandardInstrumentations.h
@@ -187,17 +187,6 @@ protected:
   // Register required callbacks.
   void registerRequiredCallbacks(PassInstrumentationCallbacks &PIC);
 
-  // Return true when this is a defined function for which printing
-  // of changes is desired.
-  bool isInterestingFunction(const Function &F);
-
-  // Return true when this is a pass for which printing of changes is desired.
-  bool isInterestingPass(StringRef PassID);
-
-  // Return true when this is a pass on IR for which printing
-  // of changes is desired.
-  bool isInteresting(Any IR, StringRef PassID);
-
   // Called on the first IR processed.
   virtual void handleInitialIR(Any IR) = 0;
   // Called before and after a pass to get the representation of the IR.

commit 1ae6da42e1f352d2baed0e0ff6397f5bc152cbd2

[NFC] Fix build failure with GCC 11 in C++20 mode
This was already fixed in
https://github.com/llvm/llvm-project/commit/87aaf8932904b4390ab8c40ef3e368a5662acee6
but then regressed in
https://github.com/llvm/llvm-project/commit/2494f101b2cbacf88a4f6ed97d49820a9a730061

Reviewed By: aeubanks

Differential Revision: https://reviews.llvm.org/D123589
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Passes/StandardInstrumentations.cpp
@@ -465,7 +465,7 @@ bool isInteresting(Any IR, StringRef PassID) {
 
 } // namespace
 
-template <typename T> ChangeReporter<T>::~ChangeReporter<T>() {
+template <typename T> ChangeReporter<T>::~ChangeReporter() {
   assert(BeforeStack.empty() && "Problem with Change Printer stack.");
 }
 