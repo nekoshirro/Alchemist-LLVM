nekoshirro/Alchemist-LLVM

commit 8b5f5c398d983a6b2290f0582b0ca947359a7a51

[LLDB][NFC] Decouple dwarf location table from DWARFExpression.
Differential Revision: https://reviews.llvm.org/D125509
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Expression/DWARFExpressionList.cpp
@@ -1,0 +1,216 @@
+//===-- DWARFExpressionList.cpp -------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "lldb/Expression/DWARFExpressionList.h"
+#include "Plugins/SymbolFile/DWARF/DWARFUnit.h"
+#include "lldb/Symbol/Function.h"
+#include "lldb/Target/RegisterContext.h"
+#include "lldb/Target/StackFrame.h"
+#include "llvm/DebugInfo/DWARF/DWARFDebugLoc.h"
+#include "llvm/DebugInfo/DWARF/DWARFFormValue.h"
+
+using namespace lldb;
+using namespace lldb_private;
+
+bool DWARFExpressionList::IsAlwaysValidSingleExpr() const {
+  if (m_exprs.GetSize() != 1)
+    return false;
+  const Entry expr = m_exprs.GetEntryRef(0);
+  return expr.base == 0 && expr.size == LLDB_INVALID_ADDRESS;
+}
+
+bool DWARFExpressionList::AddExpression(addr_t base, addr_t end,
+                                        DWARFExpression expr) {
+  if (IsAlwaysValidSingleExpr() || base >= end)
+    return false;
+  m_exprs.Append({base, end - base, expr});
+  return true;
+}
+
+bool DWARFExpressionList::GetExpressionData(DataExtractor &data,
+                                            lldb::addr_t file_addr) const {
+  if (const DWARFExpression *expr = GetExpressionAtAddress(file_addr))
+    return expr->GetExpressionData(data);
+  return false;
+}
+
+bool DWARFExpressionList::ContainsAddress(lldb::addr_t file_addr) const {
+  return m_exprs.FindEntryThatContains(file_addr) != nullptr;
+}
+
+const DWARFExpression *
+DWARFExpressionList::GetExpressionAtAddress(lldb::addr_t file_addr) const {
+  uint32_t index = m_exprs.FindEntryIndexThatContains(file_addr);
+  if (index == UINT32_MAX)
+    return nullptr;
+  return &m_exprs.GetEntryAtIndex(index)->data;
+}
+
+DWARFExpression *
+DWARFExpressionList::GetMutableExpressionAtAddress(lldb::addr_t file_addr) {
+  uint32_t index = m_exprs.FindEntryIndexThatContains(file_addr);
+  if (index == UINT32_MAX)
+    return nullptr;
+  return &m_exprs.GetMutableEntryAtIndex(index)->data;
+}
+
+bool DWARFExpressionList::ContainsThreadLocalStorage() const {
+  // We are assuming for now that any thread local variable will not have a
+  // location list. This has been true for all thread local variables we have
+  // seen so far produced by any compiler.
+  if (!IsAlwaysValidSingleExpr())
+    return false;
+
+  const DWARFExpression &expr = m_exprs.GetEntryRef(0).data;
+  return expr.ContainsThreadLocalStorage();
+}
+
+bool DWARFExpressionList::LinkThreadLocalStorage(
+    lldb::ModuleSP new_module_sp,
+    std::function<lldb::addr_t(lldb::addr_t file_addr)> const
+        &link_address_callback) {
+  // We are assuming for now that any thread local variable will not have a
+  // location list. This has been true for all thread local variables we have
+  // seen so far produced by any compiler.
+  if (!IsAlwaysValidSingleExpr())
+    return false;
+
+  DWARFExpression &expr = m_exprs.GetEntryRef(0).data;
+  // If we linked the TLS address correctly, update the module so that when the
+  // expression is evaluated it can resolve the file address to a load address
+  // and read the TLS data
+  if (expr.LinkThreadLocalStorage(link_address_callback))
+    m_module_wp = new_module_sp;
+  return true;
+}
+
+bool DWARFExpressionList::MatchesOperand(
+    StackFrame &frame, const Instruction::Operand &operand) const {
+  RegisterContextSP reg_ctx_sp = frame.GetRegisterContext();
+  if (!reg_ctx_sp) {
+    return false;
+  }
+  const DWARFExpression *expr = nullptr;
+  if (IsAlwaysValidSingleExpr())
+    expr = &m_exprs.GetEntryAtIndex(0)->data;
+  else {
+    SymbolContext sc = frame.GetSymbolContext(eSymbolContextFunction);
+    if (!sc.function)
+      return false;
+
+    addr_t load_function_start =
+        sc.function->GetAddressRange().GetBaseAddress().GetFileAddress();
+    if (load_function_start == LLDB_INVALID_ADDRESS)
+      return false;
+
+    addr_t pc = frame.GetFrameCodeAddressForSymbolication().GetFileAddress();
+    expr = GetExpressionAtAddress(pc);
+  }
+  if (!expr)
+    return false;
+  return expr->MatchesOperand(frame, operand);
+}
+
+bool DWARFExpressionList::DumpLocations(Stream *s, lldb::DescriptionLevel level,
+                                        lldb::addr_t file_addr,
+                                        ABI *abi) const {
+  llvm::raw_ostream &os = s->AsRawOstream();
+  llvm::ListSeparator separator;
+  for (const Entry &entry : *this) {
+    if (file_addr != LLDB_INVALID_ADDRESS &&
+        (file_addr < entry.GetRangeBase() || file_addr >= entry.GetRangeEnd()))
+      continue;
+    const auto &expr = entry.data;
+    DataExtractor data;
+    expr.GetExpressionData(data);
+    uint32_t addr_size = data.GetAddressByteSize();
+    if (entry.GetRangeEnd() == LLDB_INVALID_ADDRESS) {
+      expr.DumpLocation(s, level, abi);
+      break;
+    }
+
+    os << separator;
+    os << "[";
+    os << llvm::format_hex(entry.GetRangeBase(), 2 + 2 * addr_size);
+    os << ", ";
+    os << llvm::format_hex(entry.GetRangeEnd(), 2 + 2 * addr_size);
+    os << ") -> ";
+    expr.DumpLocation(s, level, abi);
+    if (file_addr != LLDB_INVALID_ADDRESS)
+      break;
+  }
+  return true;
+}
+
+void DWARFExpressionList::GetDescription(Stream *s,
+                                         lldb::DescriptionLevel level,
+                                         ABI *abi) const {
+  llvm::raw_ostream &os = s->AsRawOstream();
+  if (IsAlwaysValidSingleExpr()) {
+    m_exprs.Back()->data.DumpLocation(s, level, abi);
+    return;
+  }
+  os << llvm::format("0x%8.8" PRIx64 ": ", 0);
+  for (const Entry &entry : *this) {
+    const auto &expr = entry.data;
+    DataExtractor data;
+    expr.GetExpressionData(data);
+    uint32_t addr_size = data.GetAddressByteSize();
+    os << "\n";
+    os.indent(s->GetIndentLevel() + 2);
+    os << "[";
+    llvm::DWARFFormValue::dumpAddress(os, addr_size, entry.GetRangeBase());
+    os << ", ";
+    llvm::DWARFFormValue::dumpAddress(os, addr_size, entry.GetRangeEnd());
+    os << "): ";
+    expr.DumpLocation(s, level, abi);
+  }
+}
+
+bool DWARFExpressionList::Evaluate(ExecutionContext *exe_ctx,
+                                   RegisterContext *reg_ctx,
+                                   const Value *initial_value_ptr,
+                                   const Value *object_address_ptr,
+                                   Value &result, Status *error_ptr) const {
+  ModuleSP module_sp = m_module_wp.lock();
+  DataExtractor data;
+  RegisterKind reg_kind;
+  DWARFExpression expr;
+  if (IsAlwaysValidSingleExpr()) {
+    expr = m_exprs.Back()->data;
+  } else {
+    Address pc;
+    StackFrame *frame = nullptr;
+    if (!reg_ctx || !reg_ctx->GetPCForSymbolication(pc)) {
+      if (exe_ctx)
+        frame = exe_ctx->GetFramePtr();
+      if (!frame)
+        return false;
+      RegisterContextSP reg_ctx_sp = frame->GetRegisterContext();
+      if (!reg_ctx_sp)
+        return false;
+      reg_ctx_sp->GetPCForSymbolication(pc);
+    }
+
+    if (!pc.IsValid()) {
+      if (error_ptr)
+        error_ptr->SetErrorString("Invalid PC in frame.");
+      return false;
+    }
+    addr_t addr = pc.GetFileAddress();
+    const auto *entry = m_exprs.FindEntryThatContains(addr);
+    if (!entry)
+      return false;
+    expr = entry->data;
+  }
+  expr.GetExpressionData(data);
+  reg_kind = expr.GetRegisterKind();
+  return DWARFExpression::Evaluate(exe_ctx, reg_ctx, module_sp, data,
+                                   m_dwarf_cu, reg_kind, initial_value_ptr,
+                                   object_address_ptr, result, error_ptr);
+}

lldb/include/lldb/Expression/DWARFExpression.h
@@ -42,48 +42,13 @@ public:
   /// \param[in] data
   ///     A data extractor configured to read the DWARF location expression's
   ///     bytecode.
-  DWARFExpression(lldb::ModuleSP module, const DataExtractor &data,
-                  const DWARFUnit *dwarf_cu);
+  DWARFExpression(const DataExtractor &data);
 
   /// Destructor
   virtual ~DWARFExpression();
 
-  /// Print the description of the expression to a stream
-  ///
-  /// \param[in] s
-  ///     The stream to print to.
-  ///
-  /// \param[in] level
-  ///     The level of verbosity to use.
-  ///
-  /// \param[in] abi
-  ///     An optional ABI plug-in that can be used to resolve register
-  ///     names.
-  void GetDescription(Stream *s, lldb::DescriptionLevel level, ABI *abi) const;
-
   /// Return true if the location expression contains data
   bool IsValid() const;
-
-  /// Return true if a location list was provided
-  bool IsLocationList() const;
-
-  /// Search for a load address in the location list
-  ///
-  /// \param[in] func_load_addr
-  ///     The actual address of the function containing this location list.
-  ///
-  /// \param[in] addr
-  ///     The address to resolve
-  ///
-  /// \return
-  ///     True if IsLocationList() is true and the address was found;
-  ///     false otherwise.
-  //    bool
-  //    LocationListContainsLoadAddress (Process* process, const Address &addr)
-  //    const;
-  //
-  bool LocationListContainsAddress(lldb::addr_t func_load_addr,
-                                   lldb::addr_t addr) const;
 
   /// If a location is not a location list, return true if the location
   /// contains a DW_OP_addr () opcode in the stream that matches \a file_addr.
@@ -92,6 +57,9 @@ public:
   /// NOT a location list). This helps us detect if a variable is a global or
   /// static variable since there is no other indication from DWARF debug
   /// info.
+  ///
+  /// \param[in] dwarf_cu
+  ///     The dwarf unit this expression belongs to.
   ///
   /// \param[in] op_addr_idx
   ///     The DW_OP_addr index to retrieve in case there is more than
@@ -104,56 +72,28 @@ public:
   /// \return
   ///     LLDB_INVALID_ADDRESS if the location doesn't contain a
   ///     DW_OP_addr for \a op_addr_idx, otherwise a valid file address
-  lldb::addr_t GetLocation_DW_OP_addr(uint32_t op_addr_idx, bool &error) const;
+  lldb::addr_t GetLocation_DW_OP_addr(const DWARFUnit *dwarf_cu,
+                                      uint32_t op_addr_idx, bool &error) const;
 
   bool Update_DW_OP_addr(lldb::addr_t file_addr);
 
   void UpdateValue(uint64_t const_value, lldb::offset_t const_value_byte_size,
                    uint8_t addr_byte_size);
 
-  void SetModule(const lldb::ModuleSP &module) { m_module_wp = module; }
-
   bool ContainsThreadLocalStorage() const;
 
   bool LinkThreadLocalStorage(
-      lldb::ModuleSP new_module_sp,
       std::function<lldb::addr_t(lldb::addr_t file_addr)> const
           &link_address_callback);
 
-  /// Tells the expression that it refers to a location list.
-  ///
-  /// \param[in] cu_file_addr
-  ///     The base address to use for interpreting relative location list
-  ///     entries.
-  /// \param[in] func_file_addr
-  ///     The file address of the function containing this location list. This
-  ///     address will be used to relocate the location list on the fly (in
-  ///     conjuction with the func_load_addr arguments).
-  void SetLocationListAddresses(lldb::addr_t cu_file_addr,
-                                lldb::addr_t func_file_addr);
-
   /// Return the call-frame-info style register kind
-  int GetRegisterKind();
+  lldb::RegisterKind GetRegisterKind() const;
 
   /// Set the call-frame-info style register kind
   ///
   /// \param[in] reg_kind
   ///     The register kind.
   void SetRegisterKind(lldb::RegisterKind reg_kind);
-
-  /// Wrapper for the static evaluate function that accepts an
-  /// ExecutionContextScope instead of an ExecutionContext and uses member
-  /// variables to populate many operands
-  bool Evaluate(ExecutionContextScope *exe_scope, lldb::addr_t func_load_addr,
-                const Value *initial_value_ptr, const Value *object_address_ptr,
-                Value &result, Status *error_ptr) const;
-
-  /// Wrapper for the static evaluate function that uses member variables to
-  /// populate many operands
-  bool Evaluate(ExecutionContext *exe_ctx, RegisterContext *reg_ctx,
-                lldb::addr_t loclist_base_load_addr,
-                const Value *initial_value_ptr, const Value *object_address_ptr,
-                Value &result, Status *error_ptr) const;
 
   /// Evaluate a DWARF location expression in a particular context
   ///
@@ -194,72 +134,32 @@ public:
   ///     True on success; false otherwise.  If error_ptr is non-NULL,
   ///     details of the failure are provided through it.
   static bool Evaluate(ExecutionContext *exe_ctx, RegisterContext *reg_ctx,
-                       lldb::ModuleSP opcode_ctx, const DataExtractor &opcodes,
+                       lldb::ModuleSP module_sp, const DataExtractor &opcodes,
                        const DWARFUnit *dwarf_cu,
                        const lldb::RegisterKind reg_set,
                        const Value *initial_value_ptr,
                        const Value *object_address_ptr, Value &result,
                        Status *error_ptr);
 
+  static bool ParseDWARFLocationList(const DWARFUnit *dwarf_cu,
+                                     const DataExtractor &data,
+                                     DWARFExpressionList *loc_list);
+
   bool GetExpressionData(DataExtractor &data) const {
     data = m_data;
     return data.GetByteSize() > 0;
   }
 
-  bool DumpLocationForAddress(Stream *s, lldb::DescriptionLevel level,
-                              lldb::addr_t func_load_addr, lldb::addr_t address,
-                              ABI *abi);
+  void DumpLocation(Stream *s, lldb::DescriptionLevel level, ABI *abi) const;
 
-  bool DumpLocations(Stream *s, lldb::DescriptionLevel level,
-                     lldb::addr_t func_load_addr, lldb::addr_t addr, ABI *abi);
-
-  bool GetLocationExpressions(
-      lldb::addr_t load_function_start,
-      llvm::function_ref<bool(llvm::DWARFLocationExpression)> callback) const;
-
-  bool MatchesOperand(StackFrame &frame, const Instruction::Operand &op);
-
-  llvm::Optional<DataExtractor>
-  GetLocationExpression(lldb::addr_t load_function_start,
-                        lldb::addr_t addr) const;
+  bool MatchesOperand(StackFrame &frame, const Instruction::Operand &op) const;
 
 private:
-  /// Pretty-prints the location expression to a stream
-  ///
-  /// \param[in] s
-  ///     The stream to use for pretty-printing.
-  ///
-  /// \param[in] data
-  ///     The data extractor.
-  ///
-  /// \param[in] level
-  ///     The level of detail to use in pretty-printing.
-  ///
-  /// \param[in] abi
-  ///     An optional ABI plug-in that can be used to resolve register
-  ///     names.
-  void DumpLocation(Stream *s, const DataExtractor &data,
-                    lldb::DescriptionLevel level, ABI *abi) const;
-
-  /// Module which defined this expression.
-  lldb::ModuleWP m_module_wp;
-
   /// A data extractor capable of reading opcode bytes
   DataExtractor m_data;
 
-  /// The DWARF compile unit this expression belongs to. It is used to evaluate
-  /// values indexing into the .debug_addr section (e.g. DW_OP_GNU_addr_index,
-  /// DW_OP_GNU_const_index)
-  const DWARFUnit *m_dwarf_cu = nullptr;
-
   /// One of the defines that starts with LLDB_REGKIND_
   lldb::RegisterKind m_reg_kind = lldb::eRegisterKindDWARF;
-
-  struct LoclistAddresses {
-    lldb::addr_t cu_file_addr;
-    lldb::addr_t func_file_addr;
-  };
-  llvm::Optional<LoclistAddresses> m_loclist_addresses;
 };
 
 } // namespace lldb_private

lldb/include/lldb/Expression/DWARFExpressionList.h
@@ -1,0 +1,132 @@
+//===-- DWARFExpressionList.h -----------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLDB_EXPRESSION_DWARFEXPRESSIONLIST_H
+#define LLDB_EXPRESSION_DWARFEXPRESSIONLIST_H
+
+#include "lldb/Expression/DWARFExpression.h"
+#include "lldb/Utility/RangeMap.h"
+#include "lldb/lldb-private.h"
+#include "llvm/ADT/Optional.h"
+
+class DWARFUnit;
+
+namespace lldb_private {
+
+/// \class DWARFExpressionList DWARFExpressionList.h
+/// "lldb/Expression/DWARFExpressionList.h" Encapsulates a range map from file
+/// address range to a single DWARF location expression.
+class DWARFExpressionList {
+public:
+  DWARFExpressionList() = default;
+
+  DWARFExpressionList(lldb::ModuleSP module_sp, const DWARFUnit *dwarf_cu)
+      : m_module_wp(module_sp), m_dwarf_cu(dwarf_cu) {}
+
+  DWARFExpressionList(lldb::ModuleSP module_sp, DWARFExpression expr,
+                      const DWARFUnit *dwarf_cu)
+      : DWARFExpressionList(module_sp, dwarf_cu) {
+    AddExpression(0, LLDB_INVALID_ADDRESS, expr);
+  }
+
+  /// Return true if the location expression contains data
+  bool IsValid() const { return !m_exprs.IsEmpty(); }
+
+  void Clear() { m_exprs.Clear(); }
+
+  // Return true if the location expression is always valid.
+  bool IsAlwaysValidSingleExpr() const;
+
+  bool AddExpression(lldb::addr_t base, lldb::addr_t end, DWARFExpression expr);
+
+  /// Get the expression data at the file address.
+  bool GetExpressionData(DataExtractor &data, lldb::addr_t file_addr = 0) const;
+
+  /// Sort m_expressions.
+  void Sort() { m_exprs.Sort(); }
+
+  const DWARFExpression *GetExpressionAtAddress(lldb::addr_t file_addr) const;
+
+  const DWARFExpression *GetAlwaysValidExpr() const {
+    return GetExpressionAtAddress(0);
+  }
+
+  DWARFExpression *GetMutableExpressionAtAddress(lldb::addr_t file_addr = 0);
+
+  size_t GetSize() const { return m_exprs.GetSize(); }
+
+  bool ContainsThreadLocalStorage() const;
+
+  bool LinkThreadLocalStorage(
+      lldb::ModuleSP new_module_sp,
+      std::function<lldb::addr_t(lldb::addr_t file_addr)> const
+          &link_address_callback);
+
+  bool MatchesOperand(StackFrame &frame,
+                      const Instruction::Operand &operand) const;
+
+  /// Dump locations that contains file_addr if it's valid. Otherwise. dump all
+  /// locations.
+  bool DumpLocations(Stream *s, lldb::DescriptionLevel level,
+                     lldb::addr_t file_addr, ABI *abi) const;
+
+  /// Dump all locaitons with each seperated by new line.
+  void GetDescription(Stream *s, lldb::DescriptionLevel level, ABI *abi) const;
+
+  /// Search for a load address in the location list
+  ///
+  /// \param[in] file_addr
+  ///     The file address to resolve
+  ///
+  /// \return
+  ///     True if IsLocationList() is true and the address was found;
+  ///     false otherwise.
+  //    bool
+  //    LocationListContainsLoadAddress (Process* process, const Address &addr)
+  //    const;
+  //
+  bool ContainsAddress(lldb::addr_t file_addr) const;
+
+  void SetModule(const lldb::ModuleSP &module) { m_module_wp = module; }
+
+  bool Evaluate(ExecutionContext *exe_ctx, RegisterContext *reg_ctx,
+                const Value *initial_value_ptr, const Value *object_address_ptr,
+                Value &result, Status *error_ptr) const;
+
+private:
+  // RangeDataVector requires a comparator for DWARFExpression, but it doesn't
+  // make sense to do so.
+  struct DWARFExpressionCompare {
+  public:
+    bool operator()(const DWARFExpression &lhs,
+                    const DWARFExpression &rhs) const {
+      return false;
+    }
+  };
+  using ExprVec = RangeDataVector<lldb::addr_t, lldb::addr_t, DWARFExpression,
+                                  0, DWARFExpressionCompare>;
+  using Entry = ExprVec::Entry;
+
+  // File address range mapping to single dwarf expression.
+  ExprVec m_exprs;
+
+  /// Module which defined this expression.
+  lldb::ModuleWP m_module_wp;
+
+  /// The DWARF compile unit this expression belongs to. It is used to evaluate
+  /// values indexing into the .debug_addr section (e.g. DW_OP_GNU_addr_index,
+  /// DW_OP_GNU_const_index)
+  const DWARFUnit *m_dwarf_cu = nullptr;
+
+  using const_iterator = ExprVec::Collection::const_iterator;
+  const_iterator begin() const { return m_exprs.begin(); }
+  const_iterator end() const { return m_exprs.end(); }
+};
+} // namespace lldb_private
+
+#endif // LLDB_EXPRESSION_DWARFEXPRESSIONLIST_H

lldb/include/lldb/Symbol/Function.h
@@ -12,7 +12,7 @@
 #include "lldb/Core/AddressRange.h"
 #include "lldb/Core/Declaration.h"
 #include "lldb/Core/Mangled.h"
-#include "lldb/Expression/DWARFExpression.h"
+#include "lldb/Expression/DWARFExpressionList.h"
 #include "lldb/Symbol/Block.h"
 #include "lldb/Utility/UserID.h"
 #include "llvm/ADT/ArrayRef.h"
@@ -253,8 +253,8 @@ class Function;
 /// Represent the locations of a parameter at a call site, both in the caller
 /// and in the callee.
 struct CallSiteParameter {
-  DWARFExpression LocationInCallee;
-  DWARFExpression LocationInCaller;
+  DWARFExpressionList LocationInCallee;
+  DWARFExpressionList LocationInCaller;
 };
 
 /// A vector of \c CallSiteParameter.
@@ -370,7 +370,7 @@ class IndirectCallEdge : public CallEdge {
 public:
   /// Construct a call edge using a DWARFExpression to identify the callee, and
   /// a return PC within the calling function to identify a specific call site.
-  IndirectCallEdge(DWARFExpression call_target, AddrType caller_address_type,
+  IndirectCallEdge(DWARFExpressionList call_target, AddrType caller_address_type,
                    lldb::addr_t caller_address, bool is_tail_call,
                    CallSiteParameterArray &&parameters)
       : CallEdge(caller_address_type, caller_address, is_tail_call,
@@ -383,7 +383,7 @@ private:
   // Used to describe an indirect call.
   //
   // Specifies the location of the callee address in the calling frame.
-  DWARFExpression call_target;
+  DWARFExpressionList call_target;
 };
 
 /// \class Function Function.h "lldb/Symbol/Function.h"
@@ -521,13 +521,13 @@ public:
   /// \return
   ///     A location expression that describes the function frame
   ///     base.
-  DWARFExpression &GetFrameBaseExpression() { return m_frame_base; }
+  DWARFExpressionList &GetFrameBaseExpression() { return m_frame_base; }
 
   /// Get const accessor for the frame base location.
   ///
   /// \return
   ///     A const compile unit object pointer.
-  const DWARFExpression &GetFrameBaseExpression() const { return m_frame_base; }
+  const DWARFExpressionList &GetFrameBaseExpression() const { return m_frame_base; }
 
   ConstString GetName() const;
 
@@ -659,7 +659,7 @@ protected:
 
   /// The frame base expression for variables that are relative to the frame
   /// pointer.
-  DWARFExpression m_frame_base;
+  DWARFExpressionList m_frame_base;
 
   Flags m_flags;
 

lldb/include/lldb/Symbol/Variable.h
@@ -11,7 +11,7 @@
 
 #include "lldb/Core/Declaration.h"
 #include "lldb/Core/Mangled.h"
-#include "lldb/Expression/DWARFExpression.h"
+#include "lldb/Expression/DWARFExpressionList.h"
 #include "lldb/Utility/CompletionRequest.h"
 #include "lldb/Utility/RangeMap.h"
 #include "lldb/Utility/UserID.h"
@@ -32,8 +32,8 @@ public:
   Variable(lldb::user_id_t uid, const char *name, const char *mangled,
            const lldb::SymbolFileTypeSP &symfile_type_sp, lldb::ValueType scope,
            SymbolContextScope *owner_scope, const RangeList &scope_range,
-           Declaration *decl, const DWARFExpression &location, bool external,
-           bool artificial, bool location_is_constant_data,
+           Declaration *decl, const DWARFExpressionList &location,
+           bool external, bool artificial, bool location_is_constant_data,
            bool static_member = false);
 
   virtual ~Variable();
@@ -73,9 +73,11 @@ public:
 
   bool IsStaticMember() const { return m_static_member; }
 
-  DWARFExpression &LocationExpression() { return m_location; }
+  DWARFExpressionList &LocationExpressionList() { return m_location_list; }
 
-  const DWARFExpression &LocationExpression() const { return m_location; }
+  const DWARFExpressionList &LocationExpressionList() const {
+    return m_location_list;
+  }
 
   // When given invalid address, it dumps all locations. Otherwise it only dumps
   // the location that contains this address.
@@ -128,7 +130,7 @@ protected:
   Declaration m_declaration;
   /// The location of this variable that can be fed to
   /// DWARFExpression::Evaluate().
-  DWARFExpression m_location;
+  DWARFExpressionList m_location_list;
   /// Visible outside the containing compile unit?
   unsigned m_external : 1;
   /// Non-zero if the variable is not explicitly declared in source.

lldb/include/lldb/Target/StackFrame.h
@@ -202,7 +202,7 @@ public:
   ///   frames may be unable to provide this value; they will return false.
   bool GetFrameBaseValue(Scalar &value, Status *error_ptr);
 
-  /// Get the DWARFExpression corresponding to the Canonical Frame Address.
+  /// Get the DWARFExpressionList corresponding to the Canonical Frame Address.
   ///
   /// Often a register (bp), but sometimes a register + offset.
   ///
@@ -212,7 +212,7 @@ public:
   ///
   /// \return
   ///   Returns the corresponding DWARF expression, or NULL.
-  DWARFExpression *GetFrameBaseExpression(Status *error_ptr);
+  DWARFExpressionList *GetFrameBaseExpression(Status *error_ptr);
 
   /// Get the current lexical scope block for this StackFrame, if possible.
   ///

lldb/include/lldb/Utility/RangeMap.h
@@ -627,6 +627,10 @@ public:
     return (m_entries.empty() ? nullptr : &m_entries.back());
   }
 
+  using const_iterator = typename Collection::const_iterator;
+  const_iterator begin() const { return m_entries.begin(); }
+  const_iterator end() const { return m_entries.end(); }
+
 protected:
   Collection m_entries;
   Compare m_compare;

lldb/include/lldb/lldb-forward.h
@@ -66,6 +66,7 @@ class ConstStringTable;
 class DWARFCallFrameInfo;
 class DWARFDataExtractor;
 class DWARFExpression;
+class DWARFExpressionList;
 class DataBuffer;
 class WritableDataBuffer;
 class DataBufferHeap;

commit 11090ce5f32fc62f2168778a1afc86b482eaffda

[LLDB] Fix aggregate-indirect-arg.cpp failure introduced by 8b5f5c398d983a6b2290f0582b0ca947359a7a51 Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Expression/DWARFExpressionList.cpp
@@ -204,8 +204,11 @@ bool DWARFExpressionList::Evaluate(ExecutionContext *exe_ctx,
     }
     addr_t addr = pc.GetFileAddress();
     const auto *entry = m_exprs.FindEntryThatContains(addr);
-    if (!entry)
+    if (!entry) {
+      if (error_ptr)
+        error_ptr->SetErrorString("variable not available");
       return false;
+    }
     expr = entry->data;
   }
   expr.GetExpressionData(data);