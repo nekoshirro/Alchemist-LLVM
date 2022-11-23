nekoshirro/Alchemist-LLVM

commit 268b25435242561e1e434f7dacda707c63419218

Add the ability to see when a type in incomplete.
-flimit-debug-info and other compiler options might end up removing debug info that is needed for debugging. LLDB marks these types as being forcefully completed in the metadata in the TypeSystem. These types should have been complete in the debug info but were not because the compiler omitted them to save space. When we can't find a suitable replacement for the type, we should let the user know that these types are incomplete to indicate there was an issue instead of just showing nothing for a type.

The solution is to display presented in this patch is to display "<incomplete type>" as the summary for any incomplete types. If there is a summary string or function that is provided for a type, but the type is currently forcefully completed, the installed summary will be ignored and we will display "<incomplete type>". This patch also exposes the ability to ask a SBType if it was forcefully completed with:

  bool SBType::IsTypeForcefullyCompleted();

This will allow the user interface for a debugger to also detect this issue and possibly mark the variable display up on some way to indicate to the user the type is incomplete.

To show how this is diplayed, we can look at the existing output first for the example source file from the file: lldb/test/API/functionalities/limit-debug-info/main.cpp

(lldb) frame variable inherits_from_one inherits_from_two one_as_member two_as_member array_of_one array_of_two shadowed_one
(InheritsFromOne) ::inherits_from_one = (member = 47)
(InheritsFromTwo) ::inherits_from_two = (member = 47)
(OneAsMember) ::one_as_member = (one = member::One @ 0x0000000100008028, member = 47)
(TwoAsMember) ::two_as_member = (two = member::Two @ 0x0000000100008040, member = 47)
(array::One [3]) ::array_of_one = ([0] = array::One @ 0x0000000100008068, [1] = array::One @ 0x0000000100008069, [2] = array::One @ 0x000000010000806a)

lldb/source/Symbol/CompilerType.cpp
@@ -94,6 +94,12 @@ bool CompilerType::IsCompleteType() const {
   return false;
 }
 
+bool CompilerType::IsForcefullyCompleted() const {
+  if (IsValid())
+    return m_type_system->IsForcefullyCompleted(m_type);
+  return false;
+}
+
 bool CompilerType::IsConst() const {
   if (IsValid())
     if (auto type_system_sp = GetTypeSystem())

lldb/include/lldb/Symbol/CompilerType.h
@@ -203,6 +203,8 @@ public:
   bool GetCompleteType() const;
   /// \}
 
+  bool IsForcefullyCompleted() const;
+
   /// AST related queries.
   /// \{
   size_t GetPointerByteSize() const;

lldb/include/lldb/Symbol/TypeSystem.h
@@ -202,6 +202,10 @@ public:
 
   virtual bool GetCompleteType(lldb::opaque_compiler_type_t type) = 0;
 
+  virtual bool IsForcefullyCompleted(lldb::opaque_compiler_type_t type) {
+    return false;
+  }
+
   // AST related queries
 
   virtual uint32_t GetPointerByteSize() = 0;

lldb/source/API/SBType.cpp
@@ -490,7 +490,12 @@ bool SBType::IsTypeComplete() {
 
   if (!IsValid())
     return false;
-  return m_opaque_sp->GetCompilerType(false).IsCompleteType();
+  CompilerType compiler_type = m_opaque_sp->GetCompilerType(false);
+  // Only return true if we have a complete type and it wasn't forcefully
+  // completed.
+  if (compiler_type.IsCompleteType())
+    return !compiler_type.IsForcefullyCompleted();
+  return false;
 }
 
 uint32_t SBType::GetTypeFlags() {

lldb/source/Core/ValueObject.cpp
@@ -594,6 +594,14 @@ bool ValueObject::GetSummaryAsCString(TypeSummaryImpl *summary_ptr,
                                       const TypeSummaryOptions &options) {
   destination.clear();
 
+  // If we have a forcefully completed type, don't try and show a summary from
+  // a valid summary string or function because the type is not complete and
+  // no member variables or member functions will be available.
+  if (GetCompilerType().IsForcefullyCompleted()) {
+      destination = "<incomplete type>";
+      return true;
+  }
+
   // ideally we would like to bail out if passing NULL, but if we do so we end
   // up not providing the summary for function pointers anymore
   if (/*summary_ptr == NULL ||*/ m_flags.m_is_getting_summary)

lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -1802,6 +1802,17 @@ bool TypeSystemClang::RecordHasFields(const RecordDecl *record_decl) {
         return true;
     }
   }
+
+  // We always want forcefully completed types to show up so we can print a
+  // message in the summary that indicates that the type is incomplete.
+  // This will help users know when they are running into issues with
+  // -flimit-debug-info instead of just seeing nothing if this is a base class
+  // (since we were hiding empty base classes), or nothing when you turn open
+  // an valiable whose type was incomplete.
+  ClangASTMetadata *meta_data = GetMetadata(record_decl);
+  if (meta_data && meta_data->IsForcefullyCompleted())
+    return true;
+
   return false;
 }
 
@@ -1829,7 +1840,7 @@ CompilerType TypeSystemClang::CreateObjCClass(
   return GetType(ast.getObjCInterfaceType(decl));
 }
 
-static inline bool BaseSpecifierIsEmpty(const CXXBaseSpecifier *b) {
+bool TypeSystemClang::BaseSpecifierIsEmpty(const CXXBaseSpecifier *b) {
   return !TypeSystemClang::RecordHasFields(b->getType()->getAsCXXRecordDecl());
 }
 
@@ -6608,9 +6619,10 @@ CompilerType TypeSystemClang::GetChildCompilerTypeAtIndex(
   return CompilerType();
 }
 
-static uint32_t GetIndexForRecordBase(const clang::RecordDecl *record_decl,
-                                      const clang::CXXBaseSpecifier *base_spec,
-                                      bool omit_empty_base_classes) {
+uint32_t TypeSystemClang::GetIndexForRecordBase(
+    const clang::RecordDecl *record_decl,
+    const clang::CXXBaseSpecifier *base_spec,
+    bool omit_empty_base_classes) {
   uint32_t child_idx = 0;
 
   const clang::CXXRecordDecl *cxx_record_decl =
@@ -6635,9 +6647,9 @@ static uint32_t GetIndexForRecordBase(const clang::RecordDecl *record_decl,
   return UINT32_MAX;
 }
 
-static uint32_t GetIndexForRecordChild(const clang::RecordDecl *record_decl,
-                                       clang::NamedDecl *canonical_decl,
-                                       bool omit_empty_base_classes) {
+uint32_t TypeSystemClang::GetIndexForRecordChild(
+    const clang::RecordDecl *record_decl, clang::NamedDecl *canonical_decl,
+    bool omit_empty_base_classes) {
   uint32_t child_idx = TypeSystemClang::GetNumBaseClasses(
       llvm::dyn_cast<clang::CXXRecordDecl>(record_decl),
       omit_empty_base_classes);
@@ -10033,4 +10045,20 @@ TypeSystemClang &ScratchTypeSystemClang::GetIsolatedAST(
                                               m_triple, CreateASTSource());
   m_isolated_asts.insert({feature, new_ast_sp});
   return *new_ast_sp;
+}
+
+bool TypeSystemClang::IsForcefullyCompleted(lldb::opaque_compiler_type_t type) {
+  if (type) {
+    clang::QualType qual_type(GetQualType(type));
+    const clang::RecordType *record_type =
+        llvm::dyn_cast<clang::RecordType>(qual_type.getTypePtr());
+    if (record_type) {
+      const clang::RecordDecl *record_decl = record_type->getDecl();
+      assert(record_decl);
+      ClangASTMetadata *metadata = GetMetadata(record_decl);
+      if (metadata)
+        return metadata->IsForcefullyCompleted();
+    }
+  }
+  return false;
 }

lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.h
@@ -303,8 +303,16 @@ public:
   static clang::AccessSpecifier
   UnifyAccessSpecifiers(clang::AccessSpecifier lhs, clang::AccessSpecifier rhs);
 
-  static uint32_t GetNumBaseClasses(const clang::CXXRecordDecl *cxx_record_decl,
-                                    bool omit_empty_base_classes);
+  uint32_t GetNumBaseClasses(const clang::CXXRecordDecl *cxx_record_decl,
+                             bool omit_empty_base_classes);
+
+  uint32_t GetIndexForRecordChild(const clang::RecordDecl *record_decl,
+                                  clang::NamedDecl *canonical_decl,
+                                  bool omit_empty_base_classes);
+
+  uint32_t GetIndexForRecordBase(const clang::RecordDecl *record_decl,
+                                 const clang::CXXBaseSpecifier *base_spec,
+                                 bool omit_empty_base_classes);
 
   /// Synthesize a clang::Module and return its ID or a default-constructed ID.
   OptionalClangModuleID GetOrCreateClangModule(llvm::StringRef name,
@@ -374,7 +382,9 @@ public:
 
   bool FieldIsBitfield(clang::FieldDecl *field, uint32_t &bitfield_bit_size);
 
-  static bool RecordHasFields(const clang::RecordDecl *record_decl);
+  bool RecordHasFields(const clang::RecordDecl *record_decl);
+
+  bool BaseSpecifierIsEmpty(const clang::CXXBaseSpecifier *b);
 
   CompilerType CreateObjCClass(llvm::StringRef name,
                                clang::DeclContext *decl_ctx,
@@ -640,6 +650,8 @@ public:
   // Type Completion
 
   bool GetCompleteType(lldb::opaque_compiler_type_t type) override;
+
+  bool IsForcefullyCompleted(lldb::opaque_compiler_type_t type) override;
 
   // Accessors
 

lldb/test/API/functionalities/limit-debug-info/TestLimitDebugInfo.py
@@ -16,10 +16,12 @@ class LimitDebugInfoTestCase(TestBase):
         type_ = exe.FindFirstType(name)
         self.trace("type_: %s"%type_)
         self.assertTrue(type_)
+        self.assertTrue(type_.IsTypeComplete())
         base = type_.GetDirectBaseClassAtIndex(0).GetType()
         self.trace("base:%s"%base)
         self.assertTrue(base)
         self.assertEquals(base.GetNumberOfFields(), 0)
+        self.assertFalse(base.IsTypeComplete())
 
     def _check_debug_info_is_limited(self, target):
         # Without other shared libraries we should only see the member declared
@@ -27,6 +29,100 @@ class LimitDebugInfoTestCase(TestBase):
         # building with limited debug info.
         self._check_type(target, "InheritsFromOne")
         self._check_type(target, "InheritsFromTwo")
+
+    def _check_incomplete_frame_variable_output(self):
+        # Check that the display of the "frame variable" output identifies the
+        # incomplete types. Currently the expression parser will find the real
+        # definition for a type when running an expression for any forcefully
+        # completed types, but "frame variable" won't. I hope to fix this with
+        # a follow up patch, but if we don't find the actual definition we
+        # should clearly show this to the user by showing which types were
+        # incomplete. So this will test verifies the expected output for such
+        # types. We also need to verify the standard "frame variable" output
+        # which will inline all of the members on one line, versus the full
+        # output from "frame variable --raw" and a few other options.
+        # self.expect("frame variable two_as_member", error=True,
+        #     substrs=["no member named 'one' in 'InheritsFromOne'"])
+
+        command_expect_pairs = [
+            # Test standard "frame variable" output for types to make sure
+            # "<incomplete type>" shows up where we expect it to
+            ["var two_as_member", [
+                "(TwoAsMember) ::two_as_member = (two = <incomplete type>, member = 47)"]
+            ],
+            ["var inherits_from_one", [
+                "(InheritsFromOne) ::inherits_from_one = (One = <incomplete type>, member = 47)"]
+            ],
+            ["var inherits_from_two", [
+                "(InheritsFromTwo) ::inherits_from_two = (Two = <incomplete type>, member = 47)"]
+            ],
+            ["var one_as_member", [
+                "(OneAsMember) ::one_as_member = (one = <incomplete type>, member = 47)"]
+            ],
+            ["var two_as_member", [
+                "(TwoAsMember) ::two_as_member = (two = <incomplete type>, member = 47)"]
+            ],
+            ["var array_of_one", [
+                "(array::One[3]) ::array_of_one = ([0] = <incomplete type>, [1] = <incomplete type>, [2] = <incomplete type>)"]
+            ],
+            ["var array_of_two", [
+                "(array::Two[3]) ::array_of_two = ([0] = <incomplete type>, [1] = <incomplete type>, [2] = <incomplete type>)"]
+            ],
+            ["var shadowed_one", [
+                "(ShadowedOne) ::shadowed_one = (func_shadow::One = <incomplete type>, member = 47)"]
+            ],
+
+            # Now test "frame variable --show-types output" which has multi-line
+            # output and should not always show classes that were forcefully
+            # completed to the user to let them know they have a type that should
+            # have been complete but wasn't.
+            ["var --show-types inherits_from_one", [
+                "(InheritsFromOne) ::inherits_from_one = {",
+                "  (One) One = <incomplete type> {}",
+                "  (int) member = 47",
+                "}"]
+            ],
+            ["var --show-types inherits_from_two", [
+                "(InheritsFromTwo) ::inherits_from_two = {",
+                "  (Two) Two = <incomplete type> {}",
+                "  (int) member = 47",
+                "}"]
+            ],
+            ["var  --show-types one_as_member", [
+                "(OneAsMember) ::one_as_member = {",
+                "  (member::One) one = <incomplete type> {}",
+                "  (int) member = 47",
+                "}"]
+            ],
+            ["var  --show-types two_as_member", [
+                "(TwoAsMember) ::two_as_member = {",
+                "  (member::Two) two = <incomplete type> {}",
+                "  (int) member = 47",
+                "}"]
+            ],
+            ["var  --show-types array_of_one", [
+                "(array::One[3]) ::array_of_one = {",
+                "  (array::One) [0] = <incomplete type> {}",
+                "  (array::One) [1] = <incomplete type> {}",
+                "  (array::One) [2] = <incomplete type> {}",
+                "}"]
+            ],
+            ["var  --show-types array_of_two", [
+                "(array::Two[3]) ::array_of_two = {",
+                "  (array::Two) [0] = <incomplete type> {}",
+                "  (array::Two) [1] = <incomplete type> {}",
+                "  (array::Two) [2] = <incomplete type> {}",
+                "}"]
+            ],
+            ["var  --show-types shadowed_one", [
+                "(ShadowedOne) ::shadowed_one = {",
+                "  (func_shadow::One) func_shadow::One = <incomplete type> {}",
+                "  (int) member = 47",
+                "}"]
+            ],
+        ]
+        for command, expect_items in command_expect_pairs:
+            self.expect(command, substrs=expect_items)
 
     @skipIf(bugnumber="pr46284", debug_info="gmodules")
     @skipIfWindows # Clang emits type info even with -flimit-debug-info
@@ -40,7 +136,7 @@ class LimitDebugInfoTestCase(TestBase):
         self._check_debug_info_is_limited(target)
 
         lldbutil.run_to_name_breakpoint(self, "main",
-                extra_images=["one", "two"])
+                                        extra_images=["one", "two"])
 
         # But when other shared libraries are loaded, we should be able to see
         # all members.
@@ -66,6 +162,8 @@ class LimitDebugInfoTestCase(TestBase):
 
         self.expect_expr("shadowed_one.member", result_value="47")
         self.expect_expr("shadowed_one.one", result_value="142")
+
+        self._check_incomplete_frame_variable_output()
 
     @skipIf(bugnumber="pr46284", debug_info="gmodules")
     @skipIfWindows # Clang emits type info even with -flimit-debug-info
@@ -109,6 +207,8 @@ class LimitDebugInfoTestCase(TestBase):
         self.expect("expr get_two().one().member", error=True,
                 substrs=["calling 'one' with incomplete return type 'result::One'"])
         self.expect_expr("get_two().member", result_value="224")
+
+        self._check_incomplete_frame_variable_output()
 
     @skipIf(bugnumber="pr46284", debug_info="gmodules")
     @skipIfWindows # Clang emits type info even with -flimit-debug-info
@@ -155,3 +255,5 @@ class LimitDebugInfoTestCase(TestBase):
                 substrs=["calling 'get_two' with incomplete return type 'result::Two'"])
         self.expect("expr get_two().member", error=True,
                 substrs=["calling 'get_two' with incomplete return type 'result::Two'"])
+
+        self._check_incomplete_frame_variable_output()

commit 26c1448b6c59ad0239904a9dd943ae9cb17ba7cb

Unbreak lldb build broken by 268b25435242 Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Symbol/CompilerType.cpp
@@ -96,7 +96,8 @@ bool CompilerType::IsCompleteType() const {
 
 bool CompilerType::IsForcefullyCompleted() const {
   if (IsValid())
-    return m_type_system->IsForcefullyCompleted(m_type);
+    if (auto type_system_sp = GetTypeSystem())
+      return type_system_sp->IsForcefullyCompleted(m_type);
   return false;
 }
 