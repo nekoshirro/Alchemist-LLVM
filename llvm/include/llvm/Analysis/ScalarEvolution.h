nekoshirro/Alchemist-LLVM

commit 539b3dbca8e7f630b25cebf018fd2a35acde781d

[SCEV] Fix FoldID::addInteger(unsigned long I)
"unsigned long" can be 8 bytes, but the code assumes 4.

This this the real root cause D122215 was reverted.

Reviewed By: fhahn

Differential Revision: https://reviews.llvm.org/D144316
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/include/llvm/Analysis/ScalarEvolution.h
@@ -1300,7 +1300,14 @@ public:
     SmallVector<unsigned, 4> Bits;
 
   public:
-    void addInteger(unsigned long I) { Bits.push_back(I); }
+    void addInteger(unsigned long I) {
+      if (sizeof(long) == sizeof(int))
+        addInteger(unsigned(I));
+      else if (sizeof(long) == sizeof(long long))
+        addInteger((unsigned long long)I);
+      else
+        llvm_unreachable("unexpected sizeof(long)");
+    }
     void addInteger(unsigned I) { Bits.push_back(I); }
     void addInteger(int I) { Bits.push_back(I); }
 

commit 8c3b3b5f34a05f5f26d7165f6b0957f32240ae08

[SCEV] Increase FoldID bits size cover common cases.
This should fix the regressions introduced by 539b3dbca8e7 by making
sure no dynamic allocations are needed in the common case, while
retaining support for arbitrary SCEV expressions.

Alternative to D144335.

Compile-time impact:
* NewPM-O3: -0.11%
* NewPM-ReleaseThinLTO: -0.10%
*NewPM-ReleaseLTO-g: -0.08%

https://llvm-compile-time-tracker.com/compare.php?from=cef387f89b5659a10e5cb9bb53d5056f2ef878e2&to=c1c64de4a973bcecaddbc4038a539234eb39413b&stat=instructions:u

Reviewed By: vitalybuka, nikic

Differential Revision: https://reviews.llvm.org/D144382
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/include/llvm/Analysis/ScalarEvolution.h
@@ -1297,7 +1297,7 @@ public:
   bool loopIsFiniteByAssumption(const Loop *L);
 
   class FoldID {
-    SmallVector<unsigned, 4> Bits;
+    SmallVector<unsigned, 5> Bits;
 
   public:
     void addInteger(unsigned long I) {