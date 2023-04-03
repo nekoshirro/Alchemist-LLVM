nekoshirro/Alchemist-LLVM

commit dbfed99e46a40c7a616779b1fb259dae45446fe2

[ORC] Move most ORC APIs to ExecutorAddr, introduce ExecutorSymbolDef.
ExecutorAddr was introduced in 8c8165e85b8 as an eventual replacement for
JITTargetAddress. ExecutorSymbolDef is introduced in this patch as a
replacement for JITEvaluatedSymbol: ExecutorSymbolDef is an (ExecutorAddr,
JITSymbolFlags) pair, where JITEvaluatedSymbol was a (JITTargetAddress,
JITSymbolFlags) pair.

A number of APIs had already migrated from JITTargetAddress to ExecutorAddr,
but many of ORC's internals were still using the older type. This patch aims
to address that.

Some public APIs are affected as well. If you need to migrate your APIs you can
use the following operations:

* ExecutorAddr::toPtr replaces jitTargetAddressToPointer and
  jitTargetAddressToFunction.

* ExecutorAddr::fromPtr replace pointerToJITTargetAddress.

* ExecutorAddr(JITTargetAddress) creates an ExecutorAddr value from a
  JITTargetAddress.

* ExecutorAddr::getValue() creates a JITTargetAddress value from an
  ExecutorAddr.

JITTargetAddress and JITEvaluatedSymbol will remain in JITSymbol.h for now, but
the aim will be to eventually deprecate and remove these types (probably when
MCJIT and RuntimeDyld are deprecated).
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/ExecutionEngine/Orc/DebugObjectManagerPlugin.cpp
@@ -106,8 +106,8 @@ Error ELFDebugObjectSection<ELFT>::validateInBounds(StringRef Buffer,
 
 template <typename ELFT>
 void ELFDebugObjectSection<ELFT>::dump(raw_ostream &OS, StringRef Name) {
-  if (auto Addr = static_cast<JITTargetAddress>(Header->sh_addr)) {
-    OS << formatv("  {0:x16} {1}\n", Addr, Name);
+  if (Header->sh_addr) {
+    OS << formatv("  {0:x16} {1}\n", Header->sh_addr, Name);
   } else {
     OS << formatv("                     {0}\n", Name);
   }

commit f7e7a670225cf2b4bdc44c67efe6483e3de5e32f

[Orc] Print section load-addresses for debug objects as hex again
Apparently, the refactor in dbfed99e46a40c7a616779b1fb259dae45446fe2 broke the logic behind formatv here:
```
  140736272011264 .rodata
  140736272015360 .text
  140736272011424 .eh_frame
```
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/ExecutionEngine/Orc/DebugObjectManagerPlugin.cpp
@@ -93,8 +93,8 @@ Error ELFDebugObjectSection<ELFT>::validateInBounds(StringRef Buffer,
 
 template <typename ELFT>
 void ELFDebugObjectSection<ELFT>::dump(raw_ostream &OS, StringRef Name) {
-  if (Header->sh_addr) {
-    OS << formatv("  {0:x16} {1}\n", Header->sh_addr, Name);
+  if (uint64_t Addr = Header->sh_addr) {
+    OS << formatv("  {0:x16} {1}\n", Addr, Name);
   } else {
     OS << formatv("                     {0}\n", Name);
   }