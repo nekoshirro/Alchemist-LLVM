nekoshirro/Alchemist-LLVM

commit 076ba896caa53edfba72106ac3877c247950a778

[libc] Make UInt<T> trivially copyable
This patch defaults the copy constructor and copy operator so it can be
used with __builtin_bit_cast

Reviewed By: sivachandra

Differential Revision: https://reviews.llvm.org/D148794
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

libc/src/__support/UInt.h
@@ -35,12 +35,9 @@ template <size_t Bits> struct UInt {
   static constexpr uint64_t low(uint64_t v) { return v & MASK32; }
   static constexpr uint64_t high(uint64_t v) { return (v >> 32) & MASK32; }
 
-  constexpr UInt() {}
+  constexpr UInt() = default;
 
-  constexpr UInt(const UInt<Bits> &other) {
-    for (size_t i = 0; i < WORDCOUNT; ++i)
-      val[i] = other.val[i];
-  }
+  constexpr UInt(const UInt<Bits> &other) = default;
 
   template <size_t OtherBits> constexpr UInt(const UInt<OtherBits> &other) {
     if (OtherBits >= Bits) {
@@ -90,11 +87,7 @@ template <size_t Bits> struct UInt {
     return uint8_t(uint64_t(*this));
   }
 
-  UInt<Bits> &operator=(const UInt<Bits> &other) {
-    for (size_t i = 0; i < WORDCOUNT; ++i)
-      val[i] = other.val[i];
-    return *this;
-  }
+  UInt<Bits> &operator=(const UInt<Bits> &other) = default;
 
   constexpr bool is_zero() const {
     for (size_t i = 0; i < WORDCOUNT; ++i) {

commit 7f1c7d4638b316c7abc5b88ebe201ab9bbd541fa

[libc] Zero initialize Uint<T>::val
This fix a regression introduced by commit 076ba896caa53edfba72106ac3877c247950a778

Fixes the compilation failure reported by libc-x86_64-debian-gcc-fullbuild-dbg:

.../libc/src/__support/UInt.h:26:31: note: ‘struct __llvm_libc::cpp::UInt<192>’ has no user-provided default constructor
   26 | template <size_t Bits> struct UInt {
      |                               ^~~~
.../libc/src/__support/UInt.h:38:13: note: constructor is not user-provided because it is explicitly defaulted in the class body
   38 |   constexpr UInt() = default;
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

libc/src/__support/UInt.h
@@ -28,7 +28,7 @@ template <size_t Bits> struct UInt {
   static_assert(Bits > 0 && Bits % 64 == 0,
                 "Number of bits in UInt should be a multiple of 64.");
   static constexpr size_t WORDCOUNT = Bits / 64;
-  uint64_t val[WORDCOUNT];
+  uint64_t val[WORDCOUNT]{};
 
   static constexpr uint64_t MASK32 = 0xFFFFFFFFu;
 