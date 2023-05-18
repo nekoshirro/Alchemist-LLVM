nekoshirro/Alchemist-LLVM

commit 17a0fdcc8af0987cde91eba3268ba9a3a5dc31f1

Revert "ValueTracking: Handle phi in computeKnownFPClass"
This reverts commit 8dd1202bf8a3629871e879be599f132594874a7f.

Causing crashes in lencod
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Analysis/ValueTracking.cpp
@@ -5094,47 +5094,6 @@ void computeKnownFPClass(const Value *V, const APInt &DemandedElts,
                         Known, Depth + 1, Q, TLI);
     break;
   }
-  case Instruction::PHI: {
-    const PHINode *P = cast<PHINode>(Op);
-    // Unreachable blocks may have zero-operand PHI nodes.
-    if (P->getNumIncomingValues() == 0)
-      break;
-
-    // Otherwise take the unions of the known bit sets of the operands,
-    // taking conservative care to avoid excessive recursion.
-    if (Depth < MaxAnalysisRecursionDepth - 1) {
-      // Skip if every incoming value references to ourself.
-      if (isa_and_nonnull<UndefValue>(P->hasConstantValue()))
-        break;
-
-      bool First = true;
-
-      for (Value *IncValue : P->incoming_values()) {
-        // Skip direct self references.
-        if (IncValue == P)
-          continue;
-
-        KnownFPClass KnownSrc;
-        // Recurse, but cap the recursion to two levels, because we don't want
-        // to waste time spinning around in loops. We need at least depth 2 to
-        // detect known sign bits.
-        computeKnownFPClass(IncValue, DemandedElts, InterestedClasses, KnownSrc,
-                            MaxAnalysisRecursionDepth - 2, Q, TLI);
-
-        if (First) {
-          Known = KnownSrc;
-          First = false;
-        } else {
-          Known |= KnownSrc;
-        }
-
-        if (Known.KnownFPClasses == fcAllFlags)
-          break;
-      }
-    }
-
-    break;
-  }
   default:
     break;
   }

llvm/unittests/Analysis/ValueTrackingTest.cpp
@@ -1630,114 +1630,6 @@ TEST_F(ComputeKnownFPClassTest, FMulNoZero) {
   expectKnownFPClass(fcAllFlags, std::nullopt, A7);
 }
 
-TEST_F(ComputeKnownFPClassTest, Phi) {
-  parseAssembly(
-      "define float @test(i1 %cond, float nofpclass(nan inf) %arg0, float nofpclass(nan) %arg1) {\n"
-      "entry:\n"
-      "  br i1 %cond, label %bb0, label %bb1\n"
-      "bb0:\n"
-      "  br label %ret\n"
-      "bb1:\n"
-      "  br label %ret\n"
-      "ret:\n"
-      "  %A = phi float [ %arg0, %bb0 ],  [ %arg1, %bb1 ]\n"
-      "  ret float %A\n"
-      "}\n");
-  expectKnownFPClass(~fcNan, std::nullopt);
-}
-
-TEST_F(ComputeKnownFPClassTest, PhiKnownSignFalse) {
-  parseAssembly(
-      "declare float @llvm.fabs.f32(float)"
-      "define float @test(i1 %cond, float nofpclass(nan) %arg0, float nofpclass(nan) %arg1) {\n"
-      "entry:\n"
-      "  br i1 %cond, label %bb0, label %bb1\n"
-      "bb0:\n"
-      "  %fabs.arg0 = call float @llvm.fabs.f32(float %arg0)\n"
-      "  br label %ret\n"
-      "bb1:\n"
-      "  %fabs.arg1 = call float @llvm.fabs.f32(float %arg1)\n"
-      "  br label %ret\n"
-      "ret:\n"
-      "  %A = phi float [ %fabs.arg0, %bb0 ],  [ %fabs.arg1, %bb1 ]\n"
-      "  ret float %A\n"
-      "}\n");
-  expectKnownFPClass(fcPositive, false);
-}
-
-TEST_F(ComputeKnownFPClassTest, PhiKnownSignTrue) {
-  parseAssembly(
-      "declare float @llvm.fabs.f32(float)"
-      "define float @test(i1 %cond, float nofpclass(nan) %arg0, float %arg1) {\n"
-      "entry:\n"
-      "  br i1 %cond, label %bb0, label %bb1\n"
-      "bb0:\n"
-      "  %fabs.arg0 = call float @llvm.fabs.f32(float %arg0)\n"
-      "  %fneg.fabs.arg0 = fneg float %fabs.arg0\n"
-      "  br label %ret\n"
-      "bb1:\n"
-      "  %fabs.arg1 = call float @llvm.fabs.f32(float %arg1)\n"
-      "  %fneg.fabs.arg1 = fneg float %fabs.arg1\n"
-      "  br label %ret\n"
-      "ret:\n"
-      "  %A = phi float [ %fneg.fabs.arg0, %bb0 ],  [ %fneg.fabs.arg1, %bb1 ]\n"
-      "  ret float %A\n"
-      "}\n");
-  expectKnownFPClass(fcNegative | fcNan, true);
-}
-
-TEST_F(ComputeKnownFPClassTest, UnreachablePhi) {
-  parseAssembly(
-      "define float @test(float %arg) {\n"
-      "entry:\n"
-      "  ret float 0.0\n"
-      "unreachable:\n"
-      "  %A = phi float\n"
-      "  ret float %A\n"
-      "}\n");
-  expectKnownFPClass(fcAllFlags, std::nullopt);
-}
-
-TEST_F(ComputeKnownFPClassTest, SelfPhiOnly) {
-  parseAssembly(
-      "define float @test(float %arg) {\n"
-      "entry:\n"
-      "  ret float 0.0\n"
-      "loop:\n"
-      "  %A = phi float [ %A, %loop ]\n"
-      "  br label %loop\n"
-      "}\n");
-  expectKnownFPClass(fcAllFlags, std::nullopt);
-}
-
-TEST_F(ComputeKnownFPClassTest, SelfPhiFirstArg) {
-  parseAssembly(
-      "define float @test(i1 %cond, float nofpclass(inf) %arg) {\n"
-      "entry:\n"
-      "  br i1 %cond, label %loop, label %ret\n"
-      "loop:\n"
-      "  %A = phi float [ %arg, %entry ], [ %A, %loop ]\n"
-      "  br label %loop\n"
-      "ret:\n"
-      "  ret float %A"
-      "}\n");
-  expectKnownFPClass(~fcInf, std::nullopt);
-}
-
-TEST_F(ComputeKnownFPClassTest, SelfPhiSecondArg) {
-  parseAssembly(
-      "define float @test(i1 %cond, float nofpclass(inf) %arg) {\n"
-      "entry:\n"
-      "  br i1 %cond, label %loop, label %ret\n"
-      "loop:\n"
-      "  %A = phi float [ %A, %loop ], [ %arg, %entry ]\n"
-      "  br label %loop\n"
-      "ret:\n"
-      "  ret float %A"
-      "}\n");
-  expectKnownFPClass(~fcInf, std::nullopt);
-}
-
 TEST_F(ComputeKnownFPClassTest, CannotBeOrderedLessThanZero) {
   parseAssembly("define float @test(float %arg) {\n"
                 "  %A = fmul float %arg, %arg"

commit d988d4396c4651bdbc90e20effa9054f9d1698a5

EarlyCSE: Add regression test for computeKnownFPClass phi handling
This was reduced from the failure that caused the revert in
17a0fdcc8af0987cde91eba3268ba9a3a5dc31f1
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/test/Transforms/EarlyCSE/nofpclass-phi-regression.ll
@@ -1,0 +1,48 @@
+; NOTE: Assertions have been autogenerated by utils/update_test_checks.py UTC_ARGS: --version 2
+; RUN: opt -S -passes=early-cse %s | FileCheck %s
+
+; Check for regression when phi support for computeKnownFPClass was
+; first committed.
+define void @compute_known_fpclass_phi_assert(i1 %cmp46, i1 %tobool51, ptr %a01) {
+; CHECK-LABEL: define void @compute_known_fpclass_phi_assert
+; CHECK-SAME: (i1 [[CMP46:%.*]], i1 [[TOBOOL51:%.*]], ptr [[A01:%.*]]) {
+; CHECK-NEXT:  entry:
+; CHECK-NEXT:    br label [[FOR_COND45:%.*]]
+; CHECK:       for.cond45:
+; CHECK-NEXT:    [[TMP0:%.*]] = phi double [ [[TMP1:%.*]], [[FOR_INC80:%.*]] ], [ 0.000000e+00, [[ENTRY:%.*]] ]
+; CHECK-NEXT:    br i1 [[CMP46]], label [[FOR_BODY48:%.*]], label [[FOR_END82:%.*]]
+; CHECK:       for.body48:
+; CHECK-NEXT:    br i1 [[TOBOOL51]], label [[FOR_INC80]], label [[IF_THEN52:%.*]]
+; CHECK:       if.then52:
+; CHECK-NEXT:    store double 0.000000e+00, ptr [[A01]], align 8
+; CHECK-NEXT:    br label [[FOR_INC80]]
+; CHECK:       for.inc80:
+; CHECK-NEXT:    [[TMP1]] = phi double [ 0.000000e+00, [[IF_THEN52]] ], [ [[TMP0]], [[FOR_BODY48]] ]
+; CHECK-NEXT:    br label [[FOR_COND45]]
+; CHECK:       for.end82:
+; CHECK-NEXT:    [[MUL84:%.*]] = fmul double [[TMP0]], 0.000000e+00
+; CHECK-NEXT:    ret void
+;
+entry:
+  br label %for.cond45
+
+for.cond45:                                       ; preds = %for.inc80, %entry
+  %0 = phi double [ %1, %for.inc80 ], [ 0.000000e+00, %entry ]
+  br i1 %cmp46, label %for.body48, label %for.end82
+
+for.body48:                                       ; preds = %for.cond45
+  br i1 %tobool51, label %for.inc80, label %if.then52
+
+if.then52:                                        ; preds = %for.body48
+  store double 0.000000e+00, ptr %a01, align 8
+  br label %for.inc80
+
+for.inc80:                                        ; preds = %if.then52, %for.body48
+  %1 = phi double [ 0.000000e+00, %if.then52 ], [ %0, %for.body48 ]
+  br label %for.cond45
+
+for.end82:                                        ; preds = %for.cond45
+  %mul84 = fmul double %0, 0.000000e+00
+  %neg = fneg double %mul84
+  ret void
+}