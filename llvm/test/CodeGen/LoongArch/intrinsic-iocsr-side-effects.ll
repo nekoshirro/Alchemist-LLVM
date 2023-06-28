nekoshirro/Alchemist-LLVM

commit 3b0a4119e954e7c77ee2bcb37dd241e721f6a5da

[LoongArch] Add missing chains and remove unnecessary `SDNPSideEffect` property for some intrinsic nodes Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Target/LoongArch/LoongArchInstrInfo.td
@@ -75,21 +75,21 @@ def loongarch_srl_w : SDNode<"LoongArchISD::SRL_W", SDT_LoongArchIntBinOpW>;
 def loongarch_rotr_w : SDNode<"LoongArchISD::ROTR_W", SDT_LoongArchIntBinOpW>;
 def loongarch_rotl_w : SDNode<"LoongArchISD::ROTL_W", SDT_LoongArchIntBinOpW>;
 def loongarch_crc_w_b_w
-    : SDNode<"LoongArchISD::CRC_W_B_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRC_W_B_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crc_w_h_w
-    : SDNode<"LoongArchISD::CRC_W_H_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRC_W_H_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crc_w_w_w
-    : SDNode<"LoongArchISD::CRC_W_W_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRC_W_W_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crc_w_d_w
-    : SDNode<"LoongArchISD::CRC_W_D_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRC_W_D_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crcc_w_b_w
-    : SDNode<"LoongArchISD::CRCC_W_B_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRCC_W_B_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crcc_w_h_w
-    : SDNode<"LoongArchISD::CRCC_W_H_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRCC_W_H_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crcc_w_w_w
-    : SDNode<"LoongArchISD::CRCC_W_W_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRCC_W_W_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_crcc_w_d_w
-    : SDNode<"LoongArchISD::CRCC_W_D_W", SDT_LoongArchIntBinOpW>;
+    : SDNode<"LoongArchISD::CRCC_W_D_W", SDT_LoongArchIntBinOpW, [SDNPHasChain]>;
 def loongarch_bstrins
     : SDNode<"LoongArchISD::BSTRINS", SDT_LoongArchBStrIns>;
 def loongarch_bstrpick
@@ -106,26 +106,27 @@ def loongarch_ibar : SDNode<"LoongArchISD::IBAR", SDT_LoongArchVI,
                              [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_break : SDNode<"LoongArchISD::BREAK", SDT_LoongArchVI,
                               [SDNPHasChain, SDNPSideEffect]>;
-def loongarch_movfcsr2gr : SDNode<"LoongArchISD::MOVFCSR2GR", SDT_LoongArchMovfcsr2gr>;
+def loongarch_movfcsr2gr : SDNode<"LoongArchISD::MOVFCSR2GR", SDT_LoongArchMovfcsr2gr,
+                                  [SDNPHasChain]>;
 def loongarch_movgr2fcsr : SDNode<"LoongArchISD::MOVGR2FCSR", SDT_LoongArchMovgr2fcsr,
                                    [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_syscall : SDNode<"LoongArchISD::SYSCALL", SDT_LoongArchVI,
                                 [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_csrrd : SDNode<"LoongArchISD::CSRRD", SDT_LoongArchCsrrd,
-                              [SDNPHasChain, SDNPSideEffect]>;
+                              [SDNPHasChain]>;
 def loongarch_csrwr : SDNode<"LoongArchISD::CSRWR", SDT_LoongArchCsrwr,
                               [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_csrxchg : SDNode<"LoongArchISD::CSRXCHG",
                                 SDT_LoongArchCsrxchg,
                                 [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_iocsrrd_b : SDNode<"LoongArchISD::IOCSRRD_B", SDTUnaryOp,
-                                  [SDNPHasChain, SDNPSideEffect]>;
+                                  [SDNPHasChain]>;
 def loongarch_iocsrrd_h : SDNode<"LoongArchISD::IOCSRRD_H", SDTUnaryOp,
-                                  [SDNPHasChain, SDNPSideEffect]>;
+                                  [SDNPHasChain]>;
 def loongarch_iocsrrd_w : SDNode<"LoongArchISD::IOCSRRD_W", SDTUnaryOp,
-                                  [SDNPHasChain, SDNPSideEffect]>;
+                                  [SDNPHasChain]>;
 def loongarch_iocsrrd_d : SDNode<"LoongArchISD::IOCSRRD_D", SDTUnaryOp,
-                                  [SDNPHasChain, SDNPSideEffect]>;
+                                  [SDNPHasChain]>;
 def loongarch_iocsrwr_b : SDNode<"LoongArchISD::IOCSRWR_B",
                                   SDT_LoongArchIocsrwr,
                                   [SDNPHasChain, SDNPSideEffect]>;
@@ -139,7 +140,7 @@ def loongarch_iocsrwr_d : SDNode<"LoongArchISD::IOCSRWR_D",
                                   SDT_LoongArchIocsrwr,
                                   [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_cpucfg : SDNode<"LoongArchISD::CPUCFG", SDTUnaryOp,
-                               [SDNPHasChain, SDNPSideEffect]>;
+                               [SDNPHasChain]>;
 
 //===----------------------------------------------------------------------===//
 // Operand and SDNode transformation definitions.

llvm/test/CodeGen/LoongArch/intrinsic-la64.ll
@@ -29,6 +29,14 @@ define i32 @crc_w_b_w(i32 %a, i32 %b) nounwind {
   ret i32 %res
 }
 
+define void @crc_w_b_w_noret(i32 %a, i32 %b) nounwind {
+; CHECK-LABEL: crc_w_b_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crc.w.b.w(i32 %a, i32 %b)
+  ret void
+}
+
 define i32 @crc_w_h_w(i32 %a, i32 %b) nounwind {
 ; CHECK-LABEL: crc_w_h_w:
 ; CHECK:       # %bb.0:
@@ -38,6 +46,14 @@ define i32 @crc_w_h_w(i32 %a, i32 %b) nounwind {
   ret i32 %res
 }
 
+define void @crc_w_h_w_noret(i32 %a, i32 %b) nounwind {
+; CHECK-LABEL: crc_w_h_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crc.w.h.w(i32 %a, i32 %b)
+  ret void
+}
+
 define i32 @crc_w_w_w(i32 %a, i32 %b) nounwind {
 ; CHECK-LABEL: crc_w_w_w:
 ; CHECK:       # %bb.0:
@@ -45,6 +61,14 @@ define i32 @crc_w_w_w(i32 %a, i32 %b) nounwind {
 ; CHECK-NEXT:    ret
   %res = call i32 @llvm.loongarch.crc.w.w.w(i32 %a, i32 %b)
   ret i32 %res
+}
+
+define void @crc_w_w_w_noret(i32 %a, i32 %b) nounwind {
+; CHECK-LABEL: crc_w_w_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crc.w.w.w(i32 %a, i32 %b)
+  ret void
 }
 
 define void @cacop_d(i64 %a) nounwind {
@@ -65,6 +89,14 @@ define i32 @crc_w_d_w(i64 %a, i32 %b) nounwind {
   ret i32 %res
 }
 
+define void @crc_w_d_w_noret(i64 %a, i32 %b) nounwind {
+; CHECK-LABEL: crc_w_d_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crc.w.d.w(i64 %a, i32 %b)
+  ret void
+}
+
 define i32 @crcc_w_b_w(i32 %a, i32 %b) nounwind {
 ; CHECK-LABEL: crcc_w_b_w:
 ; CHECK:       # %bb.0:
@@ -72,6 +104,14 @@ define i32 @crcc_w_b_w(i32 %a, i32 %b) nounwind {
 ; CHECK-NEXT:    ret
   %res = call i32 @llvm.loongarch.crcc.w.b.w(i32 %a, i32 %b)
   ret i32 %res
+}
+
+define void @crcc_w_b_w_noret(i32 %a, i32 %b) nounwind {
+; CHECK-LABEL: crcc_w_b_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crcc.w.b.w(i32 %a, i32 %b)
+  ret void
 }
 
 define i32 @crcc_w_h_w(i32 %a, i32 %b) nounwind {
@@ -83,6 +123,14 @@ define i32 @crcc_w_h_w(i32 %a, i32 %b) nounwind {
   ret i32 %res
 }
 
+define void @crcc_w_h_w_noret(i32 %a, i32 %b) nounwind {
+; CHECK-LABEL: crcc_w_h_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crcc.w.h.w(i32 %a, i32 %b)
+  ret void
+}
+
 define i32 @crcc_w_w_w(i32 %a, i32 %b) nounwind {
 ; CHECK-LABEL: crcc_w_w_w:
 ; CHECK:       # %bb.0:
@@ -90,6 +138,14 @@ define i32 @crcc_w_w_w(i32 %a, i32 %b) nounwind {
 ; CHECK-NEXT:    ret
   %res = call i32 @llvm.loongarch.crcc.w.w.w(i32 %a, i32 %b)
   ret i32 %res
+}
+
+define void @crcc_w_w_w_noret(i32 %a, i32 %b) nounwind {
+; CHECK-LABEL: crcc_w_w_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crcc.w.w.w(i32 %a, i32 %b)
+  ret void
 }
 
 define i32 @crcc_w_d_w(i64 %a, i32 %b) nounwind {
@@ -101,6 +157,14 @@ define i32 @crcc_w_d_w(i64 %a, i32 %b) nounwind {
   ret i32 %res
 }
 
+define void @crcc_w_d_w_noret(i64 %a, i32 %b) nounwind {
+; CHECK-LABEL: crcc_w_d_w_noret:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    ret
+  %res = call i32 @llvm.loongarch.crcc.w.d.w(i64 %a, i32 %b)
+  ret void
+}
+
 define i64 @csrrd_d() {
 ; CHECK-LABEL: csrrd_d:
 ; CHECK:       # %bb.0: # %entry
@@ -109,6 +173,15 @@ define i64 @csrrd_d() {
 entry:
   %0 = tail call i64 @llvm.loongarch.csrrd.d(i32 1)
   ret i64 %0
+}
+
+define void @csrrd_d_noret() {
+; CHECK-LABEL: csrrd_d_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i64 @llvm.loongarch.csrrd.d(i32 1)
+  ret void
 }
 
 define i64 @csrwr_d(i64 %a) {
@@ -163,6 +236,15 @@ entry:
   ret i64 %0
 }
 
+define void @iocsrrd_d_noret(i32 %a) {
+; CHECK-LABEL: iocsrrd_d_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i64 @llvm.loongarch.iocsrrd.d(i32 %a)
+  ret void
+}
+
 define void @iocsrwr_d(i64 %a, i32 signext %b) {
 ; CHECK-LABEL: iocsrwr_d:
 ; CHECK:       # %bb.0: # %entry
@@ -201,6 +283,16 @@ define i64 @lddir_d(i64 %a) {
 entry:
   %0 = tail call i64 @llvm.loongarch.lddir.d(i64 %a, i64 1)
   ret i64 %0
+}
+
+define void @lddir_d_noret(i64 %a) {
+; CHECK-LABEL: lddir_d_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    lddir $a0, $a0, 1
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i64 @llvm.loongarch.lddir.d(i64 %a, i64 1)
+  ret void
 }
 
 define void @ldpte_d(i64 %a) {

llvm/test/CodeGen/LoongArch/intrinsic.ll
@@ -69,6 +69,17 @@ entry:
   ret i32 %res
 }
 
+;; TODO: Optimize out `movfcsr2gr` without data-dependency.
+define void @movfcsr2gr_noret() nounwind {
+; CHECK-LABEL: movfcsr2gr_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    movfcsr2gr $a0, $fcsr1
+; CHECK-NEXT:    ret
+entry:
+  %res = call i32 @llvm.loongarch.movfcsr2gr(i32 1)
+  ret void
+}
+
 define void @syscall() nounwind {
 ; CHECK-LABEL: syscall:
 ; CHECK:       # %bb.0: # %entry
@@ -87,6 +98,15 @@ define i32 @csrrd_w() {
 entry:
   %0 = tail call i32 @llvm.loongarch.csrrd.w(i32 1)
   ret i32 %0
+}
+
+define void @csrrd_w_noret() {
+; CHECK-LABEL: csrrd_w_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i32 @llvm.loongarch.csrrd.w(i32 1)
+  ret void
 }
 
 define i32 @csrwr_w(i32 signext %a) {
@@ -161,6 +181,33 @@ entry:
   ret i32 %0
 }
 
+define void @iocsrrd_b_noret(i32 %a) {
+; CHECK-LABEL: iocsrrd_b_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i32 @llvm.loongarch.iocsrrd.b(i32 %a)
+  ret void
+}
+
+define void @iocsrrd_h_noret(i32 %a) {
+; CHECK-LABEL: iocsrrd_h_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i32 @llvm.loongarch.iocsrrd.h(i32 %a)
+  ret void
+}
+
+define void @iocsrrd_w_noret(i32 %a) {
+; CHECK-LABEL: iocsrrd_w_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i32 @llvm.loongarch.iocsrrd.w(i32 %a)
+  ret void
+}
+
 define void @iocsrwr_b(i32 %a, i32 %b) {
 ; CHECK-LABEL: iocsrwr_b:
 ; CHECK:       # %bb.0: # %entry
@@ -199,4 +246,13 @@ define i32 @cpucfg(i32 %a) {
 entry:
   %0 = tail call i32 @llvm.loongarch.cpucfg(i32 %a)
   ret i32 %0
+}
+
+define void @cpucfg_noret(i32 %a) {
+; CHECK-LABEL: cpucfg_noret:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    ret
+entry:
+  %0 = tail call i32 @llvm.loongarch.cpucfg(i32 %a)
+  ret void
 }

commit 5506ee55f08f74d36729be004df8854ecaf79a7f

[LoongArch] Add back SDNPSideEffect properties to CSR and IOCSR read ops
In general, CSR and IOCSR reads should be treated as volatile because:

* there may well be intervening writes between seemingly common
  expressions;
* the stateful entity behind a given (IO)CSR may well be volatile.

Confirmed to fix broken Clang Linux/LoongArch builds (dying when a
userspace process tries to use FPU, panicking when that process happens
to be PID 1) with this patch.

Fixes: https://github.com/llvm/llvm-project/issues/63549
Fixes: 3b0a4119e954 ("[LoongArch] Add missing chains and remove unnecessary `SDNPSideEffect` property for some intrinsic nodes")

Reviewed By: SixWeining, hev

Differential Revision: https://reviews.llvm.org/D153865
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Target/LoongArch/LoongArchInstrInfo.td
@@ -114,20 +114,20 @@ def loongarch_movgr2fcsr : SDNode<"LoongArchISD::MOVGR2FCSR",
 def loongarch_syscall : SDNode<"LoongArchISD::SYSCALL", SDT_LoongArchVI,
                                 [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_csrrd : SDNode<"LoongArchISD::CSRRD", SDT_LoongArchCsrrd,
-                              [SDNPHasChain]>;
+                              [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_csrwr : SDNode<"LoongArchISD::CSRWR", SDT_LoongArchCsrwr,
                               [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_csrxchg : SDNode<"LoongArchISD::CSRXCHG",
                                 SDT_LoongArchCsrxchg,
                                 [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_iocsrrd_b : SDNode<"LoongArchISD::IOCSRRD_B", SDTUnaryOp,
-                                  [SDNPHasChain]>;
+                                  [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_iocsrrd_h : SDNode<"LoongArchISD::IOCSRRD_H", SDTUnaryOp,
-                                  [SDNPHasChain]>;
+                                  [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_iocsrrd_w : SDNode<"LoongArchISD::IOCSRRD_W", SDTUnaryOp,
-                                  [SDNPHasChain]>;
+                                  [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_iocsrrd_d : SDNode<"LoongArchISD::IOCSRRD_D", SDTUnaryOp,
-                                  [SDNPHasChain]>;
+                                  [SDNPHasChain, SDNPSideEffect]>;
 def loongarch_iocsrwr_b : SDNode<"LoongArchISD::IOCSRWR_B",
                                   SDT_LoongArchIocsrwr,
                                   [SDNPHasChain, SDNPSideEffect]>;

llvm/test/CodeGen/LoongArch/intrinsic-csr-side-effects.ll
@@ -1,0 +1,47 @@
+; NOTE: Assertions have been autogenerated by utils/update_llc_test_checks.py
+; RUN: llc --mtriple=loongarch32 < %s | FileCheck %s
+; RUN: llc --mtriple=loongarch64 < %s | FileCheck %s
+
+declare i32 @llvm.loongarch.csrrd.w(i32 immarg) nounwind
+declare i32 @llvm.loongarch.csrwr.w(i32, i32 immarg) nounwind
+declare void @bug()
+
+define dso_local void @foo(i32 noundef signext %flag) nounwind {
+; CHECK-LABEL: foo:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    beqz $a0, .LBB0_2
+; CHECK-NEXT:  # %bb.1: # %if.then
+; CHECK-NEXT:    csrrd $a0, 2
+; CHECK-NEXT:    ori $a0, $a0, 1
+; CHECK-NEXT:    csrwr $a0, 2
+; CHECK-NEXT:  .LBB0_2: # %if.end
+; CHECK-NEXT:    csrrd $a0, 2
+; CHECK-NEXT:    andi $a0, $a0, 1
+; CHECK-NEXT:    bnez $a0, .LBB0_4
+; CHECK-NEXT:  # %bb.3: # %if.then2
+; CHECK-NEXT:    b %plt(bug)
+; CHECK-NEXT:  .LBB0_4: # %if.end3
+; CHECK-NEXT:    ret
+entry:
+  %tobool.not = icmp eq i32 %flag, 0
+  br i1 %tobool.not, label %if.end, label %if.then
+
+if.then:                                          ; preds = %entry
+  %0 = tail call i32 @llvm.loongarch.csrrd.w(i32 2)
+  %or = or i32 %0, 1
+  %1 = tail call i32 @llvm.loongarch.csrwr.w(i32 %or, i32 2)
+  br label %if.end
+
+if.end:                                           ; preds = %if.then, %entry
+  %2 = tail call i32 @llvm.loongarch.csrrd.w(i32 2)
+  %and = and i32 %2, 1
+  %tobool1.not = icmp eq i32 %and, 0
+  br i1 %tobool1.not, label %if.then2, label %if.end3
+
+if.then2:                                         ; preds = %if.end
+  tail call void @bug()
+  br label %if.end3
+
+if.end3:                                          ; preds = %if.then2, %if.end
+  ret void
+}

llvm/test/CodeGen/LoongArch/intrinsic-iocsr-side-effects.ll
@@ -1,0 +1,180 @@
+; NOTE: Assertions have been autogenerated by utils/update_llc_test_checks.py
+; RUN: llc --mtriple=loongarch64 < %s | FileCheck %s
+
+declare i32 @llvm.loongarch.iocsrrd.b(i32) nounwind
+declare void @llvm.loongarch.iocsrwr.b(i32, i32) nounwind
+declare i32 @llvm.loongarch.iocsrrd.h(i32) nounwind
+declare void @llvm.loongarch.iocsrwr.h(i32, i32) nounwind
+declare i32 @llvm.loongarch.iocsrrd.w(i32) nounwind
+declare void @llvm.loongarch.iocsrwr.w(i32, i32) nounwind
+declare i64 @llvm.loongarch.iocsrrd.d(i32) nounwind
+declare void @llvm.loongarch.iocsrwr.d(i64, i32) nounwind
+declare void @bug()
+
+define dso_local void @test_b(i32 noundef signext %flag) nounwind {
+; CHECK-LABEL: test_b:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    beqz $a0, .LBB0_2
+; CHECK-NEXT:  # %bb.1: # %if.then
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.b $a1, $a0
+; CHECK-NEXT:    ori $a1, $a1, 1
+; CHECK-NEXT:    iocsrwr.b $a1, $a0
+; CHECK-NEXT:  .LBB0_2: # %if.end
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.b $a0, $a0
+; CHECK-NEXT:    andi $a0, $a0, 1
+; CHECK-NEXT:    bnez $a0, .LBB0_4
+; CHECK-NEXT:  # %bb.3: # %if.then2
+; CHECK-NEXT:    b %plt(bug)
+; CHECK-NEXT:  .LBB0_4: # %if.end3
+; CHECK-NEXT:    ret
+entry:
+  %tobool.not = icmp eq i32 %flag, 0
+  br i1 %tobool.not, label %if.end, label %if.then
+
+if.then:                                          ; preds = %entry
+  %0 = tail call i32 @llvm.loongarch.iocsrrd.b(i32 2)
+  %or = or i32 %0, 1
+  tail call void @llvm.loongarch.iocsrwr.b(i32 %or, i32 2)
+  br label %if.end
+
+if.end:                                           ; preds = %if.then, %entry
+  %1 = tail call i32 @llvm.loongarch.iocsrrd.b(i32 2)
+  %and = and i32 %1, 1
+  %tobool1.not = icmp eq i32 %and, 0
+  br i1 %tobool1.not, label %if.then2, label %if.end3
+
+if.then2:                                         ; preds = %if.end
+  tail call void @bug()
+  br label %if.end3
+
+if.end3:                                          ; preds = %if.then2, %if.end
+  ret void
+}
+
+define dso_local void @test_h(i32 noundef signext %flag) nounwind {
+; CHECK-LABEL: test_h:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    beqz $a0, .LBB1_2
+; CHECK-NEXT:  # %bb.1: # %if.then
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.h $a1, $a0
+; CHECK-NEXT:    ori $a1, $a1, 1
+; CHECK-NEXT:    iocsrwr.h $a1, $a0
+; CHECK-NEXT:  .LBB1_2: # %if.end
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.h $a0, $a0
+; CHECK-NEXT:    andi $a0, $a0, 1
+; CHECK-NEXT:    bnez $a0, .LBB1_4
+; CHECK-NEXT:  # %bb.3: # %if.then2
+; CHECK-NEXT:    b %plt(bug)
+; CHECK-NEXT:  .LBB1_4: # %if.end3
+; CHECK-NEXT:    ret
+entry:
+  %tobool.not = icmp eq i32 %flag, 0
+  br i1 %tobool.not, label %if.end, label %if.then
+
+if.then:                                          ; preds = %entry
+  %0 = tail call i32 @llvm.loongarch.iocsrrd.h(i32 2)
+  %or = or i32 %0, 1
+  tail call void @llvm.loongarch.iocsrwr.h(i32 %or, i32 2)
+  br label %if.end
+
+if.end:                                           ; preds = %if.then, %entry
+  %1 = tail call i32 @llvm.loongarch.iocsrrd.h(i32 2)
+  %and = and i32 %1, 1
+  %tobool1.not = icmp eq i32 %and, 0
+  br i1 %tobool1.not, label %if.then2, label %if.end3
+
+if.then2:                                         ; preds = %if.end
+  tail call void @bug()
+  br label %if.end3
+
+if.end3:                                          ; preds = %if.then2, %if.end
+  ret void
+}
+
+define dso_local void @test_w(i32 noundef signext %flag) nounwind {
+; CHECK-LABEL: test_w:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    beqz $a0, .LBB2_2
+; CHECK-NEXT:  # %bb.1: # %if.then
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.w $a1, $a0
+; CHECK-NEXT:    ori $a1, $a1, 1
+; CHECK-NEXT:    iocsrwr.w $a1, $a0
+; CHECK-NEXT:  .LBB2_2: # %if.end
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.w $a0, $a0
+; CHECK-NEXT:    andi $a0, $a0, 1
+; CHECK-NEXT:    bnez $a0, .LBB2_4
+; CHECK-NEXT:  # %bb.3: # %if.then2
+; CHECK-NEXT:    b %plt(bug)
+; CHECK-NEXT:  .LBB2_4: # %if.end3
+; CHECK-NEXT:    ret
+entry:
+  %tobool.not = icmp eq i32 %flag, 0
+  br i1 %tobool.not, label %if.end, label %if.then
+
+if.then:                                          ; preds = %entry
+  %0 = tail call i32 @llvm.loongarch.iocsrrd.w(i32 2)
+  %or = or i32 %0, 1
+  tail call void @llvm.loongarch.iocsrwr.w(i32 %or, i32 2)
+  br label %if.end
+
+if.end:                                           ; preds = %if.then, %entry
+  %1 = tail call i32 @llvm.loongarch.iocsrrd.w(i32 2)
+  %and = and i32 %1, 1
+  %tobool1.not = icmp eq i32 %and, 0
+  br i1 %tobool1.not, label %if.then2, label %if.end3
+
+if.then2:                                         ; preds = %if.end
+  tail call void @bug()
+  br label %if.end3
+
+if.end3:                                          ; preds = %if.then2, %if.end
+  ret void
+}
+
+define dso_local void @test_d(i32 noundef signext %flag) nounwind {
+; CHECK-LABEL: test_d:
+; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    beqz $a0, .LBB3_2
+; CHECK-NEXT:  # %bb.1: # %if.then
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.d $a1, $a0
+; CHECK-NEXT:    ori $a1, $a1, 1
+; CHECK-NEXT:    iocsrwr.d $a1, $a0
+; CHECK-NEXT:  .LBB3_2: # %if.end
+; CHECK-NEXT:    ori $a0, $zero, 2
+; CHECK-NEXT:    iocsrrd.d $a0, $a0
+; CHECK-NEXT:    andi $a0, $a0, 1
+; CHECK-NEXT:    bnez $a0, .LBB3_4
+; CHECK-NEXT:  # %bb.3: # %if.then2
+; CHECK-NEXT:    b %plt(bug)
+; CHECK-NEXT:  .LBB3_4: # %if.end3
+; CHECK-NEXT:    ret
+entry:
+  %tobool.not = icmp eq i32 %flag, 0
+  br i1 %tobool.not, label %if.end, label %if.then
+
+if.then:                                          ; preds = %entry
+  %0 = tail call i64 @llvm.loongarch.iocsrrd.d(i32 2)
+  %or = or i64 %0, 1
+  tail call void @llvm.loongarch.iocsrwr.d(i64 %or, i32 2)
+  br label %if.end
+
+if.end:                                           ; preds = %if.then, %entry
+  %1 = tail call i64 @llvm.loongarch.iocsrrd.d(i32 2)
+  %and = and i64 %1, 1
+  %tobool1.not = icmp eq i64 %and, 0
+  br i1 %tobool1.not, label %if.then2, label %if.end3
+
+if.then2:                                         ; preds = %if.end
+  tail call void @bug()
+  br label %if.end3
+
+if.end3:                                          ; preds = %if.then2, %if.end
+  ret void
+}

llvm/test/CodeGen/LoongArch/intrinsic-la64.ll
@@ -178,6 +178,7 @@ entry:
 define void @csrrd_d_noret() {
 ; CHECK-LABEL: csrrd_d_noret:
 ; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    csrrd $a0, 1
 ; CHECK-NEXT:    ret
 entry:
   %0 = tail call i64 @llvm.loongarch.csrrd.d(i32 1)
@@ -239,6 +240,7 @@ entry:
 define void @iocsrrd_d_noret(i32 %a) {
 ; CHECK-LABEL: iocsrrd_d_noret:
 ; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    iocsrrd.d $a0, $a0
 ; CHECK-NEXT:    ret
 entry:
   %0 = tail call i64 @llvm.loongarch.iocsrrd.d(i32 %a)

llvm/test/CodeGen/LoongArch/intrinsic.ll
@@ -103,6 +103,7 @@ entry:
 define void @csrrd_w_noret() {
 ; CHECK-LABEL: csrrd_w_noret:
 ; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    csrrd $a0, 1
 ; CHECK-NEXT:    ret
 entry:
   %0 = tail call i32 @llvm.loongarch.csrrd.w(i32 1)
@@ -184,6 +185,7 @@ entry:
 define void @iocsrrd_b_noret(i32 %a) {
 ; CHECK-LABEL: iocsrrd_b_noret:
 ; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    iocsrrd.b $a0, $a0
 ; CHECK-NEXT:    ret
 entry:
   %0 = tail call i32 @llvm.loongarch.iocsrrd.b(i32 %a)
@@ -193,6 +195,7 @@ entry:
 define void @iocsrrd_h_noret(i32 %a) {
 ; CHECK-LABEL: iocsrrd_h_noret:
 ; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    iocsrrd.h $a0, $a0
 ; CHECK-NEXT:    ret
 entry:
   %0 = tail call i32 @llvm.loongarch.iocsrrd.h(i32 %a)
@@ -202,6 +205,7 @@ entry:
 define void @iocsrrd_w_noret(i32 %a) {
 ; CHECK-LABEL: iocsrrd_w_noret:
 ; CHECK:       # %bb.0: # %entry
+; CHECK-NEXT:    iocsrrd.w $a0, $a0
 ; CHECK-NEXT:    ret
 entry:
   %0 = tail call i32 @llvm.loongarch.iocsrrd.w(i32 %a)