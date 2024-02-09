nekoshirro/Alchemist-LLVM

commit b1b822d65c6f55e617ccad3d97af9d9d5fb20b38

Move AsmParser::parseTypeList() out-of-line (NFC)
There is an ABI incompatibility between clang and gcc that is unfortunate and
has bitten users multiple times already.

Fixes #62918
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

mlir/lib/IR/AsmPrinter.cpp
@@ -67,6 +67,15 @@ OpAsmParser::~OpAsmParser() = default;
 
 MLIRContext *AsmParser::getContext() const { return getBuilder().getContext(); }
 
+/// Parse a type list.
+/// This is out-of-line to work-around https://github.com/llvm/llvm-project/issues/62918
+ParseResult AsmParser::parseTypeList(SmallVectorImpl<Type> &result) {
+    return parseCommaSeparatedList(
+        [&]() { return parseType(result.emplace_back()); });
+  }
+
+
+
 //===----------------------------------------------------------------------===//
 // DialectAsmPrinter
 //===----------------------------------------------------------------------===//

mlir/include/mlir/IR/OpImplementation.h
@@ -1242,10 +1242,7 @@ public:
   }
 
   /// Parse a type list.
-  ParseResult parseTypeList(SmallVectorImpl<Type> &result) {
-    return parseCommaSeparatedList(
-        [&]() { return parseType(result.emplace_back()); });
-  }
+  ParseResult parseTypeList(SmallVectorImpl<Type> &result);
 
   /// Parse an arrow followed by a type list.
   virtual ParseResult parseArrowTypeList(SmallVectorImpl<Type> &result) = 0;

commit d8ab90dcbd92a5dafeb3101a30d37b9e6b4a5438

[MLIR] Fix a small formatting issue in AsmPrinter.cpp (#81214)
Introduced in b1b822d65c6f55e617ccad3d97af9d9d5fb20b38
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

mlir/lib/IR/AsmPrinter.cpp
@@ -74,11 +74,9 @@ MLIRContext *AsmParser::getContext() const { return getBuilder().getContext(); }
 /// Parse a type list.
 /// This is out-of-line to work-around https://github.com/llvm/llvm-project/issues/62918
 ParseResult AsmParser::parseTypeList(SmallVectorImpl<Type> &result) {
-    return parseCommaSeparatedList(
-        [&]() { return parseType(result.emplace_back()); });
-  }
-
-
+  return parseCommaSeparatedList(
+      [&]() { return parseType(result.emplace_back()); });
+}
 
 //===----------------------------------------------------------------------===//
 // DialectAsmPrinter