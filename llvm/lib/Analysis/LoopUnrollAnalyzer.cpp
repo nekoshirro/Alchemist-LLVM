nekoshirro/Alchemist-LLVM

commit da54d355c2947f5e334fcdf1eb0fecd40ab6bf38

[LoopUnrollAnalyzer] Store SimplifiedAddress offset as APInt (NFC) Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Analysis/LoopUnrollAnalyzer.cpp
@@ -64,7 +64,7 @@ bool UnrolledInstAnalyzer::simplifyInstWithSCEV(Instruction *I) {
     return false;
   SimplifiedAddress Address;
   Address.Base = Base->getValue();
-  Address.Offset = Offset->getValue();
+  Address.Offset = Offset->getAPInt();
   SimplifiedAddresses[I] = Address;
   return false;
 }
@@ -105,7 +105,7 @@ bool UnrolledInstAnalyzer::visitLoad(LoadInst &I) {
   auto AddressIt = SimplifiedAddresses.find(AddrOp);
   if (AddressIt == SimplifiedAddresses.end())
     return false;
-  ConstantInt *SimplifiedAddrOp = AddressIt->second.Offset;
+  const APInt &SimplifiedAddrOp = AddressIt->second.Offset;
 
   auto *GV = dyn_cast<GlobalVariable>(AddressIt->second.Base);
   // We're only interested in loads that can be completely folded to a
@@ -125,9 +125,9 @@ bool UnrolledInstAnalyzer::visitLoad(LoadInst &I) {
     return false;
 
   unsigned ElemSize = CDS->getElementType()->getPrimitiveSizeInBits() / 8U;
-  if (SimplifiedAddrOp->getValue().getActiveBits() > 64)
+  if (SimplifiedAddrOp.getActiveBits() > 64)
     return false;
-  int64_t SimplifiedAddrOpV = SimplifiedAddrOp->getSExtValue();
+  int64_t SimplifiedAddrOpV = SimplifiedAddrOp.getSExtValue();
   if (SimplifiedAddrOpV < 0) {
     // FIXME: For now we conservatively ignore out of bound accesses, but
     // we're allowed to perform the optimization in this case.
@@ -186,10 +186,9 @@ bool UnrolledInstAnalyzer::visitCmpInst(CmpInst &I) {
       if (SimplifiedRHS != SimplifiedAddresses.end()) {
         SimplifiedAddress &LHSAddr = SimplifiedLHS->second;
         SimplifiedAddress &RHSAddr = SimplifiedRHS->second;
-        if (LHSAddr.Base == RHSAddr.Base) {
-          LHS = LHSAddr.Offset;
-          RHS = RHSAddr.Offset;
-        }
+        if (LHSAddr.Base == RHSAddr.Base)
+          return ICmpInst::compare(LHSAddr.Offset, RHSAddr.Offset,
+                                   I.getPredicate());
       }
     }
   }

llvm/include/llvm/Analysis/LoopUnrollAnalyzer.h
@@ -44,7 +44,7 @@ class UnrolledInstAnalyzer : private InstVisitor<UnrolledInstAnalyzer, bool> {
   friend class InstVisitor<UnrolledInstAnalyzer, bool>;
   struct SimplifiedAddress {
     Value *Base = nullptr;
-    ConstantInt *Offset = nullptr;
+    APInt Offset;
   };
 
 public:

commit 53dc893d9ffee95e048be757b8f8fdb222977869

[LoopUnrollAnalyzer] Fix icmp simplification
Fix a bug I introduced in da54d355c2947f5e334fcdf1eb0fecd40ab6bf38.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Analysis/LoopUnrollAnalyzer.cpp
@@ -186,9 +186,12 @@ bool UnrolledInstAnalyzer::visitCmpInst(CmpInst &I) {
       if (SimplifiedRHS != SimplifiedAddresses.end()) {
         SimplifiedAddress &LHSAddr = SimplifiedLHS->second;
         SimplifiedAddress &RHSAddr = SimplifiedRHS->second;
-        if (LHSAddr.Base == RHSAddr.Base)
-          return ICmpInst::compare(LHSAddr.Offset, RHSAddr.Offset,
-                                   I.getPredicate());
+        if (LHSAddr.Base == RHSAddr.Base) {
+          bool Res = ICmpInst::compare(LHSAddr.Offset, RHSAddr.Offset,
+                                       I.getPredicate());
+          SimplifiedValues[&I] = ConstantInt::getBool(I.getType(), Res);
+          return true;
+        }
       }
     }
   }