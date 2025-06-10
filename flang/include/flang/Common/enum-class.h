nekoshirro/Alchemist-LLVM

commit 5f9c1a404c61e60b3cb2ab5ad4fae0b2e82a9bc7

[flang][cli] Add diagnostic flags to the CLI (#142022)
This change allows the flang CLI to accept `-W[no-]<feature>` flags matching the clang syntax and enable and disable usage and language feature warnings.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

flang/include/flang/Common/enum-class.h
@@ -18,8 +18,8 @@
 #define FORTRAN_COMMON_ENUM_CLASS_H_
 
 #include <array>
-#include <string>
-
+#include <functional>
+#include <string_view>
 namespace Fortran::common {
 
 constexpr std::size_t CountEnumNames(const char *p) {
@@ -62,11 +62,19 @@ constexpr std::array<std::string_view, ITEMS> EnumNames(const char *p) {
   enum class NAME { __VA_ARGS__ }; \
   [[maybe_unused]] static constexpr std::size_t NAME##_enumSize{ \
       ::Fortran::common::CountEnumNames(#__VA_ARGS__)}; \
+  [[maybe_unused]] static constexpr std::array<std::string_view, \
+      NAME##_enumSize> NAME##_names{ \
+      ::Fortran::common::EnumNames<NAME##_enumSize>(#__VA_ARGS__)}; \
+  [[maybe_unused]] static inline std::size_t EnumToInt(NAME e) { \
+    return static_cast<std::size_t>(e); \
+  } \
   [[maybe_unused]] static inline std::string_view EnumToString(NAME e) { \
-    static const constexpr auto names{ \
-        ::Fortran::common::EnumNames<NAME##_enumSize>(#__VA_ARGS__)}; \
-    return names[static_cast<std::size_t>(e)]; \
+    return NAME##_names[static_cast<std::size_t>(e)]; \
+  } \
+  [[maybe_unused]] inline void ForEach##NAME(std::function<void(NAME)> f) { \
+    for (std::size_t i{0}; i < NAME##_enumSize; ++i) { \
+      f(static_cast<NAME>(i)); \
+    } \
   }
-
 } // namespace Fortran::common
 #endif // FORTRAN_COMMON_ENUM_CLASS_H_

flang/include/flang/Common/optional.h
@@ -28,7 +28,6 @@
 
 #include "api-attrs.h"
 #include <optional>
-#include <type_traits>
 
 #if !defined(STD_OPTIONAL_UNSUPPORTED) && \
     (defined(__CUDACC__) || defined(__CUDA__)) && defined(__CUDA_ARCH__)

flang/include/flang/Support/Fortran-features.h
@@ -11,8 +11,7 @@
 
 #include "Fortran.h"
 #include "flang/Common/enum-set.h"
-#include "flang/Common/idioms.h"
-#include <optional>
+#include <string_view>
 #include <vector>
 
 namespace Fortran::common {
@@ -83,9 +82,6 @@ ENUM_CLASS(UsageWarning, Portability, PointerToUndefinable,
 using LanguageFeatures = EnumSet<LanguageFeature, LanguageFeature_enumSize>;
 using UsageWarnings = EnumSet<UsageWarning, UsageWarning_enumSize>;
 
-std::optional<LanguageFeature> FindLanguageFeature(const char *);
-std::optional<UsageWarning> FindUsageWarning(const char *);
-
 class LanguageFeatureControl {
 public:
   LanguageFeatureControl();
@@ -98,8 +94,10 @@ public:
   void EnableWarning(UsageWarning w, bool yes = true) {
     warnUsage_.set(w, yes);
   }
-  void WarnOnAllNonstandard(bool yes = true) { warnAllLanguage_ = yes; }
-  void WarnOnAllUsage(bool yes = true) { warnAllUsage_ = yes; }
+  void WarnOnAllNonstandard(bool yes = true);
+  bool IsWarnOnAllNonstandard() const { return warnAllLanguage_; }
+  void WarnOnAllUsage(bool yes = true);
+  bool IsWarnOnAllUsage() const { return warnAllUsage_; }
   void DisableAllNonstandardWarnings() {
     warnAllLanguage_ = false;
     warnLanguage_.clear();
@@ -108,26 +106,59 @@ public:
     warnAllUsage_ = false;
     warnUsage_.clear();
   }
-
+  void DisableAllWarnings() {
+    disableAllWarnings_ = true;
+    DisableAllNonstandardWarnings();
+    DisableAllUsageWarnings();
+  }
+  bool AreWarningsDisabled() const { return disableAllWarnings_; }
   bool IsEnabled(LanguageFeature f) const { return !disable_.test(f); }
-  bool ShouldWarn(LanguageFeature f) const {
-    return (warnAllLanguage_ && f != LanguageFeature::OpenMP &&
-               f != LanguageFeature::OpenACC && f != LanguageFeature::CUDA) ||
-        warnLanguage_.test(f);
+  bool ShouldWarn(LanguageFeature f) const { return warnLanguage_.test(f); }
+  bool ShouldWarn(UsageWarning w) const { return warnUsage_.test(w); }
+  // Cli options
+  // Take a string from the Cli and apply it to the LanguageFeatureControl.
+  // Return true if the option was recognized (and hence applied).
+  bool ApplyCliOption(std::string input);
+  // The add and replace functions are not currently used but are provided
+  // to allow a flexible many-to-one mapping from Cli spellings to enum values.
+  // Taking a string by value because the functions own this string after the
+  // call.
+  void AddAlternativeCliSpelling(LanguageFeature f, std::string input) {
+    cliOptions_.insert({input, {f}});
   }
-  bool ShouldWarn(UsageWarning w) const {
-    return warnAllUsage_ || warnUsage_.test(w);
+  void AddAlternativeCliSpelling(UsageWarning w, std::string input) {
+    cliOptions_.insert({input, {w}});
   }
+  void ReplaceCliCanonicalSpelling(LanguageFeature f, std::string input);
+  void ReplaceCliCanonicalSpelling(UsageWarning w, std::string input);
+  std::string_view getDefaultCliSpelling(LanguageFeature f) const {
+    return languageFeatureCliCanonicalSpelling_[EnumToInt(f)];
+  };
+  std::string_view getDefaultCliSpelling(UsageWarning w) const {
+    return usageWarningCliCanonicalSpelling_[EnumToInt(w)];
+  };
   // Return all spellings of operators names, depending on features enabled
   std::vector<const char *> GetNames(LogicalOperator) const;
   std::vector<const char *> GetNames(RelationalOperator) const;
 
 private:
+  // Map from Cli syntax of language features and usage warnings to their enum
+  // values.
+  std::unordered_map<std::string, std::variant<LanguageFeature, UsageWarning>>
+      cliOptions_;
+  // These two arrays map the enum values to their cannonical Cli spellings.
+  // Since each of the CanonicalSpelling is a string in the domain of the map
+  // above we just use a view of the string instead of another copy.
+  std::array<std::string_view, LanguageFeature_enumSize>
+      languageFeatureCliCanonicalSpelling_;
+  std::array<std::string_view, UsageWarning_enumSize>
+      usageWarningCliCanonicalSpelling_;
   LanguageFeatures disable_;
   LanguageFeatures warnLanguage_;
   bool warnAllLanguage_{false};
   UsageWarnings warnUsage_;
   bool warnAllUsage_{false};
+  bool disableAllWarnings_{false};
 };
 } // namespace Fortran::common
 #endif // FORTRAN_SUPPORT_FORTRAN_FEATURES_H_

flang/lib/Frontend/CompilerInvocation.cpp
@@ -20,12 +20,10 @@
 #include "flang/Support/Version.h"
 #include "flang/Tools/TargetSetup.h"
 #include "flang/Version.inc"
-#include "clang/Basic/AllDiagnostics.h"
 #include "clang/Basic/DiagnosticDriver.h"
 #include "clang/Basic/DiagnosticOptions.h"
 #include "clang/Driver/CommonArgs.h"
 #include "clang/Driver/Driver.h"
-#include "clang/Driver/DriverDiagnostic.h"
 #include "clang/Driver/OptionUtils.h"
 #include "clang/Driver/Options.h"
 #include "llvm/ADT/StringRef.h"
@@ -36,7 +34,6 @@
 #include "llvm/Option/OptTable.h"
 #include "llvm/Support/CodeGen.h"
 #include "llvm/Support/FileSystem.h"
-#include "llvm/Support/FileUtilities.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/Process.h"
 #include "llvm/Support/raw_ostream.h"
@@ -975,9 +972,22 @@ static bool parseSemaArgs(CompilerInvocation &res, llvm::opt::ArgList &args,
 
 /// Parses all diagnostics related arguments and populates the variables
 /// options accordingly. Returns false if new errors are generated.
+/// FC1 driver entry point for parsing diagnostic arguments.
 static bool parseDiagArgs(CompilerInvocation &res, llvm::opt::ArgList &args,
                           clang::DiagnosticsEngine &diags) {
   unsigned numErrorsBefore = diags.getNumErrors();
+
+  auto &features{res.getFrontendOpts().features};
+  // The order of these flags (-pedantic -W<feature> -w) is important and is
+  // chosen to match clang's behavior.
+
+  // -pedantic
+  if (args.hasArg(clang::driver::options::OPT_pedantic)) {
+    features.WarnOnAllNonstandard();
+    features.WarnOnAllUsage();
+    res.setEnableConformanceChecks();
+    res.setEnableUsageChecks();
+  }
 
   // -Werror option
   // TODO: Currently throws a Diagnostic for anything other than -W<error>,
@@ -988,22 +998,26 @@ static bool parseDiagArgs(CompilerInvocation &res, llvm::opt::ArgList &args,
     for (const auto &wArg : wArgs) {
       if (wArg == "error") {
         res.setWarnAsErr(true);
-      } else {
-        const unsigned diagID =
-            diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
-                                  "Only `-Werror` is supported currently.");
-        diags.Report(diagID);
+        // -W(no-)<feature>
+      } else if (!features.ApplyCliOption(wArg)) {
+        const unsigned diagID = diags.getCustomDiagID(
+            clang::DiagnosticsEngine::Error, "Unknown diagnostic option: -W%0");
+        diags.Report(diagID) << wArg;
       }
     }
   }
 
+  // -w
+  if (args.hasArg(clang::driver::options::OPT_w)) {
+    features.DisableAllWarnings();
+    res.setDisableWarnings();
+  }
+
   // Default to off for `flang -fc1`.
-  res.getFrontendOpts().showColors =
-      parseShowColorsArgs(args, /*defaultDiagColor=*/false);
-
-  // Honor color diagnostics.
-  res.getDiagnosticOpts().ShowColors = res.getFrontendOpts().showColors;
-
+  bool showColors{parseShowColorsArgs(args, false)};
+  diags.getDiagnosticOptions().ShowColors = showColors;
+  res.getDiagnosticOpts().ShowColors = showColors;
+  res.getFrontendOpts().showColors = showColors;
   return diags.getNumErrors() == numErrorsBefore;
 }
 
@@ -1078,16 +1092,6 @@ static bool parseDialectArgs(CompilerInvocation &res, llvm::opt::ArgList &args,
         Fortran::common::LanguageFeature::OpenACC);
   }
 
-  // -pedantic
-  if (args.hasArg(clang::driver::options::OPT_pedantic)) {
-    res.setEnableConformanceChecks();
-    res.setEnableUsageChecks();
-  }
-
-  // -w
-  if (args.hasArg(clang::driver::options::OPT_w))
-    res.setDisableWarnings();
-
   // -std=f2018
   // TODO: Set proper options when more fortran standards
   // are supported.
@@ -1096,6 +1100,7 @@ static bool parseDialectArgs(CompilerInvocation &res, llvm::opt::ArgList &args,
     // We only allow f2018 as the given standard
     if (standard == "f2018") {
       res.setEnableConformanceChecks();
+      res.getFrontendOpts().features.WarnOnAllNonstandard();
     } else {
       const unsigned diagID =
           diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
@@ -1702,16 +1707,7 @@ void CompilerInvocation::setFortranOpts() {
   if (frontendOptions.needProvenanceRangeToCharBlockMappings)
     fortranOptions.needProvenanceRangeToCharBlockMappings = true;
 
-  if (getEnableConformanceChecks())
-    fortranOptions.features.WarnOnAllNonstandard();
-
-  if (getEnableUsageChecks())
-    fortranOptions.features.WarnOnAllUsage();
-
-  if (getDisableWarnings()) {
-    fortranOptions.features.DisableAllNonstandardWarnings();
-    fortranOptions.features.DisableAllUsageWarnings();
-  }
+  fortranOptions.features = frontendOptions.features;
 }
 
 std::unique_ptr<Fortran::semantics::SemanticsContext>

flang/lib/Support/Fortran-features.cpp
@@ -8,11 +8,68 @@
 
 #include "flang/Support/Fortran-features.h"
 #include "flang/Common/idioms.h"
+#include "flang/Parser/characters.h"
 #include "flang/Support/Fortran.h"
+#include <string>
+#include <string_view>
 
 namespace Fortran::common {
 
+static std::vector<std::string_view> SplitCamelCase(std::string_view x) {
+  std::vector<std::string_view> result;
+  // NB, we start at 1 because the first character is never a word boundary.
+  size_t xSize{x.size()}, wordStart{0}, wordEnd{1};
+  for (; wordEnd < xSize; ++wordEnd) {
+    // Identify when wordEnd is at the start of a new word.
+    if ((!parser::IsUpperCaseLetter(x[wordEnd - 1]) &&
+            parser::IsUpperCaseLetter(x[wordEnd])) ||
+        // ACCUsage => ACC-Usage, CComment => C-Comment, etc.
+        (parser::IsUpperCaseLetter(x[wordEnd]) && wordEnd + 1 < xSize &&
+            parser::IsLowerCaseLetter(x[wordEnd + 1]))) {
+      result.push_back(x.substr(wordStart, wordEnd - wordStart));
+      wordStart = wordEnd;
+    }
+  }
+  // We went one past the end of the last word.
+  result.push_back(x.substr(wordStart, wordEnd - wordStart));
+  return result;
+}
+
+// Namespace for helper functions for parsing Cli options used instead of static
+// so that there can be unit tests for this function.
+namespace details {
+std::string CamelCaseToLowerCaseHyphenated(std::string_view x) {
+  std::vector<std::string_view> words{SplitCamelCase(x)};
+  std::string result{};
+  result.reserve(x.size() + words.size() + 1);
+  for (size_t i{0}; i < words.size(); ++i) {
+    std::string word{parser::ToLowerCaseLetters(words[i])};
+    result += i == 0 ? "" : "-";
+    result += word;
+  }
+  return result;
+}
+} // namespace details
+
 LanguageFeatureControl::LanguageFeatureControl() {
+  // Initialize the bidirectional maps with the default spellings.
+  cliOptions_.reserve(LanguageFeature_enumSize + UsageWarning_enumSize);
+  ForEachLanguageFeature([&](auto feature) {
+    std::string_view name{Fortran::common::EnumToString(feature)};
+    std::string cliOption{details::CamelCaseToLowerCaseHyphenated(name)};
+    cliOptions_.insert({cliOption, {feature}});
+    languageFeatureCliCanonicalSpelling_[EnumToInt(feature)] =
+        std::string_view{cliOption};
+  });
+
+  ForEachUsageWarning([&](auto warning) {
+    std::string_view name{Fortran::common::EnumToString(warning)};
+    std::string cliOption{details::CamelCaseToLowerCaseHyphenated(name)};
+    cliOptions_.insert({cliOption, {warning}});
+    usageWarningCliCanonicalSpelling_[EnumToInt(warning)] =
+        std::string_view{cliOption};
+  });
+
   // These features must be explicitly enabled by command line options.
   disable_.set(LanguageFeature::OldDebugLines);
   disable_.set(LanguageFeature::OpenACC);
@@ -94,57 +151,40 @@ LanguageFeatureControl::LanguageFeatureControl() {
   warnLanguage_.set(LanguageFeature::NullActualForAllocatable);
 }
 
-// Ignore case and any inserted punctuation (like '-'/'_')
-static std::optional<char> GetWarningChar(char ch) {
-  if (ch >= 'a' && ch <= 'z') {
-    return ch;
-  } else if (ch >= 'A' && ch <= 'Z') {
-    return ch - 'A' + 'a';
-  } else if (ch >= '0' && ch <= '9') {
-    return ch;
-  } else {
-    return std::nullopt;
+// Take a string from the Cli and apply it to the LanguageFeatureControl.
+bool LanguageFeatureControl::ApplyCliOption(std::string input) {
+  bool negated{false};
+  if (input.size() > 3 && input.substr(0, 3) == "no-") {
+    negated = true;
+    input = input.substr(3);
   }
-}
-
-static bool WarningNameMatch(const char *a, const char *b) {
-  while (true) {
-    auto ach{GetWarningChar(*a)};
-    while (!ach && *a) {
-      ach = GetWarningChar(*++a);
-    }
-    auto bch{GetWarningChar(*b)};
-    while (!bch && *b) {
-      bch = GetWarningChar(*++b);
-    }
-    if (!ach && !bch) {
+  if (auto it{cliOptions_.find(input)}; it != cliOptions_.end()) {
+    if (std::holds_alternative<LanguageFeature>(it->second)) {
+      EnableWarning(std::get<LanguageFeature>(it->second), !negated);
       return true;
-    } else if (!ach || !bch || *ach != *bch) {
-      return false;
     }
-    ++a, ++b;
-  }
-}
-
-template <typename ENUM, std::size_t N>
-std::optional<ENUM> ScanEnum(const char *name) {
-  if (name) {
-    for (std::size_t j{0}; j < N; ++j) {
-      auto feature{static_cast<ENUM>(j)};
-      if (WarningNameMatch(name, EnumToString(feature).data())) {
-        return feature;
-      }
+    if (std::holds_alternative<UsageWarning>(it->second)) {
+      EnableWarning(std::get<UsageWarning>(it->second), !negated);
+      return true;
     }
   }
-  return std::nullopt;
+  return false;
 }
 
-std::optional<LanguageFeature> FindLanguageFeature(const char *name) {
-  return ScanEnum<LanguageFeature, LanguageFeature_enumSize>(name);
+void LanguageFeatureControl::ReplaceCliCanonicalSpelling(
+    LanguageFeature f, std::string input) {
+  std::string_view &old{languageFeatureCliCanonicalSpelling_[EnumToInt(f)]};
+  cliOptions_.erase(std::string{old});
+  languageFeatureCliCanonicalSpelling_[EnumToInt(f)] = input;
+  cliOptions_.insert({input, {f}});
 }
 
-std::optional<UsageWarning> FindUsageWarning(const char *name) {
-  return ScanEnum<UsageWarning, UsageWarning_enumSize>(name);
+void LanguageFeatureControl::ReplaceCliCanonicalSpelling(
+    UsageWarning w, std::string input) {
+  std::string_view &old{usageWarningCliCanonicalSpelling_[EnumToInt(w)]};
+  cliOptions_.erase(std::string{old});
+  usageWarningCliCanonicalSpelling_[EnumToInt(w)] = input;
+  cliOptions_.insert({input, {w}});
 }
 
 std::vector<const char *> LanguageFeatureControl::GetNames(
@@ -201,4 +241,26 @@ std::vector<const char *> LanguageFeatureControl::GetNames(
   }
 }
 
+void LanguageFeatureControl::WarnOnAllNonstandard(bool yes) {
+  warnAllLanguage_ = yes;
+  warnLanguage_.reset();
+  if (yes) {
+    disableAllWarnings_ = false;
+    warnLanguage_.flip();
+    // These three features do not need to be warned about,
+    // but we do want their feature flags.
+    warnLanguage_.set(LanguageFeature::OpenMP, false);
+    warnLanguage_.set(LanguageFeature::OpenACC, false);
+    warnLanguage_.set(LanguageFeature::CUDA, false);
+  }
+}
+
+void LanguageFeatureControl::WarnOnAllUsage(bool yes) {
+  warnAllUsage_ = yes;
+  warnUsage_.reset();
+  if (yes) {
+    disableAllWarnings_ = false;
+    warnUsage_.flip();
+  }
+}
 } // namespace Fortran::common

flang/test/Driver/disable-diagnostic.f90
@@ -1,0 +1,20 @@
+! RUN: %flang -Wknown-bad-implicit-interface %s -c 2>&1 | FileCheck %s --check-prefix=WARN
+! RUN: %flang -pedantic -Wno-known-bad-implicit-interface %s -c 2>&1 | FileCheck %s --allow-empty
+! RUN: not %flang -WKnownBadImplicitInterface %s -c 2>&1 | FileCheck %s --check-prefix=ERROR1
+! RUN: not %flang -WKnown-Bad-Implicit-Interface %s -c 2>&1 | FileCheck %s --check-prefix=ERROR2
+
+! ERROR1: error: Unknown diagnostic option: -WKnownBadImplicitInterface
+! ERROR2: error: Unknown diagnostic option: -WKnown-Bad-Implicit-Interface
+
+program disable_diagnostic
+  REAL :: x
+  INTEGER :: y
+   ! CHECK-NOT: warning
+  ! WARN: warning: If the procedure's interface were explicit, this reference would be in error
+  call sub(x)
+  ! WARN: warning: If the procedure's interface were explicit, this reference would be in error
+  call sub(y)
+end program disable_diagnostic
+
+subroutine sub()
+end subroutine sub

flang/test/Driver/werror-wrong.f90
@@ -1,6 +1,7 @@
 ! Ensure that only argument -Werror is supported.
 
-! RUN: not %flang_fc1 -fsyntax-only -Wall %s  2>&1 | FileCheck %s --check-prefix=WRONG
-! RUN: not %flang_fc1 -fsyntax-only -WX %s  2>&1 | FileCheck %s --check-prefix=WRONG
+! RUN: not %flang_fc1 -fsyntax-only -Wall %s  2>&1 | FileCheck %s --check-prefix=WRONG1
+! RUN: not %flang_fc1 -fsyntax-only -WX %s  2>&1 | FileCheck %s --check-prefix=WRONG2
 
-! WRONG: Only `-Werror` is supported currently.
+! WRONG1: error: Unknown diagnostic option: -Wall
+! WRONG2: error: Unknown diagnostic option: -WX

flang/test/Driver/wextra-ok.f90
@@ -5,7 +5,7 @@
 ! RUN: not %flang -std=f2018 -Wblah -Wextra %s -c 2>&1 | FileCheck %s --check-prefix=WRONG
 
 ! CHECK-OK: the warning option '-Wextra' is not supported
-! WRONG: Only `-Werror` is supported currently.
+! WRONG: Unknown diagnostic option: -Wblah
 
 program wextra_ok
 end program wextra_ok

commit 2f65e6d7b94f50fd36cf9d87bcf603d1d1eaceb0

[flang][common] return ENUM_CLASS names definition to original state (#143553)
This PR simply reverts a few lines in
eaee04a432a94c809a9933c2f01b6d96cabbcfb2 to their state in
f4f83b4344be188bd0bab1932aeb05eb432713fe so that they are constant for
some of the build tests that require it. This should fix the breakage
caused by #142022.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

flang/include/flang/Common/enum-class.h
@@ -62,14 +62,13 @@ constexpr std::array<std::string_view, ITEMS> EnumNames(const char *p) {
   enum class NAME { __VA_ARGS__ }; \
   [[maybe_unused]] static constexpr std::size_t NAME##_enumSize{ \
       ::Fortran::common::CountEnumNames(#__VA_ARGS__)}; \
-  [[maybe_unused]] static constexpr std::array<std::string_view, \
-      NAME##_enumSize> NAME##_names{ \
-      ::Fortran::common::EnumNames<NAME##_enumSize>(#__VA_ARGS__)}; \
   [[maybe_unused]] static inline std::size_t EnumToInt(NAME e) { \
     return static_cast<std::size_t>(e); \
   } \
   [[maybe_unused]] static inline std::string_view EnumToString(NAME e) { \
-    return NAME##_names[static_cast<std::size_t>(e)]; \
+    static const constexpr auto names{ \
+        ::Fortran::common::EnumNames<NAME##_enumSize>(#__VA_ARGS__)}; \
+    return names[static_cast<std::size_t>(e)]; \
   } \
   [[maybe_unused]] inline void ForEach##NAME(std::function<void(NAME)> f) { \
     for (std::size_t i{0}; i < NAME##_enumSize; ++i) { \