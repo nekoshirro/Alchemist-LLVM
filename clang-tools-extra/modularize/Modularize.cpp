nekoshirro/Alchemist-LLVM

commit 5f35ec74eb343529ecff384c50a00caf0d237300

[modularize] Use std::tie to implement operator< (NFC) (#146220)
std::tie clearly expresses the intent while slightly shortening the
code.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

clang-tools-extra/modularize/Modularize.cpp
@@ -459,7 +459,7 @@ struct HeaderEntry {
     return !(X == Y);
   }
   friend bool operator<(const HeaderEntry &X, const HeaderEntry &Y) {
-    return X.Loc < Y.Loc || (X.Loc == Y.Loc && X.Name < Y.Name);
+    return std::tie(X.Loc, X.Name) < std::tie(Y.Loc, Y.Name);
   }
   friend bool operator>(const HeaderEntry &X, const HeaderEntry &Y) {
     return Y < X;

commit 4325cadc66506769138554fdea1a50240efd37b2

[Modularize] Make `Location::operator bool` explicit
This unbreaks C++20 buildbot that was broken since
5f35ec74eb343529ecff384c50a00caf0d237300.

With implicit conversion in C++20 compilation mode the spaceship
will unintentionally be based on `operator bool`:

```cpp
auto foo(Location L, Location R) {
  return L <=> R;
  // Equivalent to the following line due to implicit conversions.
  // return L.operator bool() <=> R.operator bool();
}
```

The spaceship operator is rarely used explicitly, but its implicit uses
in the STL may cause surprising results, as exposed by the use of  `std::tie`
in 5f35ec74eb343529ecff384c50a00caf0d237300, which ended up changing the
comparisons results unintentionally.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

clang-tools-extra/modularize/Modularize.cpp
@@ -395,7 +395,7 @@ struct Location {
     Column = SM.getColumnNumber(Decomposed.first, Decomposed.second);
   }
 
-  operator bool() const { return File != nullptr; }
+  explicit operator bool() const { return File != nullptr; }
 
   friend bool operator==(const Location &X, const Location &Y) {
     return X.File == Y.File && X.Line == Y.Line && X.Column == Y.Column;