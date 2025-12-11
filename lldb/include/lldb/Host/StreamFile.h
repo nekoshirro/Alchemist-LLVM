nekoshirro/Alchemist-LLVM

commit ea335121763d6607b84a49934ecff4aa16501263

[lldb] Synchronize the debuggers output & error streams
This patch improves the synchronization of the debugger's output and error
streams using two new abstractions: `LockableStreamFile` and
`LockedStreamFile`.

 - `LockableStreamFile` is a wrapper around a `StreamFile` and a mutex. Client
   cannot use the `StreamFile` without calling `Lock`, which returns a
   `LockedStreamFile`.

 - `LockedStreamFile` is an RAII object that locks the stream for the duration
   of its existence.  As long as you hold on to the returned object you are
   permitted to write to the stream. The destruction of the object
   automatically flush the output stream.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/include/lldb/Host/StreamFile.h
@@ -13,9 +13,12 @@
 #include "lldb/Utility/Stream.h"
 #include "lldb/lldb-defines.h"
 #include "lldb/lldb-enumerations.h"
+#include "lldb/lldb-forward.h"
 
 #include <cstdint>
 #include <cstdio>
+#include <memory>
+#include <mutex>
 
 namespace lldb_private {
 
@@ -50,6 +53,55 @@ protected:
 private:
   StreamFile(const StreamFile &) = delete;
   const StreamFile &operator=(const StreamFile &) = delete;
+};
+
+class LockableStreamFile;
+class LockedStreamFile : public StreamFile {
+public:
+  ~LockedStreamFile() { Flush(); }
+
+  LockedStreamFile(LockedStreamFile &&other)
+      : StreamFile(other.m_file_sp), m_lock(std::move(other.m_lock)) {}
+
+private:
+  LockedStreamFile(std::shared_ptr<File> file, std::recursive_mutex &mutex)
+      : StreamFile(file), m_lock(mutex) {}
+
+  friend class LockableStreamFile;
+
+  std::unique_lock<std::recursive_mutex> m_lock;
+};
+
+class LockableStreamFile {
+public:
+  using Mutex = std::recursive_mutex;
+
+  LockableStreamFile(std::shared_ptr<StreamFile> stream_file_sp, Mutex &mutex)
+      : m_file_sp(stream_file_sp->GetFileSP()), m_mutex(mutex) {}
+  LockableStreamFile(StreamFile &stream_file, Mutex &mutex)
+      : m_file_sp(stream_file.GetFileSP()), m_mutex(mutex) {}
+  LockableStreamFile(FILE *fh, bool transfer_ownership, Mutex &mutex)
+      : m_file_sp(std::make_shared<NativeFile>(fh, transfer_ownership)),
+        m_mutex(mutex) {}
+  LockableStreamFile(std::shared_ptr<File> file_sp, Mutex &mutex)
+      : m_file_sp(file_sp), m_mutex(mutex) {}
+
+  LockedStreamFile Lock() { return LockedStreamFile(m_file_sp, m_mutex); }
+
+  /// Unsafe accessors to get the underlying File without a lock. Exists for
+  /// legacy reasons.
+  /// @{
+  File &GetUnlockedFile() { return *m_file_sp; }
+  std::shared_ptr<File> GetUnlockedFileSP() { return m_file_sp; }
+  /// @}
+
+protected:
+  std::shared_ptr<File> m_file_sp;
+  Mutex &m_mutex;
+
+private:
+  LockableStreamFile(const LockableStreamFile &) = delete;
+  const LockableStreamFile &operator=(const LockableStreamFile &) = delete;
 };
 
 } // namespace lldb_private

lldb/source/Core/IOHandler.cpp
@@ -54,17 +54,17 @@ using llvm::StringRef;
 
 IOHandler::IOHandler(Debugger &debugger, IOHandler::Type type)
     : IOHandler(debugger, type,
-                FileSP(),       // Adopt STDIN from top input reader
-                StreamFileSP(), // Adopt STDOUT from top input reader
-                StreamFileSP(), // Adopt STDERR from top input reader
-                0               // Flags
+                FileSP(),               // Adopt STDIN from top input reader
+                LockableStreamFileSP(), // Adopt STDOUT from top input reader
+                LockableStreamFileSP(), // Adopt STDERR from top input reader
+                0                       // Flags
 
       ) {}
 
 IOHandler::IOHandler(Debugger &debugger, IOHandler::Type type,
                      const lldb::FileSP &input_sp,
-                     const lldb::StreamFileSP &output_sp,
-                     const lldb::StreamFileSP &error_sp, uint32_t flags)
+                     const lldb::LockableStreamFileSP &output_sp,
+                     const lldb::LockableStreamFileSP &error_sp, uint32_t flags)
     : m_debugger(debugger), m_input_sp(input_sp), m_output_sp(output_sp),
       m_error_sp(error_sp), m_popped(false), m_flags(flags), m_type(type),
       m_user_data(nullptr), m_done(false), m_active(false) {
@@ -81,30 +81,18 @@ int IOHandler::GetInputFD() {
 }
 
 int IOHandler::GetOutputFD() {
-  return (m_output_sp ? m_output_sp->GetFile().GetDescriptor() : -1);
+  return (m_output_sp ? m_output_sp->GetUnlockedFile().GetDescriptor() : -1);
 }
 
 int IOHandler::GetErrorFD() {
-  return (m_error_sp ? m_error_sp->GetFile().GetDescriptor() : -1);
-}
-
-FILE *IOHandler::GetInputFILE() {
-  return (m_input_sp ? m_input_sp->GetStream() : nullptr);
-}
-
-FILE *IOHandler::GetOutputFILE() {
-  return (m_output_sp ? m_output_sp->GetFile().GetStream() : nullptr);
-}
-
-FILE *IOHandler::GetErrorFILE() {
-  return (m_error_sp ? m_error_sp->GetFile().GetStream() : nullptr);
+  return (m_error_sp ? m_error_sp->GetUnlockedFile().GetDescriptor() : -1);
 }
 
 FileSP IOHandler::GetInputFileSP() { return m_input_sp; }
 
-StreamFileSP IOHandler::GetOutputStreamFileSP() { return m_output_sp; }
+LockableStreamFileSP IOHandler::GetOutputStreamFileSP() { return m_output_sp; }
 
-StreamFileSP IOHandler::GetErrorStreamFileSP() { return m_error_sp; }
+LockableStreamFileSP IOHandler::GetErrorStreamFileSP() { return m_error_sp; }
 
 bool IOHandler::GetIsInteractive() {
   return GetInputFileSP() ? GetInputFileSP()->GetIsInteractive() : false;
@@ -119,10 +107,9 @@ void IOHandler::SetPopped(bool b) { m_popped.SetValue(b, eBroadcastOnChange); }
 void IOHandler::WaitForPop() { m_popped.WaitForValueEqualTo(true); }
 
 void IOHandler::PrintAsync(const char *s, size_t len, bool is_stdout) {
-  std::lock_guard<std::recursive_mutex> guard(m_output_mutex);
-  lldb::StreamFileSP stream = is_stdout ? m_output_sp : m_error_sp;
-  stream->Write(s, len);
-  stream->Flush();
+  lldb::LockableStreamFileSP stream_sp = is_stdout ? m_output_sp : m_error_sp;
+  LockedStreamFile locked_Stream = stream_sp->Lock();
+  locked_Stream.Write(s, len);
 }
 
 bool IOHandlerStack::PrintAsync(const char *s, size_t len, bool is_stdout) {
@@ -228,19 +215,20 @@ IOHandlerEditline::IOHandlerEditline(
     llvm::StringRef prompt, llvm::StringRef continuation_prompt,
     bool multi_line, bool color, uint32_t line_number_start,
     IOHandlerDelegate &delegate)
-    : IOHandlerEditline(debugger, type,
-                        FileSP(),       // Inherit input from top input reader
-                        StreamFileSP(), // Inherit output from top input reader
-                        StreamFileSP(), // Inherit error from top input reader
-                        0,              // Flags
-                        editline_name,  // Used for saving history files
-                        prompt, continuation_prompt, multi_line, color,
-                        line_number_start, delegate) {}
+    : IOHandlerEditline(
+          debugger, type,
+          FileSP(),               // Inherit input from top input reader
+          LockableStreamFileSP(), // Inherit output from top input reader
+          LockableStreamFileSP(), // Inherit error from top input reader
+          0,                      // Flags
+          editline_name,          // Used for saving history files
+          prompt, continuation_prompt, multi_line, color, line_number_start,
+          delegate) {}
 
 IOHandlerEditline::IOHandlerEditline(
     Debugger &debugger, IOHandler::Type type, const lldb::FileSP &input_sp,
-    const lldb::StreamFileSP &output_sp, const lldb::StreamFileSP &error_sp,
-    uint32_t flags,
+    const lldb::LockableStreamFileSP &output_sp,
+    const lldb::LockableStreamFileSP &error_sp, uint32_t flags,
     const char *editline_name, // Used for saving history files
     llvm::StringRef prompt, llvm::StringRef continuation_prompt,
     bool multi_line, bool color, uint32_t line_number_start,
@@ -256,15 +244,12 @@ IOHandlerEditline::IOHandlerEditline(
   SetPrompt(prompt);
 
 #if LLDB_ENABLE_LIBEDIT
-  bool use_editline = false;
-
-  use_editline = GetInputFILE() && GetOutputFILE() && GetErrorFILE() &&
-                 m_input_sp && m_input_sp->GetIsRealTerminal();
-
+  const bool use_editline = m_input_sp && m_output_sp && m_error_sp &&
+                            m_input_sp->GetIsRealTerminal();
   if (use_editline) {
-    m_editline_up = std::make_unique<Editline>(editline_name, GetInputFILE(),
-                                               GetOutputFILE(), GetErrorFILE(),
-                                               m_color, GetOutputMutex());
+    m_editline_up = std::make_unique<Editline>(
+        editline_name, m_input_sp ? m_input_sp->GetStream() : nullptr,
+        m_output_sp, m_error_sp, m_color);
     m_editline_up->SetIsInputCompleteCallback(
         [this](Editline *editline, StringList &lines) {
           return this->IsInputCompleteCallback(editline, lines);
@@ -366,8 +351,8 @@ bool IOHandlerEditline::GetLine(std::string &line, bool &interrupted) {
 
     if (prompt && prompt[0]) {
       if (m_output_sp) {
-        m_output_sp->Printf("%s", prompt);
-        m_output_sp->Flush();
+        LockedStreamFile locked_stream = m_output_sp->Lock();
+        locked_stream.Printf("%s", prompt);
       }
     }
   }
@@ -380,7 +365,7 @@ bool IOHandlerEditline::GetLine(std::string &line, bool &interrupted) {
     return false;
   }
 
-  FILE *in = GetInputFILE();
+  FILE *in = m_input_sp ? m_input_sp->GetStream() : nullptr;
   char buffer[256];
 
   if (!got_line && !in && m_input_sp) {
@@ -545,9 +530,10 @@ bool IOHandlerEditline::GetLines(StringList &lines, bool &interrupted) {
       std::string line;
       if (m_base_line_number > 0 && GetIsInteractive()) {
         if (m_output_sp) {
-          m_output_sp->Printf("%u%s",
-                              m_base_line_number + (uint32_t)lines.GetSize(),
-                              GetPrompt() == nullptr ? " " : "");
+          LockedStreamFile locked_stream = m_output_sp->Lock();
+          locked_stream.Printf("%u%s",
+                               m_base_line_number + (uint32_t)lines.GetSize(),
+                               GetPrompt() == nullptr ? " " : "");
         }
       }
 
@@ -630,9 +616,8 @@ void IOHandlerEditline::GotEOF() {
 void IOHandlerEditline::PrintAsync(const char *s, size_t len, bool is_stdout) {
 #if LLDB_ENABLE_LIBEDIT
   if (m_editline_up) {
-    std::lock_guard<std::recursive_mutex> guard(m_output_mutex);
-    lldb::StreamFileSP stream = is_stdout ? m_output_sp : m_error_sp;
-    m_editline_up->PrintAsync(stream.get(), s, len);
+    lldb::LockableStreamFileSP stream_sp = is_stdout ? m_output_sp : m_error_sp;
+    m_editline_up->PrintAsync(stream_sp, s, len);
   } else
 #endif
   {

commit 36a24674d87efcb906103707321305c29a5f82d8

[lldb] Correct use_editline check in IOHandlerEditline (#171733)
Correct the use_editline check in IOHandlerEditline to prevent a crash
when we have an output and/or error file, but no stream. This fixes a
regression introduced by ea33512 that results in a crash when calling
el_init with a NULL stream.

The original code was checking the stream: GetOutputFILE and
GetErrorFILE.

```
use_editline = GetInputFILE() && GetOutputFILE() && GetErrorFILE() &&
               m_input_sp && m_input_sp->GetIsRealTerminal();
```

The new code is checking the file: `m_output_sp` and `m_error_sp`.

```
use_editline = m_input_sp && m_output_sp && m_error_sp &&
               m_input_sp->GetIsRealTerminal();
```
The correct check is:

```
use_editline =
    m_input_sp && m_input_sp->GetIsRealTerminal() &&
    m_output_sp && m_output_sp->GetUnlockedFile().GetStream() &&
    m_error_sp && m_error_sp->GetUnlockedFile().GetStream();
```

We don't need to update the check for the input, because we're handling
the missing stream there correctly in the call to the constructor:

```
m_editline_up = std::make_unique<Editline>(
    editline_name, m_input_sp ? m_input_sp->GetStream() : nullptr,
    m_output_sp, m_error_sp, m_color);
```

We can't do the same for the output and error because we need to pass
the file, not the stream (to do proper locking).

As I was debugging this I added some more assertions. They're generally
useful so I'm keeping them.

Fixes #170891
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/include/lldb/Host/StreamFile.h
@@ -92,7 +92,10 @@ public:
   /// Unsafe accessors to get the underlying File without a lock. Exists for
   /// legacy reasons.
   /// @{
-  File &GetUnlockedFile() { return *m_file_sp; }
+  File &GetUnlockedFile() {
+    assert(m_file_sp && "GetUnlockedFile requires a valid FileSP");
+    return *m_file_sp;
+  }
   std::shared_ptr<File> GetUnlockedFileSP() { return m_file_sp; }
   /// @}
 

lldb/source/Core/IOHandler.cpp
@@ -245,8 +245,13 @@ IOHandlerEditline::IOHandlerEditline(
   SetPrompt(prompt);
 
 #if LLDB_ENABLE_LIBEDIT
-  const bool use_editline = m_input_sp && m_output_sp && m_error_sp &&
-                            m_input_sp->GetIsRealTerminal();
+  // To use Editline, we need an input, output, and error stream. Not all valid
+  // files will have a FILE* stream. Don't use Editline if the input is not a
+  // real terminal.
+  const bool use_editline =
+      m_input_sp && m_input_sp->GetIsRealTerminal() &&             // Input
+      m_output_sp && m_output_sp->GetUnlockedFile().GetStream() && // Output
+      m_error_sp && m_error_sp->GetUnlockedFile().GetStream();     // Error
   if (use_editline) {
     m_editline_up = std::make_unique<Editline>(
         editline_name, m_input_sp ? m_input_sp->GetStream() : nullptr,

lldb/source/Host/common/Editline.cpp
@@ -1511,7 +1511,8 @@ Editline::Editline(const char *editline_name, FILE *input_file,
     : m_editor_status(EditorStatus::Complete), m_input_file(input_file),
       m_output_stream_sp(output_stream_sp), m_error_stream_sp(error_stream_sp),
       m_input_connection(fileno(input_file), false), m_color(color) {
-  assert(output_stream_sp && error_stream_sp);
+  assert(output_stream_sp && output_stream_sp->GetUnlockedFile().GetStream());
+  assert(error_stream_sp && output_stream_sp->GetUnlockedFile().GetStream());
   // Get a shared history instance
   m_editor_name = (editline_name == nullptr) ? "lldb-tmp" : editline_name;
   m_history_sp = EditlineHistory::GetHistory(m_editor_name);