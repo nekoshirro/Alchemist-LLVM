llvm/llvm-project

commit 7fe5953a44bbce16c0fcc5b52e47905c1719b7b3

[lldb][windows] add Windows Virtual Console support (#168729)

lldb/source/Host/windows/ProcessLauncherWindows.cpp
@@ -9,6 +9,8 @@
 #include "lldb/Host/windows/ProcessLauncherWindows.h"
 #include "lldb/Host/HostProcess.h"
 #include "lldb/Host/ProcessLaunchInfo.h"
+#include "lldb/Host/windows/PseudoConsole.h"
+#include "lldb/Host/windows/windows.h"
 
 #include "llvm/ADT/ScopeExit.h"
 #include "llvm/ADT/SmallVector.h"
@@ -87,8 +89,12 @@ ProcessLauncherWindows::LaunchProcess(const ProcessLaunchInfo &launch_info,
   error.Clear();
 
   STARTUPINFOEXW startupinfoex = {};
-  startupinfoex.StartupInfo.cb = sizeof(startupinfoex);
+  startupinfoex.StartupInfo.cb = sizeof(STARTUPINFOEXW);
   startupinfoex.StartupInfo.dwFlags |= STARTF_USESTDHANDLES;
+
+  HPCON hPC = launch_info.GetPTY().GetPseudoTerminalHandle();
+  bool use_pty =
+      hPC != INVALID_HANDLE_VALUE && launch_info.GetNumFileActions() == 0;
 
   HANDLE stdin_handle = GetStdioHandle(launch_info, STDIN_FILENO);
   HANDLE stdout_handle = GetStdioHandle(launch_info, STDOUT_FILENO);
@@ -120,13 +126,23 @@ ProcessLauncherWindows::LaunchProcess(const ProcessLaunchInfo &launch_info,
   auto delete_attributelist = llvm::make_scope_exit(
       [&] { DeleteProcThreadAttributeList(startupinfoex.lpAttributeList); });
 
-  auto inherited_handles_or_err = GetInheritedHandles(
-      launch_info, startupinfoex, stdout_handle, stderr_handle, stdin_handle);
-  if (!inherited_handles_or_err) {
-    error = Status(inherited_handles_or_err.getError());
-    return HostProcess();
+  std::vector<HANDLE> inherited_handles;
+  if (use_pty) {
+    if (!UpdateProcThreadAttribute(startupinfoex.lpAttributeList, 0,
+                                   PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, hPC,
+                                   sizeof(hPC), NULL, NULL)) {
+      error = Status(::GetLastError(), eErrorTypeWin32);
+      return HostProcess();
+    }
+  } else {
+    auto inherited_handles_or_err = GetInheritedHandles(
+        launch_info, startupinfoex, stdout_handle, stderr_handle, stdin_handle);
+    if (!inherited_handles_or_err) {
+      error = Status(inherited_handles_or_err.getError());
+      return HostProcess();
+    }
+    inherited_handles = std::move(*inherited_handles_or_err);
   }
-  std::vector<HANDLE> inherited_handles = *inherited_handles_or_err;
 
   const char *hide_console_var =
       getenv("LLDB_LAUNCH_INFERIORS_WITHOUT_CONSOLE");
@@ -141,7 +157,7 @@ ProcessLauncherWindows::LaunchProcess(const ProcessLaunchInfo &launch_info,
   if (launch_info.GetFlags().Test(eLaunchFlagDebug))
     flags |= DEBUG_ONLY_THIS_PROCESS;
 
-  if (launch_info.GetFlags().Test(eLaunchFlagDisableSTDIO))
+  if (launch_info.GetFlags().Test(eLaunchFlagDisableSTDIO) || use_pty)
     flags &= ~CREATE_NEW_CONSOLE;
 
   std::vector<wchar_t> environment =

lldb/include/lldb/Host/ProcessLaunchInfo.h
@@ -17,11 +17,21 @@
 
 #include "lldb/Host/FileAction.h"
 #include "lldb/Host/Host.h"
+#ifdef _WIN32
+#include "lldb/Host/windows/PseudoConsole.h"
+#else
 #include "lldb/Host/PseudoTerminal.h"
+#endif
 #include "lldb/Utility/FileSpec.h"
 #include "lldb/Utility/ProcessInfo.h"
 
 namespace lldb_private {
+
+#if defined(_WIN32)
+using PTY = PseudoConsole;
+#else
+using PTY = PseudoTerminal;
+#endif
 
 // ProcessLaunchInfo
 //
@@ -118,7 +128,9 @@ public:
 
   bool MonitorProcess() const;
 
-  PseudoTerminal &GetPTY() { return *m_pty; }
+  PTY &GetPTY() const { return *m_pty; }
+
+  std::shared_ptr<PTY> GetPTYSP() const { return m_pty; }
 
   void SetLaunchEventData(const char *data) { m_event_data.assign(data); }
 
@@ -136,7 +148,7 @@ protected:
   FileSpec m_shell;
   Flags m_flags; // Bitwise OR of bits from lldb::LaunchFlags
   std::vector<FileAction> m_file_actions; // File actions for any other files
-  std::shared_ptr<PseudoTerminal> m_pty;
+  std::shared_ptr<PTY> m_pty;
   uint32_t m_resume_count = 0; // How many times do we resume after launching
   Host::MonitorChildProcessCallback m_monitor_callback;
   std::string m_event_data; // A string passed to the plugin launch, having no

lldb/include/lldb/Host/windows/PseudoConsole.h
@@ -1,0 +1,63 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LIBLLDB_HOST_WINDOWS_PSEUDOCONSOLE_H_
+#define LIBLLDB_HOST_WINDOWS_PSEUDOCONSOLE_H_
+
+#include "llvm/Support/Error.h"
+#include <string>
+
+#define PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE 0x20016
+typedef void *HANDLE;
+typedef void *HPCON;
+
+namespace lldb_private {
+
+class PseudoConsole {
+
+public:
+  llvm::Error OpenPseudoConsole();
+
+  /// Close the ConPTY, its read/write handles and invalidate them.
+  void Close();
+
+  /// The ConPTY HPCON handle accessor.
+  ///
+  /// This object retains ownership of the HPCON when this accessor is used.
+  ///
+  /// \return
+  ///     The ConPTY HPCON handle, or INVALID_HANDLE_VALUE if it is currently
+  ///     invalid.
+  HPCON GetPseudoTerminalHandle() { return m_conpty_handle; };
+
+  /// The STDOUT read HANDLE accessor.
+  ///
+  /// This object retains ownership of the HANDLE when this accessor is used.
+  ///
+  /// \return
+  ///     The STDOUT read HANDLE, or INVALID_HANDLE_VALUE if it is currently
+  ///     invalid.
+  HANDLE GetSTDOUTHandle() const { return m_conpty_output; };
+
+  /// The STDIN write HANDLE accessor.
+  ///
+  /// This object retains ownership of the HANDLE when this accessor is used.
+  ///
+  /// \return
+  ///     The STDIN write HANDLE, or INVALID_HANDLE_VALUE if it is currently
+  ///     invalid.
+  HANDLE GetSTDINHandle() const { return m_conpty_input; };
+
+protected:
+  HANDLE m_conpty_handle = ((HANDLE)(long long)-1);
+  HANDLE m_conpty_output = ((HANDLE)(long long)-1);
+  HANDLE m_conpty_input = ((HANDLE)(long long)-1);
+};
+}; // namespace lldb_private
+
+#endif // LIBLLDB_HOST_WINDOWS_PSEUDOCONSOLE_H_

lldb/include/lldb/Target/Process.h
@@ -2534,6 +2534,30 @@ void PruneThreadPlans();
 
   void CalculateExecutionContext(ExecutionContext &exe_ctx) override;
 
+#ifdef _WIN32
+  /// Associates a ConPTY read and write HANDLEs with the process' STDIO
+  /// handling and configures an asynchronous reading of that ConPTY's stdout
+  /// HANDLE.
+  ///
+  /// This method installs a ConnectionGenericFile for the passed ConPTY and
+  /// starts a dedicated read thread. If the read thread starts successfully,
+  /// the method also ensures that an IOHandlerProcessSTDIOWindows is created to
+  /// manage user input to the process.
+  ///
+  /// When data is successfully read from the ConPTY, it is stored in
+  /// m_stdout_data. There is no differentiation between stdout and stderr.
+  ///
+  /// \param[in] pty
+  ///     The ConPTY to use for process STDIO communication. It's
+  ///     assumed to be valid.
+  ///
+  /// \see lldb_private::Process::STDIOReadThreadBytesReceived()
+  /// \see lldb_private::IOHandlerProcessSTDIOWindows
+  /// \see lldb_private::PseudoConsole
+  virtual void
+  SetPseudoConsoleHandle(const std::shared_ptr<PseudoConsole> &pty) {};
+#endif
+
   /// Associates a file descriptor with the process' STDIO handling
   /// and configures an asynchronous reading of that descriptor.
   ///

lldb/packages/Python/lldbsuite/test/decorators.py
@@ -781,9 +781,35 @@ def skipIfLinux(func):
     return skipIfPlatform(["linux"])(func)
 
 
-def skipIfWindows(func):
+def skipIfWindows(func=None, major=None, build=None):
     """Decorate the item to skip tests that should be skipped on Windows."""
-    return skipIfPlatform(["windows"])(func)
+
+    def decorator(func):
+        if major is None and build is None:
+            return skipIfPlatform(["windows"])(func)
+        else:
+            import platform
+            import sys
+
+            def version_check():
+                check_major = 0 if major is None else major
+                check_build = 0 if build is None else build
+                if platform.system() != "Windows":
+                    return False
+                win_version = sys.getwindowsversion()
+                return (
+                    win_version.major >= check_major
+                    and win_version.build >= check_build
+                )
+
+            return unittest.skipIf(
+                version_check(),
+                f"Test is skipped on Windows major={major} build={build}",
+            )(func)
+
+    if func is not None:
+        return decorator(func)
+    return decorator
 
 
 def skipIfWindowsAndNonEnglish(func):

lldb/source/Host/CMakeLists.txt
@@ -76,6 +76,7 @@ if (CMAKE_SYSTEM_NAME MATCHES "Windows")
     windows/MainLoopWindows.cpp
     windows/PipeWindows.cpp
     windows/ProcessLauncherWindows.cpp
+    windows/PseudoConsole.cpp
     windows/ProcessRunLock.cpp
     )
 else()

lldb/source/Host/common/ProcessLaunchInfo.cpp
@@ -20,7 +20,9 @@
 #include "llvm/Support/ConvertUTF.h"
 #include "llvm/Support/FileSystem.h"
 
-#if !defined(_WIN32)
+#ifdef _WIN32
+#include "lldb/Host/windows/PseudoConsole.h"
+#else
 #include <climits>
 #endif
 
@@ -31,8 +33,7 @@ using namespace lldb_private;
 
 ProcessLaunchInfo::ProcessLaunchInfo()
     : ProcessInfo(), m_working_dir(), m_plugin_name(), m_flags(0),
-      m_file_actions(), m_pty(new PseudoTerminal), m_monitor_callback(nullptr) {
-}
+      m_file_actions(), m_pty(new PTY), m_monitor_callback(nullptr) {}
 
 ProcessLaunchInfo::ProcessLaunchInfo(const FileSpec &stdin_file_spec,
                                      const FileSpec &stdout_file_spec,
@@ -40,7 +41,7 @@ ProcessLaunchInfo::ProcessLaunchInfo(const FileSpec &stdin_file_spec,
                                      const FileSpec &working_directory,
                                      uint32_t launch_flags)
     : ProcessInfo(), m_working_dir(), m_plugin_name(), m_flags(launch_flags),
-      m_file_actions(), m_pty(new PseudoTerminal) {
+      m_file_actions(), m_pty(new PTY) {
   if (stdin_file_spec) {
     FileAction file_action;
     const bool read = true;
@@ -208,13 +209,12 @@ llvm::Error ProcessLaunchInfo::SetUpPtyRedirection() {
 
   LLDB_LOG(log, "Generating a pty to use for stdin/out/err");
 
-  int open_flags = O_RDWR | O_NOCTTY;
-#if !defined(_WIN32)
-  // We really shouldn't be specifying platform specific flags that are
-  // intended for a system call in generic code.  But this will have to
-  // do for now.
-  open_flags |= O_CLOEXEC;
-#endif
+#ifdef _WIN32
+  if (llvm::Error Err = m_pty->OpenPseudoConsole())
+    return Err;
+  return llvm::Error::success();
+#else
+  int open_flags = O_RDWR | O_NOCTTY | O_CLOEXEC;
   if (llvm::Error Err = m_pty->OpenFirstAvailablePrimary(open_flags))
     return Err;
 
@@ -229,6 +229,7 @@ llvm::Error ProcessLaunchInfo::SetUpPtyRedirection() {
   if (stderr_free)
     AppendOpenFileAction(STDERR_FILENO, secondary_file_spec, false, true);
   return llvm::Error::success();
+#endif
 }
 
 bool ProcessLaunchInfo::ConvertArgumentsForLaunchingInShell(

lldb/source/Host/windows/PseudoConsole.cpp
@@ -1,0 +1,126 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "lldb/Host/windows/PseudoConsole.h"
+
+#include <mutex>
+
+#include "lldb/Host/windows/PipeWindows.h"
+#include "lldb/Host/windows/windows.h"
+#include "lldb/Utility/LLDBLog.h"
+
+#include "llvm/Support/Errc.h"
+#include "llvm/Support/Errno.h"
+
+using namespace lldb_private;
+
+typedef HRESULT(WINAPI *CreatePseudoConsole_t)(COORD size, HANDLE hInput,
+                                               HANDLE hOutput, DWORD dwFlags,
+                                               HPCON *phPC);
+
+typedef VOID(WINAPI *ClosePseudoConsole_t)(HPCON hPC);
+
+struct Kernel32 {
+  Kernel32() {
+    hModule = LoadLibraryW(L"kernel32.dll");
+    if (!hModule) {
+      llvm::Error err = llvm::errorCodeToError(
+          std::error_code(GetLastError(), std::system_category()));
+      LLDB_LOG_ERROR(GetLog(LLDBLog::Host), std::move(err),
+                     "Could not load kernel32: {0}");
+      return;
+    }
+    CreatePseudoConsole_ =
+        (CreatePseudoConsole_t)GetProcAddress(hModule, "CreatePseudoConsole");
+    ClosePseudoConsole_ =
+        (ClosePseudoConsole_t)GetProcAddress(hModule, "ClosePseudoConsole");
+    isAvailable = (CreatePseudoConsole_ && ClosePseudoConsole_);
+  }
+
+  HRESULT CreatePseudoConsole(COORD size, HANDLE hInput, HANDLE hOutput,
+                              DWORD dwFlags, HPCON *phPC) {
+    assert(CreatePseudoConsole_ && "CreatePseudoConsole is not available!");
+    return CreatePseudoConsole_(size, hInput, hOutput, dwFlags, phPC);
+  }
+
+  VOID ClosePseudoConsole(HPCON hPC) {
+    assert(ClosePseudoConsole_ && "ClosePseudoConsole is not available!");
+    return ClosePseudoConsole_(hPC);
+  }
+
+  bool IsConPTYAvailable() { return isAvailable; }
+
+private:
+  HMODULE hModule;
+  CreatePseudoConsole_t CreatePseudoConsole_;
+  ClosePseudoConsole_t ClosePseudoConsole_;
+  bool isAvailable;
+};
+
+static Kernel32 kernel32;
+
+llvm::Error PseudoConsole::OpenPseudoConsole() {
+  if (!kernel32.IsConPTYAvailable())
+    return llvm::make_error<llvm::StringError>("ConPTY is not available",
+                                               llvm::errc::io_error);
+  HRESULT hr;
+  HANDLE hInputRead = INVALID_HANDLE_VALUE;
+  HANDLE hInputWrite = INVALID_HANDLE_VALUE;
+  HANDLE hOutputRead = INVALID_HANDLE_VALUE;
+  HANDLE hOutputWrite = INVALID_HANDLE_VALUE;
+
+  wchar_t pipe_name[MAX_PATH];
+  swprintf(pipe_name, MAX_PATH, L"\\\\.\\pipe\\conpty-lldb-%d-%p",
+           GetCurrentProcessId(), this);
+
+  // A 4096 bytes buffer should be large enough for the majority of console
+  // burst outputs.
+  hOutputRead =
+      CreateNamedPipeW(pipe_name, PIPE_ACCESS_INBOUND | FILE_FLAG_OVERLAPPED,
+                       PIPE_TYPE_BYTE | PIPE_WAIT, 1, 4096, 4096, 0, NULL);
+  hOutputWrite = CreateFileW(pipe_name, GENERIC_WRITE, 0, NULL, OPEN_EXISTING,
+                             FILE_ATTRIBUTE_NORMAL, NULL);
+
+  if (!CreatePipe(&hInputRead, &hInputWrite, NULL, 0))
+    return llvm::errorCodeToError(
+        std::error_code(GetLastError(), std::system_category()));
+
+  COORD consoleSize{80, 25};
+  HPCON hPC = INVALID_HANDLE_VALUE;
+  hr = kernel32.CreatePseudoConsole(consoleSize, hInputRead, hOutputWrite, 0,
+                                    &hPC);
+  CloseHandle(hInputRead);
+  CloseHandle(hOutputWrite);
+
+  if (FAILED(hr)) {
+    CloseHandle(hInputWrite);
+    CloseHandle(hOutputRead);
+    return llvm::make_error<llvm::StringError>(
+        "Failed to create Windows ConPTY pseudo terminal",
+        llvm::errc::io_error);
+  }
+
+  DWORD mode = PIPE_NOWAIT;
+  SetNamedPipeHandleState(hOutputRead, &mode, NULL, NULL);
+
+  m_conpty_handle = hPC;
+  m_conpty_output = hOutputRead;
+  m_conpty_input = hInputWrite;
+
+  return llvm::Error::success();
+}
+
+void PseudoConsole::Close() {
+  if (m_conpty_handle != INVALID_HANDLE_VALUE)
+    kernel32.ClosePseudoConsole(m_conpty_handle);
+  CloseHandle(m_conpty_input);
+  CloseHandle(m_conpty_output);
+  m_conpty_handle = INVALID_HANDLE_VALUE;
+  m_conpty_input = INVALID_HANDLE_VALUE;
+  m_conpty_output = INVALID_HANDLE_VALUE;
+}

nekoshirro/Alchemist-LLVM

commit 178a072fdb30010713744ee8151fd481a5b4c82b

[lldb][windows] only use a ConPTY if needed (#172877)
This patch fixes a regression caused by
https://github.com/llvm/llvm-project/pull/168729.

The changes ensure that we only create the process with a ConPTY if we
need to.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Host/windows/ProcessLauncherWindows.cpp
@@ -93,8 +93,9 @@ ProcessLauncherWindows::LaunchProcess(const ProcessLaunchInfo &launch_info,
   startupinfoex.StartupInfo.dwFlags |= STARTF_USESTDHANDLES;
 
   HPCON hPC = launch_info.GetPTY().GetPseudoTerminalHandle();
-  bool use_pty =
-      hPC != INVALID_HANDLE_VALUE && launch_info.GetNumFileActions() == 0;
+  bool use_pty = hPC != INVALID_HANDLE_VALUE &&
+                 launch_info.GetNumFileActions() == 0 &&
+                 launch_info.GetFlags().Test(lldb::eLaunchFlagLaunchInTTY);
 
   HANDLE stdin_handle = GetStdioHandle(launch_info, STDIN_FILENO);
   HANDLE stdout_handle = GetStdioHandle(launch_info, STDOUT_FILENO);