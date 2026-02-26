nekoshirro/Alchemist-LLVM

commit b0aee8efebafbb0a4da278e610538908838d206e

[clang][Driver][SPIRV][ClangLinkerWrapper] Pass --allow-partial-linkage to spirv-link (#181870)
Without this flag a lot of tests error in the linker.

---------

Signed-off-by: Nick Sarnie <nick.sarnie@intel.com>

clang/lib/Driver/ToolChains/Clang.cpp
@@ -9307,6 +9307,11 @@ void LinkerWrapper::ConstructJob(Compilation &C, const JobAction &JA,
           (TC->getTriple().isAMDGPU() || TC->getTriple().isNVPTX()))
         LinkerArgs.emplace_back("-lompdevice");
 
+      // For SPIR-V some functions will be defined by the runtime so allow
+      // unresolved symbols.
+      if (TC->getTriple().isSPIRV())
+        LinkerArgs.emplace_back("--allow-partial-linkage");
+
       // Forward all of these to the appropriate toolchain.
       for (StringRef Arg : CompilerArgs)
         CmdArgs.push_back(Args.MakeArgString(

clang/test/Driver/spirv-openmp-toolchain.c
@@ -65,7 +65,7 @@
 // RUN:        -nogpulib %s 2>&1 \
 // RUN: | FileCheck %s --check-prefix=CHECK-LINKER-ARG
 // CHECK-LINKER-ARG: clang-linker-wrapper
-// CHECK-LINKER-ARG-NOT: --device-linker=spirv64
+// CHECK-LINKER: --device-linker=spirv64-intel=--allow-partial-linkage"
 // CHECK-LINKER-ARG-NOT: -mllvm
 // CHECK-LINKER-ARG-NOT: --spirv-ext=+SPV_INTEL_function_pointers
 // CHECK-LINKER-ARG: --linker-path

commit d6a83b1d4289f6c8c4b33e551cec1b2cffb777a9

[Driver][SPIRV] Fix SPIR-V build for AMD. (#183529)
The AMD path doesn't use spirv-link, and the driver was incorrectly
adding flags for it, which broke the build. The regression was
introduced in
https://github.com/llvm/llvm-project/commit/b0aee8efebafbb0a4da278e610538908838d206e.

Without this PR's fix, the following assertion is triggered
[here](https://github.com/llvm/llvm-project/blob/46b1992daa3f167eaf98577dc18dfd83eb3b2816/clang/lib/Driver/ToolChains/HIPAMD.cpp#L43)
when the LLVM bitcode is linked because unexpected flags are forwarded:
```clang::driver::InputInfo::getFilename() const: Assertion
`isFilename() && "Invalid accessor."' failed.```

The bug was triggered by:
`clang++ --offload-new-driver -x hip -O3 --offload-arch=amdgcnspirv
-use-spirv-backend -fvisibility=default -v ./repro/foo.hip -o out
-save-temps`.

This eventually invoked: `clang --no-default-config -o
foo.hip-hip-spirv64-amd-amdhsa.hipfb.spirv64.amdgcnspirv.img -dumpdir
foo.hip-hip-spirv64-amd-amdhsa.hipfb.spirv64.amdgcnspirv.img.
--target=spirv64-amd-amdhsa -march=amdgcnspirv
foo-hip-spirv64-amd-amdhsa-spirv64-amd-amdhsa-amdgcnspirv.o -Xlinker
--allow-partial-linkage -O3 -use-spirv-backend -save-temps=cwd
-flto-partitions=8 -###`.
It triggered the crash due to the incorrect flags.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

clang/lib/Driver/ToolChains/Clang.cpp
@@ -9374,8 +9374,11 @@ void LinkerWrapper::ConstructJob(Compilation &C, const JobAction &JA,
       // For SPIR-V, pass some extra flags to `spirv-link`, the out-of-tree
       // SPIR-V linker. `spirv-link` isn't called in LTO mode so restrict these
       // flags to normal compilation.
-      if (TC->getTriple().isSPIRV() && !C.getDriver().isUsingLTO() &&
-          !C.getDriver().isUsingOffloadLTO()) {
+      // SPIR-V for AMD doesn't use spirv-link and therefore doesn't need these
+      // flags.
+      if (TC->getTriple().isSPIRV() &&
+          TC->getTriple().getVendor() != llvm::Triple::VendorType::AMD &&
+          !C.getDriver().isUsingLTO() && !C.getDriver().isUsingOffloadLTO()) {
         // For SPIR-V some functions will be defined by the runtime so allow
         // unresolved symbols in `spirv-link`.
         LinkerArgs.emplace_back("--allow-partial-linkage");