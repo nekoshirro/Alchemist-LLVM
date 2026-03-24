nekoshirro/Alchemist-LLVM

commit b9e94471e5d6777e4b98d481cf2338819b3e7a29

[lldb] Fix trace load hang (#187768)
#179799 removed the `SetPrivateState(eStateStopped)` call in
`ProcessTrace::DidAttach()`. This makes the call to
`WaitForProcessToStop` hang forever, causing the `trace load` command to
hang.

This fix reintroduces the `SetPrivateState` call so a postmortem trace
process will "stop" after being loaded, matching the logic used in
`Process::LoadCore()`.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Target/ProcessTrace.cpp
@@ -72,6 +72,10 @@ void ProcessTrace::DidAttach(ArchSpec &process_arch) {
     return;
   }
 
+  // Pretend we stopped so we can show all of the threads
+  // in the trace and explore the final state.
+  SetPrivateState(lldb::eStateStopped);
+
   EventSP event_sp;
   WaitForProcessToStop(std::nullopt, &event_sp, true, listener_sp);
 

lldb/unittests/Process/ProcessTraceTest.cpp
@@ -1,4 +1,4 @@
-//===-- ProcessEventDataTest.cpp ------------------------------------------===//
+//===-- ProcessTraceTest.cpp ----------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -60,4 +60,29 @@ TEST_F(ProcessTraceTest, ConstructorWithNullptrCoreFile) {
       /*can_connect*/ false);
 
   ASSERT_NE(process_sp, nullptr);
+}
+
+// Test that a trace process stops after attaching.
+TEST_F(ProcessTraceTest, DidAttach) {
+  ArchSpec arch("i386-pc-linux");
+
+  Platform::SetHostPlatform(PlatformLinux::CreateInstance(true, &arch));
+  ASSERT_NE(Platform::GetHostPlatform(), nullptr);
+
+  DebuggerSP debugger_sp = Debugger::CreateInstance();
+  ASSERT_TRUE(debugger_sp);
+
+  TargetSP target_sp = CreateTarget(debugger_sp, arch);
+  ASSERT_TRUE(target_sp);
+
+  ProcessSP process_sp = target_sp->CreateProcess(
+      /*listener*/ nullptr, "trace",
+      /*crash_file*/ nullptr,
+      /*can_connect*/ false);
+
+  ASSERT_NE(process_sp, nullptr);
+
+  process_sp->DidAttach(arch);
+
+  ASSERT_EQ(process_sp->GetState(), lldb::eStateStopped);
 }

commit 360684db506d9b9a3b9744ee88dcabca7a0646de

[lldb][trace] Ensure ProcessTrace plugin can be re-registered (#188336)
Initialize makes sure that it calls RegisterPlugin only once, but
Terminate always calls UnregisterPlugin. This is a problem for tests
that call Initialize/Terminate before and after each test case: the
second case will fail because the trace plugin won't be loaded.

This fixes a test failure introduced by #187768, which adds a test case
that passes on its own but fails when run after the previous test case.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Target/ProcessTrace.cpp
@@ -106,12 +106,8 @@ size_t ProcessTrace::ReadMemory(addr_t addr, void *buf, size_t size,
 void ProcessTrace::Clear() { m_thread_list.Clear(); }
 
 void ProcessTrace::Initialize() {
-  static llvm::once_flag g_once_flag;
-
-  llvm::call_once(g_once_flag, []() {
-    PluginManager::RegisterPlugin(GetPluginNameStatic(),
-                                  GetPluginDescriptionStatic(), CreateInstance);
-  });
+  PluginManager::RegisterPlugin(GetPluginNameStatic(),
+                                GetPluginDescriptionStatic(), CreateInstance);
 }
 
 ArchSpec ProcessTrace::GetArchitecture() {