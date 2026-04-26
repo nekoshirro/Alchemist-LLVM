nekoshirro/Alchemist-LLVM

commit 0a6cf89de45c9ffa7de8f98204051bf0be2e51d4

[compiler-rt] Replace assignment w/.set directive (#107667)
These assignment statements are rejected by `llvm-mc --triple=hexagon`,
likely because the syntax so strongly resembles hexagon asm mnemonics.

The statements were being normalized by llvm-mc into `.set` directives
for most architectures, so I assume that this change is a portable one.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

compiler-rt/lib/sanitizer_common/sanitizer_redefine_builtins.h
@@ -17,9 +17,11 @@
 // The asm hack only works with GCC and Clang.
 #    if !defined(_WIN32)
 
-asm("memcpy = __sanitizer_internal_memcpy");
-asm("memmove = __sanitizer_internal_memmove");
-asm("memset = __sanitizer_internal_memset");
+asm(R"(
+    .set memcpy, __sanitizer_internal_memcpy
+    .set memmove, __sanitizer_internal_memmove
+    .set memset, __sanitizer_internal_memset
+    )");
 
 #      if defined(__cplusplus) && \
           !defined(SANITIZER_COMMON_REDEFINE_BUILTINS_IN_STD)

commit 0e1ee95e58678a0feb66c06b5b6acf8fafd4a273

[compiler-rt] Use asm .set only for Hexagon (#194160)
Two incompatible assembler syntaxes exist for symbol assignment:
```
  sym = val      -- accepted by most GNU assembler targets; rejected by
                    Hexagon, which interprets it as a mnemonic
  .set sym, val  -- accepted by Hexagon; rejected by Alpha, which
                    reserves .set for assembler mode flags
```
Switch all to `sym = val`, and opt out Hexagon to `.set sym`.

Fixes: 0a6cf89de45c ("[compiler-rt] Replace assignment w/.set directive
(#107667)")

---------

Co-authored-by: Vitaly Buka <vitalybuka@google.com>
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

compiler-rt/lib/sanitizer_common/sanitizer_redefine_builtins.h
@@ -17,11 +17,23 @@
 // The asm hack only works with GCC and Clang.
 #    if !defined(_WIN32) && !defined(_AIX) && !defined(__APPLE__)
 
-asm(R"(
-    .set memcpy, __sanitizer_internal_memcpy
-    .set memmove, __sanitizer_internal_memmove
-    .set memset, __sanitizer_internal_memset
-    )");
+#      if defined(__hexagon__)
+
+#        define SANITIZER_REDEFINE_BUILTIN_ASM(name) \
+          asm(".set " #name ", __sanitizer_internal_" #name)
+
+#      else
+
+#        define SANITIZER_REDEFINE_BUILTIN_ASM(name) \
+          asm(#name " = __sanitizer_internal_" #name)
+
+#      endif
+
+SANITIZER_REDEFINE_BUILTIN_ASM(memcpy);
+SANITIZER_REDEFINE_BUILTIN_ASM(memmove);
+SANITIZER_REDEFINE_BUILTIN_ASM(memset);
+
+#      undef SANITIZER_REDEFINE_BUILTIN_ASM
 
 #      if defined(__cplusplus) && \
           !defined(SANITIZER_COMMON_REDEFINE_BUILTINS_IN_STD)