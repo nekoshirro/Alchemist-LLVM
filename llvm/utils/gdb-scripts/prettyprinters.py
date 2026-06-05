nekoshirro/Alchemist-LLVM

commit 7d52c835f9683bcdeef4d03001a5a94f7bd3b499

[llvm] Remove unused DenseMapInfo::getTombstoneKey (#200957)
#200595 changed DenseMap to no longer create tombstone buckets, so
DenseMapInfo<T>::getTombstoneKey() is never called. Remove dead
definitions and dead tombstone branches.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/utils/gdb-scripts/prettyprinters.py
@@ -163,7 +163,6 @@ class DenseMapPrinter:
             n = self.key_info_t.name
             is_equal = gdb.parse_and_eval(n + "::isEqual")
             empty = gdb.parse_and_eval(n + "::getEmptyKey()")
-            tombstone = gdb.parse_and_eval(n + "::getTombstoneKey()")
             # the following is invalid, GDB fails with:
             #   Python Exception <class 'gdb.error'> Attempt to take address of value
             #   not located in memory.
@@ -173,10 +172,8 @@ class DenseMapPrinter:
             # member function, not the 'first' member variable, but I've yet to figure
             # out how to find/call member functions (especially (const) overloaded
             # ones) on a gdb.Value.
-            while self.cur != self.end and (
-                is_equal(self.cur.dereference()["first"], empty)
-                or is_equal(self.cur.dereference()["first"], tombstone)
-            ):
+            while self.cur != self.end and
+                is_equal(self.cur.dereference()["first"], empty):
                 self.cur = self.cur + 1
 
         def __next__(self):

llvm/include/llvm/BinaryFormat/Minidump.h
@@ -272,10 +272,6 @@ static_assert(sizeof(ExceptionStream) == 168);
 template <> struct DenseMapInfo<minidump::StreamType> {
   static minidump::StreamType getEmptyKey() { return minidump::StreamType(-1); }
 
-  static minidump::StreamType getTombstoneKey() {
-    return minidump::StreamType(-2);
-  }
-
   static unsigned getHashValue(minidump::StreamType Val) {
     return DenseMapInfo<uint32_t>::getHashValue(static_cast<uint32_t>(Val));
   }

llvm/include/llvm/BinaryFormat/Wasm.h
@@ -519,8 +519,8 @@ struct WasmSignature {
   // but does not actually model them. Instead a placeholder signature is
   // created in the Object's signature list.
   enum { Function, Tag, Placeholder } Kind = Function;
-  // Support empty and tombstone instances, needed by DenseMap.
-  enum { Plain, Empty, Tombstone } State = Plain;
+  // Support empty instances, needed by DenseMap.
+  enum { Plain, Empty } State = Plain;
 
   WasmSignature(SmallVector<ValType, 1> &&InReturns,
                 SmallVector<ValType, 4> &&InParams)

llvm/include/llvm/BinaryFormat/WasmTraits.h
@@ -25,11 +25,6 @@ template <> struct DenseMapInfo<wasm::WasmSignature, void> {
     Sig.State = wasm::WasmSignature::Empty;
     return Sig;
   }
-  static wasm::WasmSignature getTombstoneKey() {
-    wasm::WasmSignature Sig;
-    Sig.State = wasm::WasmSignature::Tombstone;
-    return Sig;
-  }
   static unsigned getHashValue(const wasm::WasmSignature &Sig) {
     uintptr_t H = hash_value(Sig.State);
     for (auto Ret : Sig.Returns)
@@ -49,9 +44,6 @@ template <> struct DenseMapInfo<wasm::WasmGlobalType, void> {
   static wasm::WasmGlobalType getEmptyKey() {
     return wasm::WasmGlobalType{1, true};
   }
-  static wasm::WasmGlobalType getTombstoneKey() {
-    return wasm::WasmGlobalType{2, true};
-  }
   static unsigned getHashValue(const wasm::WasmGlobalType &GlobalType) {
     return hash_combine(GlobalType.Type, GlobalType.Mutable);
   }
@@ -65,9 +57,6 @@ template <> struct DenseMapInfo<wasm::WasmGlobalType, void> {
 template <> struct DenseMapInfo<wasm::WasmLimits, void> {
   static wasm::WasmLimits getEmptyKey() {
     return wasm::WasmLimits{0xff, 0xff, 0xff, 0xff};
-  }
-  static wasm::WasmLimits getTombstoneKey() {
-    return wasm::WasmLimits{0xee, 0xee, 0xee, 0xee};
   }
   static unsigned getHashValue(const wasm::WasmLimits &Limits) {
     unsigned Hash = hash_value(Limits.Flags);
@@ -88,11 +77,6 @@ template <> struct DenseMapInfo<wasm::WasmTableType, void> {
   static wasm::WasmTableType getEmptyKey() {
     return wasm::WasmTableType{
         wasm::ValType(0), DenseMapInfo<wasm::WasmLimits, void>::getEmptyKey()};
-  }
-  static wasm::WasmTableType getTombstoneKey() {
-    return wasm::WasmTableType{
-        wasm::ValType(1),
-        DenseMapInfo<wasm::WasmLimits, void>::getTombstoneKey()};
   }
   static unsigned getHashValue(const wasm::WasmTableType &TableType) {
     return hash_combine(

llvm/include/llvm/CAS/CASID.h
@@ -107,7 +107,11 @@ public:
     return CASID(nullptr, DenseMapInfo<StringRef>::getEmptyKey());
   }
   static CASID getDenseMapTombstoneKey() {
-    return CASID(nullptr, DenseMapInfo<StringRef>::getTombstoneKey());
+    // A reserved StringRef value distinct from the empty key, used only as a
+    // DenseMap sentinel for CASID.
+    return CASID(nullptr, StringRef(reinterpret_cast<const char *>(
+                                        ~static_cast<uintptr_t>(1)),
+                                    0));
   }
 
   CASID() = delete;
@@ -129,10 +133,6 @@ private:
 
 template <> struct DenseMapInfo<cas::CASID> {
   static cas::CASID getEmptyKey() { return cas::CASID::getDenseMapEmptyKey(); }
-
-  static cas::CASID getTombstoneKey() {
-    return cas::CASID::getDenseMapTombstoneKey();
-  }
 
   static unsigned getHashValue(cas::CASID ID) {
     return (unsigned)hash_value(ID);

llvm/include/llvm/CAS/CASReference.h
@@ -27,7 +27,6 @@ class ObjectRef;
 class ReferenceBase {
 protected:
   struct DenseMapEmptyTag {};
-  struct DenseMapTombstoneTag {};
   static constexpr uint64_t getDenseMapEmptyRef() { return -1ULL; }
   static constexpr uint64_t getDenseMapTombstoneRef() { return -2ULL; }
 
@@ -78,8 +77,6 @@ protected:
   }
   explicit ReferenceBase(DenseMapEmptyTag)
       : InternalRef(getDenseMapEmptyRef()) {}
-  explicit ReferenceBase(DenseMapTombstoneTag)
-      : InternalRef(getDenseMapTombstoneRef()) {}
 
 private:
   uint64_t InternalRef;
@@ -119,9 +116,6 @@ public:
   static ObjectRef getDenseMapEmptyKey() {
     return ObjectRef(DenseMapEmptyTag{});
   }
-  static ObjectRef getDenseMapTombstoneKey() {
-    return ObjectRef(DenseMapTombstoneTag{});
-  }
 
   /// Print internal ref and/or CASID. Only suitable for debugging.
   void print(raw_ostream &OS) const { return ReferenceBase::print(OS, *this); }
@@ -138,7 +132,6 @@ private:
     assert(InternalRef != -2ULL && "Reserved for DenseMapInfo");
   }
   explicit ObjectRef(DenseMapEmptyTag T) : ReferenceBase(T) {}
-  explicit ObjectRef(DenseMapTombstoneTag T) : ReferenceBase(T) {}
   explicit ObjectRef(ReferenceBase) = delete;
 };
 
@@ -175,10 +168,6 @@ private:
 template <> struct DenseMapInfo<cas::ObjectRef> {
   static cas::ObjectRef getEmptyKey() {
     return cas::ObjectRef::getDenseMapEmptyKey();
-  }
-
-  static cas::ObjectRef getTombstoneKey() {
-    return cas::ObjectRef::getDenseMapTombstoneKey();
   }
 
   static unsigned getHashValue(cas::ObjectRef Ref) {

llvm/include/llvm/CodeGenTypes/LowLevelType.h
@@ -722,11 +722,6 @@ template <> struct DenseMapInfo<LLT> {
     Invalid.Info = LLT::Kind::POINTER;
     return Invalid;
   }
-  static inline LLT getTombstoneKey() {
-    LLT Invalid;
-    Invalid.Info = LLT::Kind::VECTOR_ANY;
-    return Invalid;
-  }
   static inline unsigned getHashValue(const LLT &Ty) {
     uint64_t Val = Ty.getUniqueRAWLLTData();
     return DenseMapInfo<uint64_t>::getHashValue(Val);

llvm/include/llvm/DWARFLinker/Classic/DWARFLinkerDeclContext.h
@@ -173,14 +173,13 @@ private:
 /// Info type for the DenseMap storing the DeclContext pointers.
 struct DeclMapInfo : private DenseMapInfo<DeclContext *> {
   using DenseMapInfo<DeclContext *>::getEmptyKey;
-  using DenseMapInfo<DeclContext *>::getTombstoneKey;
 
   static unsigned getHashValue(const DeclContext *Ctxt) {
     return Ctxt->QualifiedNameHash;
   }
 
   static bool isEqual(const DeclContext *LHS, const DeclContext *RHS) {
-    if (RHS == getEmptyKey() || RHS == getTombstoneKey())
+    if (RHS == getEmptyKey())
       return RHS == LHS;
     return LHS->QualifiedNameHash == RHS->QualifiedNameHash &&
            LHS->Line == RHS->Line && LHS->ByteSize == RHS->ByteSize &&

commit 8e03c32cbabfb5d8c57d03f617a7d01a11248466

[prettyprinters] Fix syntax error introduced by 7d52c835f968. (#201359) Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/utils/gdb-scripts/prettyprinters.py
@@ -172,8 +172,9 @@ class DenseMapPrinter:
             # member function, not the 'first' member variable, but I've yet to figure
             # out how to find/call member functions (especially (const) overloaded
             # ones) on a gdb.Value.
-            while self.cur != self.end and
-                is_equal(self.cur.dereference()["first"], empty):
+            while self.cur != self.end and is_equal(
+                self.cur.dereference()["first"], empty
+            ):
                 self.cur = self.cur + 1
 
         def __next__(self):