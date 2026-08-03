nekoshirro/Alchemist-LLVM

commit ab183930204e43dca3d5323ec2c40484988b3072

[LoopUnroll] Fix freqs for unconditional latches: N>2, uniform (#182405)
This patch introduces the command-line option `-unroll-uniform-weights`.
When computing probabilities for the remaining N conditional latches in
the unrolled loop after converting some iterations' latches to
unconditional, LoopUnroll now supports the following three strategies:

- A. If N <= 2, use a simple formula to compute a single uniform
  probability across those latches.
- B. Otherwise, if `-unroll-uniform-weights` is not specified, apply the
  original loop's probability to all N latches and then, as needed, adjust
  as few of them as possible.
- C. Otherwise, bisect the range [0,1] to find a single uniform
  probability across all N latches. This patch implements this strategy.

An issue with C is that it could impact compiler performance, so this
patch makes it opt-in. Its appeal over B is that it treats all latches
the same given that we have no evidence showing that any latch should
have a higher or lower probability than any other. A has neither
problem, but I do not know how to apply it for N > 2. More experience or
feedback from others might determine that some strategies are not
worthwhile to maintain.

This patch depends on PR #182404.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Transforms/Utils/LoopUnroll.cpp
@@ -90,6 +90,11 @@ UnrollRuntimeEpilog("unroll-runtime-epilog", cl::init(false), cl::Hidden,
                     cl::desc("Allow runtime unrolled loops to be unrolled "
                              "with epilog instead of prolog."));
 
+static cl::opt<bool> UnrollUniformWeights(
+    "unroll-uniform-weights", cl::init(false), cl::Hidden,
+    cl::desc("If new branch weights must be found, work harder to keep them "
+             "uniform."));
+
 static cl::opt<bool>
 UnrollVerifyDomtree("unroll-verify-domtree", cl::Hidden,
                     cl::desc("Verify domtree after unrolling"),
@@ -495,9 +500,9 @@ static bool canHaveUnrollRemainder(const Loop *L) {
 //
 // There are often many sets of latch probabilities that can produce the
 // original total loop body frequency.  If there are many remaining conditional
-// latches, this function just quickly hacks a few of their probabilities to
-// restore the original total loop body frequency.  Otherwise, it determines
-// less arbitrary probabilities.
+// latches and !UnrollUniformWeights, this function just quickly hacks a few of
+// their probabilities to restore the original total loop body frequency.
+// Otherwise, it tries harder to determine less arbitrary probabilities.
 static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
                                  OptimizationRemarkEmitter *ORE,
                                  BranchProbability OriginalLoopProb,
@@ -582,10 +587,11 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
       SetProb(I, Prob);
   };
 
-  // If n <= 2, we choose the simplest probability model we can think of: every
-  // remaining conditional branch instruction has the same probability, Prob,
-  // of continuing to the next iteration.  This model has several helpful
-  // properties:
+  // If UnrollUniformWeights or n <= 2, we choose the simplest probability model
+  // we can think of: every remaining conditional branch instruction has the
+  // same probability, Prob, of continuing to the next iteration.  This model
+  // has several helpful properties:
+  // - There is only one search parameter, Prob.
   // - We have no reason to think one latch branch's probability should be
   //   higher or lower than another, and so this model makes them all the same.
   //   In the worst cases, we thus avoid setting just some probabilities to 0 or
@@ -598,20 +604,53 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
   //
   //     FreqOne = Sum(i=0..n)(c_i * p^i)
   //
-  // - If the backedge has been eliminated, FreqOne is the total frequency of
-  //   the original loop body in the unrolled loop.
-  // - If the backedge remains, Sum(i=0..inf)(FreqOne * p^(n*i)) =
-  //   FreqOne / (1 - p^n) is the total frequency of the original loop body in
-  //   the unrolled loop, regardless of whether the backedge is conditional or
-  //   unconditional.
+  // - If the backedge has been eliminated:
+  //   - FreqOne is the total frequency of the original loop body in the
+  //     unrolled loop.
+  //   - If Prob == 1, the total frequency of the original loop body is exactly
+  //     the number of remaining loop iterations, as expected because every
+  //     remaining loop iteration always then executes.
+  // - If the backedge remains:
+  //   - Sum(i=0..inf)(FreqOne * p^(n*i)) = FreqOne / (1 - p^n) is the total
+  //     frequency of the original loop body in the unrolled loop, regardless of
+  //     whether the backedge is conditional or unconditional.
+  //   - As Prob approaches 1, the total frequency of the original loop body
+  //     approaches infinity, as expected because the loop approaches never
+  //     exiting.
   // - For n <= 2, we can use simple formulas to solve the above polynomial
   //   equations exactly for p without performing a search.
+  // - For n > 2, evaluating each point in the search space, using ComputeFreq
+  //   below, requires about as few instructions as we could hope for.  That is,
+  //   the probability is constant across the conditional branches, so the only
+  //   computation is across conditional branches and any backedge, as required
+  //   for any model for Prob.
+  // - Prob == 1 produces the maximum possible total frequency for the original
+  //   loop body, as described above.  Prob == 0 produces the minimum, 0.
+  //   Increasing or decreasing Prob monotonically increases or decreases the
+  //   frequency, respectively.  Thus, for every possible frequency, there
+  //   exists some Prob that can produce it, and we can easily use bisection to
+  //   search the problem space.
 
   // When iterating for a solution, we stop early if we find probabilities
   // that produce a Freq whose difference from FreqDesired is small
   // (FreqPrec).  Otherwise, we expect to compute a solution at least that
   // accurate (but surely far more accurate).
   const double FreqPrec = 1e-6;
+
+  // Compute the new frequency produced by using Prob throughout CondLatches.
+  auto ComputeFreq = [&](double Prob) {
+    double ProbReaching = 1;        // p^0
+    double FreqOne = IterCounts[0]; // c_0*p^0
+    for (unsigned I = 0, E = CondLatches.size(); I < E; ++I) {
+      ProbReaching *= Prob;                        // p^(I+1)
+      FreqOne += IterCounts[I + 1] * ProbReaching; // c_(I+1)*p^(I+1)
+    }
+    double ProbReachingBackedge = CompletelyUnroll ? 0 : ProbReaching;
+    assert(FreqOne > 0 && "Expected at least one iteration before first latch");
+    if (ProbReachingBackedge == 1)
+      return std::numeric_limits<double>::infinity();
+    return FreqOne / (1 - ProbReachingBackedge);
+  };
 
   // Compute the probability that, used at CondLaches[0] where
   // CondLatches.size() == 1, gets as close as possible to FreqDesired.
@@ -621,6 +660,11 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
     double B = IterCounts[0] - FreqDesired;
     assert(A > 0 && "Expected iterations after last conditional latch");
     double Prob = -B / A;
+    // If it computes an invalid Prob, FreqDesired is impossibly low or high.
+    // Otherwise, Prob should produce nearly FreqDesired.
+    assert((Prob < 0 || Prob > 1 ||
+            fabs(ComputeFreq(Prob) - FreqDesired) < FreqPrec) &&
+           "Expected accurate frequency when linear case is possible");
     Prob = std::max(Prob, 0.);
     Prob = std::min(Prob, 1.);
     return Prob;
@@ -635,6 +679,11 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
     double C = IterCounts[0] - FreqDesired;
     assert(A > 0 && "Expected iterations after last conditional latch");
     double Prob = (-B + sqrt(B * B - 4 * A * C)) / (2 * A);
+    // If it computes an invalid Prob, FreqDesired is impossibly low or high.
+    // Otherwise, Prob should produce nearly FreqDesired.
+    assert((Prob < 0 || Prob > 1 ||
+            fabs(ComputeFreq(Prob) - FreqDesired) < FreqPrec) &&
+           "Expected accurate frequency when quadratic case is possible");
     Prob = std::max(Prob, 0.);
     Prob = std::min(Prob, 1.);
     return Prob;
@@ -774,12 +823,17 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
   };
 
   // Determine and set branch weights.
+  //
+  // Prob < 0 and Prob > 1 cannot be represented as branch weights.  We might
+  // compute such a Prob if FreqDesired is impossible (e.g., due to inaccurate
+  // profile data) for the maximum trip count we have determined when completely
+  // unrolling.  In that case, so just go with whichever is closest.
   if (CondLatches.size() == 1) {
     SetAllProbs(ComputeProbForLinear());
   } else if (CondLatches.size() == 2) {
     SetAllProbs(ComputeProbForQuadratic());
-  } else {
-    // The polynomial is too complex for a simple formula, so the quick and
+  } else if (!UnrollUniformWeights) {
+    // The polynomial is too complex for a simple formula, and the quick and
     // dirty fix has been selected.  Adjust probabilities starting from the
     // first latch, which has the most influence on the total frequency, so
     // starting there should minimize the number of latches that have to be
@@ -793,6 +847,37 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
       if (fabs(Freq - FreqDesired) < FreqPrec)
         break;
     }
+  } else {
+    // The polynomial is too complex for a simple formula, and uniform branch
+    // weights have been selected, so bisect.
+    double ProbMin = -1, ProbMax = -1; // Inits expected to be unused.
+    double ProbPrev = -1;              // Inits expected to be unused.
+    auto TryProb = [&](double Prob) {
+      ProbPrev = Prob;
+      double FreqDelta = ComputeFreq(Prob) - FreqDesired;
+      if (fabs(FreqDelta) < FreqPrec)
+        return 0;
+      if (FreqDelta < 0) {
+        ProbMin = Prob;
+        return -1;
+      }
+      ProbMax = Prob;
+      return 1;
+    };
+    // If Prob == 0 is too small and Prob == 1 is too large, bisect between
+    // them.  To place a hard upper limit on the search time, stop bisecting
+    // when Prob stops changing (ProbDelta) by much (ProbPrec).
+    if (TryProb(0.) < 0 && TryProb(1.) > 0) {
+      assert(ProbMin == 0 && ProbMax == 1 &&
+             "expected probability bounds to be initialized");
+      const double ProbPrec = 1e-12;
+      double Prob, ProbDelta;
+      do {
+        Prob = (ProbMin + ProbMax) / 2;
+        ProbDelta = Prob - ProbPrev;
+      } while (TryProb(Prob) != 0 && fabs(ProbDelta) > ProbPrec);
+    }
+    SetAllProbs(ProbPrev);
   }
 
   // FIXME: We have not considered non-latch loop exits:

commit 1bb9bc208da7136618fdf87223b75901ae336b3d

[LoopUnroll] Fix freq accuracy calculations (#213762)
This problem was reported at
<https://github.com/llvm/llvm-project/pull/182405#issuecomment-5165268733>
for the case of very large loop probabilities.

The biggest issue is that, when using linear and quadratic equations to
determine loop latch probabilities, asserts introduced by PR #182405 to
verify the accuracy of the resulting loop body frequency can fail.

Another issue is that iterations introduced by PR #182404 and PR #182405
terminate upon achieving a desired accuracy, but they can iterate longer
than necessary, wasting time achieving higher accuracy than desired.

This patch fixes the accuracy calculations to use relative differences
instead of absolute differences. It updates existing tests that reveal
the impact on the N>2 uniform case. Its adds new tests to cover the N=1,
N=2, and N>2 fast cases.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Transforms/Utils/LoopUnroll.cpp
@@ -632,7 +632,7 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
   //   search the problem space.
 
   // When iterating for a solution, we stop early if we find probabilities
-  // that produce a Freq whose difference from FreqDesired is small
+  // that produce a Freq whose relative difference from FreqDesired is small
   // (FreqPrec).  Otherwise, we expect to compute a solution at least that
   // accurate (but surely far more accurate).
   const double FreqPrec = 1e-6;
@@ -663,7 +663,7 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
     // If it computes an invalid Prob, FreqDesired is impossibly low or high.
     // Otherwise, Prob should produce nearly FreqDesired.
     assert((Prob < 0 || Prob > 1 ||
-            fabs(ComputeFreq(Prob) - FreqDesired) < FreqPrec) &&
+            fabs(ComputeFreq(Prob) - FreqDesired) / FreqDesired < FreqPrec) &&
            "Expected accurate frequency when linear case is possible");
     Prob = std::max(Prob, 0.);
     Prob = std::min(Prob, 1.);
@@ -682,7 +682,7 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
     // If it computes an invalid Prob, FreqDesired is impossibly low or high.
     // Otherwise, Prob should produce nearly FreqDesired.
     assert((Prob < 0 || Prob > 1 ||
-            fabs(ComputeFreq(Prob) - FreqDesired) < FreqPrec) &&
+            fabs(ComputeFreq(Prob) - FreqDesired) / FreqDesired < FreqPrec) &&
            "Expected accurate frequency when quadratic case is possible");
     Prob = std::max(Prob, 0.);
     Prob = std::min(Prob, 1.);
@@ -844,7 +844,7 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
     double FreqBefore = -1, FreqAfter = -1; // Inits expected to be unused.
     for (unsigned I = 0; I != CondLatches.size(); ++I) {
       double Freq = AdjustProb(I, ProbBefore, ProbAfter, FreqBefore, FreqAfter);
-      if (fabs(Freq - FreqDesired) < FreqPrec)
+      if (fabs(Freq - FreqDesired) / FreqDesired < FreqPrec)
         break;
     }
   } else {
@@ -855,7 +855,7 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
     auto TryProb = [&](double Prob) {
       ProbPrev = Prob;
       double FreqDelta = ComputeFreq(Prob) - FreqDesired;
-      if (fabs(FreqDelta) < FreqPrec)
+      if (fabs(FreqDelta) / FreqDesired < FreqPrec)
         return 0;
       if (FreqDelta < 0) {
         ProbMin = Prob;
@@ -865,8 +865,11 @@ static void fixProbContradiction(Loop *L, UnrollLoopOptions ULO,
       return 1;
     };
     // If Prob == 0 is too small and Prob == 1 is too large, bisect between
-    // them.  To place a hard upper limit on the search time, stop bisecting
-    // when Prob stops changing (ProbDelta) by much (ProbPrec).
+    // them.  Accuracy (relative difference) is controlled by FreqPrec above.
+    // However, to place a hard upper limit on the search time, we stop
+    // bisecting when Prob stops changing (ProbDelta) by much (ProbPrec).  In
+    // this case, we compute an absolute difference not a relative difference,
+    // which could produce more search time for smaller probabilities.
     if (TryProb(0.) < 0 && TryProb(1.) > 0) {
       assert(ProbMin == 0 && ProbMax == 1 &&
              "expected probability bounds to be initialized");

llvm/test/Transforms/LoopUnroll/branch-weights-freq/unroll-complete.ll
@@ -740,7 +740,7 @@
 ;     FAST4310-SAME:   !prof !1
 ;     UR4310:        call void @f
 ;     UR4310:        br label %do.end
-;     UNIF4310:      !0 = !{!"branch_weights", i32 406871040, i32 1740612608}
+;     UNIF4310:      !0 = !{!"branch_weights", i32 406872064, i32 1740611584}
 ;     FAST4310:      !0 = !{!"branch_weights", i32 113025456, i32 2034458192}
 ;     FAST4310:      !1 = !{!"branch_weights", i32 1, i32 2}
 ;

llvm/test/Transforms/LoopUnroll/branch-weights-freq/unroll-partial-unconditional-latch.ll
@@ -70,7 +70,7 @@
 ;   RUN: %{ur-bf} -unroll-count=3 -unroll-uniform-weights | %{fc} MULT3
 ;   RUN: %{ur-bf} -unroll-count=3 | %{fc} MULT3
 ;
-;   Sums to approximately the original loop body frequency, 10.
+;   Sums to get approximately the original loop body frequency, 10.
 ;   MULT3: - do.body: float = 3.69,
 ;   MULT3: - do.body.1: float = 3.321,
 ;   MULT3: - do.body.2: float = 2.9889,
@@ -176,7 +176,7 @@
 ;     MUNIF6 is like applying -unroll-count=3 to MULT2 without converting any
 ;     additional conditional latches to unconditional, so (approximately)
 ;     MULT2's branch weights make sense.
-;     MUNIF6: !0 = !{!"branch_weights", i32 1717986944, i32 429496704}
+;     MUNIF6: !0 = !{!"branch_weights", i32 1717987328, i32 429496320}
 ;     MUNIF6: !1 = distinct !{!1, !2, !3}
 ;     MUNIF6: !2 = !{!"llvm.loop.estimated_trip_count", i32 2}
 ;     MUNIF6: !3 = !{!"llvm.loop.unroll.disable"}
@@ -193,11 +193,13 @@
 ; Check case when the original loop's number of iterations is a run-time
 ; determined multiple of 10, the unroll count is even so that odd-numbered
 ; unrolled iterations become unconditional, and the original loop body frequency
-; is 1, which is impossibly low.  This case is important to ensure the
-; implementation does not malfunction by trying to use negative and possibly
-; infinite probabilities to reach the original loop body frequency.
+; is 1, which is impossibly low.
 ;
 ;   RUN: sed -e s/@N@/%mul10/ -e s/@W@/0/ %s > %t.ll
+;
+; This case is important to ensure the implementation does not malfunction by
+; trying to use negative and possibly infinite probabilities to reach the
+; original loop body frequency.
 ;
 ; Check the original loop body frequency.
 ;
@@ -347,11 +349,155 @@
 ;   CONST4: !1 = distinct !{!1, !2}
 ;   CONST4: !2 = !{!"llvm.loop.unroll.disable"}
 
+; ------------------------------------------------------------------------------
+; Check cases when the original loop's number of iterations matches the original
+; loop body frequency specified by the branch weights but is very large.
+;
+; At this scale, the probabilities and frequencies actually computed from those
+; branch weights are highly inaccurate because, for example, BranchProbability
+; has only 32 bits.  But LoopUnroll uses double for its computations after it
+; receives the inaccurate original probability, so LoopUnroll should produce
+; results consistent with that inaccurate original probability.  TODO: We should
+; consider whether greater BranchProbability and BFI accuracy is desirable.
+;
+; While this test is impacted by the above inaccuracies, its purpose is actually
+; independent.  It's simply to make sure LoopUnroll does not malfunction at this
+; scale.  In particular, when computing frequency accuracy for assertions or
+; for terminating a search, LoopUnroll must compute relative differences from
+; the desired frequency because absolute differences can be very large.
+;
+; First, try a case that produces only one conditional latch, whose branch
+; weights can thus be computed by solving a linear equation, for which an assert
+; about its accuracy would fail if we computed it as an absolute difference:
+;
+;   Original number of iterations and thus loop body frequency is a large, odd
+;   constant value.
+;   RUN: sed -e s/@N@/1000000001/ -e s/@W@/1000000000/ %s > %t.ll
+;
+;   Highly inaccurate calculated original loop body frequency.
+;   RUN: %{bf-fc} LARGE1-ORIG
+;   LARGE1-ORIG: - do.body: float = 1073741823.9,
+;
+;   -unroll-count=2 so only first unrolled iteration can possibly exit.
+;   RUN: %{ur-bf} -unroll-count=2 | %{fc} LARGE1
+;
+;   Sum to get approximately the highly inaccurately calculated original loop
+;   body frequency.  That is, LoopUnroll's results are consistent with that.
+;   LARGE1: - do.body: float = 536870912.0,
+;   LARGE1: - do.body.1: float = 536870911.0,
+;
+;   LARGE1: call void @f
+;   LARGE1: br i1 %{{.*}}, label %do.body.1, label %do.end, !prof !0
+;   LARGE1: call void @f
+;   LARGE1: br label %do.body, !llvm.loop !1
+;
+;   There is no llvm.loop.estimated_trip_count because the unrolled loop's latch
+;   in do.body.1 unconditionally continues.  The branch weights on do.body's
+;   branch imply do.body continues 2147483644/4 times and then exits once, thus
+;   executing the original loop body 1073741823 times.
+;   LARGE1: !0 = !{!"branch_weights", i32 2147483644, i32 4}
+;   LARGE1: !1 = distinct !{!1, !2}
+;   LARGE1: !2 = !{!"llvm.loop.unroll.disable"}
+;
+; Second, try a case that produces two conditional latches, whose branch
+; weights can thus be computed by solving a quadratic equation, for which an
+; assert about its accuracy would fail if we computed it as an absolute
+; difference:
+;
+;   Original number of iterations and thus loop body frequency is a run-time
+;   determined multiple of a large, even value not evenly divisible by 4.
+;   RUN: sed -e s/@N@/%mul.big/ -e s/@W@/1000000001/ %s > %t.ll
+;
+;   Highly inaccurate calculated original loop body frequency.
+;   RUN: %{bf-fc} LARGE2-ORIG
+;   LARGE2-ORIG: - do.body: float = 1073741823.9,
+;
+;   -unroll-count=4 so only two unrolled iterations can possibly exit.
+;   RUN: %{ur-bf} -unroll-count=4 | %{fc} LARGE2
+;
+;   Multiply by 2 and sum to get approximately the highly inaccurately
+;   calculated original loop body frequency.  That is, LoopUnroll's results are
+;   consistent with that.
+;   LARGE2: - do.body: float = 268435456.2,
+;   LARGE2: - do.body.2: float = 268435455.7,
+;
+;   LARGE2:     call void @f
+;   LARGE2-NOT: br
+;   LARGE2:     call void @f
+;   LARGE2:     br i1 %{{.*}}, label %do.body.2, label %do.end, !prof !0
+;   LARGE2:     call void @f
+;   LARGE2-NOT: br
+;   LARGE2:     call void @f
+;   LARGE2:     br i1 %{{.*}}, label %do.body, label %do.end, !prof !0, !llvm.loop !1
+;
+;   The branch weights on do.body's and do.body.2's branches imply each
+;   continues 2147483644/4 times and then exits once, thus executing the
+;   original loop body 1073741824 times.
+;   LARGE2: !0 = !{!"branch_weights", i32 2147483644, i32 4}
+;   LARGE2: !1 = distinct !{!1, !2, !3}
+;
+;   llvm.loop.estimated_trip_count is how many times the unrolled loop must
+;   iterate (but exit from %do.body on the last iteration) to execute exactly
+;   the original accurate trip count.  It is not forced through, for example,
+;   BranchProbability, so it is not impacted by the aforementioned inaccuracies.
+;   LARGE2: !2 = !{!"llvm.loop.estimated_trip_count", i32 250000001}
+;   LARGE2: !3 = !{!"llvm.loop.unroll.disable"}
+;
+; Third, try a case that produces three conditional latches, whose branch
+; weights thus cannot be computed by solving a simple equation:
+;
+;   Original number of iterations and thus loop body frequency is a run-time
+;   determined multiple of a large, even value.
+;   RUN: sed -e s/@N@/%mul.big/ -e s/@W@/1000000001/ %s > %t.ll
+;
+;   Highly inaccurate calculated original loop body frequency.
+;   RUN: %{bf-fc} LARGE3-ORIG
+;   LARGE3-ORIG: - do.body: float = 1073741823.9,
+;
+;   -unroll-count=6 so three unrolled iterations can exit.
+;   RUN: %{ur-bf} -unroll-count=6 | %{fc} LARGE3
+;
+;   Multiply by 2 and sum to get approximately the highly inaccurately
+;   calculated original loop body frequency.  That is, LoopUnroll's results are
+;   consistent with that.
+;   LARGE3: - do.body: float = 178956970.9,
+;   LARGE3: - do.body.2: float = 178956970.2,
+;   LARGE3: - do.body.4: float = 178956970.1,
+;
+;   LARGE3:           call void @f
+;   LARGE3-NOT:       br
+;   LARGE3:           call void @f
+;   LARGE3:           br i1 %{{.*}}, label %do.body.2, label %do.end, !prof !0
+;   LARGE3:           call void @f
+;   LARGE3-NOT:       br
+;   LARGE3:           call void @f
+;   LARGE3:           br i1 %{{.*}}, label %do.body.4, label %do.end, !prof !1
+;   LARGE3:           call void @f
+;   LARGE3-NOT:       br
+;   LARGE3:           call void @f
+;   LARGE3:           br i1 %{{.*}}, label %do.body, label %do.end, !prof !1, !llvm.loop !2
+;
+;   There are three conditional latches remaining.  Adjusting only the first is
+;   sufficient to achieve the desired accuracy, and the latter two are copied
+;   from the original loop.  More would be adjusted if we computed frequency
+;   accuracy as an absolute difference.
+;   LARGE3: !0 = !{!"branch_weights", i32 2147483640, i32 8}
+;   LARGE3: !1 = !{!"branch_weights", i32 1000000001, i32 1}
+;   LARGE3: !2 = distinct !{!2, !3, !4}
+;
+;   llvm.loop.estimated_trip_count is how many times the unrolled loop must
+;   execute to execute exactly the original accurate trip count.  It is not
+;   forced through, for example, BranchProbability, so it is not impacted by the
+;   aforementioned inaccuracies.
+;   LARGE3: !3 = !{!"llvm.loop.estimated_trip_count", i32 166666667}
+;   LARGE3: !4 = !{!"llvm.loop.unroll.disable"}
+
 declare void @f(i32)
 
 define void @test(i32 %n) {
 entry:
   %mul10 = mul i32 %n, 10
+  %mul.big = mul i32 %n, 1000000002
   br label %do.body
 
 do.body: