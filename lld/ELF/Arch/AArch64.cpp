nekoshirro/Alchemist-LLVM

commit c28dc50369d9ef46eadbc5dc04e9f57d63c5b256

[PAC][lld][AArch64][ELF] Support signed TLSDESC (#113817)
Depends on #120010

Support `R_AARCH64_AUTH_TLSDESC_ADR_PAGE21`, `R_AARCH64_AUTH_TLSDESC_LD64_LO12`
and `R_AARCH64_AUTH_TLSDESC_LD64_LO12` static relocations and
`R_AARCH64_AUTH_TLSDESC` dynamic relocation. IE/LE optimization is not
currently supported for AUTH TLSDESC.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lld/ELF/Arch/AArch64.cpp
@@ -157,9 +157,14 @@ RelExpr AArch64::getRelExpr(RelType type, const Symbol &s,
     return RE_AARCH64_AUTH;
   case R_AARCH64_TLSDESC_ADR_PAGE21:
     return RE_AARCH64_TLSDESC_PAGE;
+  case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
+    return RE_AARCH64_AUTH_TLSDESC_PAGE;
   case R_AARCH64_TLSDESC_LD64_LO12:
   case R_AARCH64_TLSDESC_ADD_LO12:
     return R_TLSDESC;
+  case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
+  case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
+    return RE_AARCH64_AUTH_TLSDESC;
   case R_AARCH64_TLSDESC_CALL:
     return R_TLSDESC_CALL;
   case R_AARCH64_TLSLE_ADD_TPREL_HI12:
@@ -545,6 +550,7 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
   case R_AARCH64_ADR_PREL_PG_HI21:
   case R_AARCH64_TLSIE_ADR_GOTTPREL_PAGE21:
   case R_AARCH64_TLSDESC_ADR_PAGE21:
+  case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
     checkInt(ctx, loc, val, 33, rel);
     [[fallthrough]];
   case R_AARCH64_ADR_PREL_PG_HI21_NC:
@@ -597,6 +603,7 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
   case R_AARCH64_TLSIE_LD64_GOTTPREL_LO12_NC:
   case R_AARCH64_TLSLE_LDST64_TPREL_LO12_NC:
   case R_AARCH64_TLSDESC_LD64_LO12:
+  case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
     checkAlignment(ctx, loc, val, 8, rel);
     write32Imm12(loc, getBits(val, 3, 11));
     break;
@@ -671,6 +678,7 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
     break;
   case R_AARCH64_TLSLE_ADD_TPREL_LO12_NC:
   case R_AARCH64_TLSDESC_ADD_LO12:
+  case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
     write32Imm12(loc, val);
     break;
   case R_AARCH64_TLSDESC:

lld/ELF/Relocations.cpp
@@ -1292,6 +1292,27 @@ static unsigned handleMipsTlsRelocation(Ctx &ctx, RelType type, Symbol &sym,
   return 0;
 }
 
+static unsigned handleAArch64PAuthTlsRelocation(InputSectionBase *sec,
+                                                RelExpr expr, RelType type,
+                                                uint64_t offset, Symbol &sym,
+                                                int64_t addend) {
+  // Do not optimize signed TLSDESC to LE/IE (as described in pauthabielf64).
+  // https://github.com/ARM-software/abi-aa/blob/main/pauthabielf64/pauthabielf64.rst#general-restrictions
+  // > PAUTHELF64 only supports the descriptor based TLS (TLSDESC).
+  if (oneof<RE_AARCH64_AUTH_TLSDESC_PAGE, RE_AARCH64_AUTH_TLSDESC>(expr)) {
+    sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_AUTH);
+    sec->addReloc({expr, type, offset, addend, &sym});
+    return 1;
+  }
+
+  // TLSDESC_CALL hint relocation should not be emitted by compiler with signed
+  // TLSDESC enabled.
+  if (expr == R_TLSDESC_CALL)
+    sym.setFlags(NEEDS_TLSDESC_NONAUTH);
+
+  return 0;
+}
+
 // Notes about General Dynamic and Local Dynamic TLS models below. They may
 // require the generation of a pair of GOT entries that have associated dynamic
 // relocations. The pair of GOT entries created are of the form GOT[e0] Module
@@ -1302,6 +1323,13 @@ static unsigned handleMipsTlsRelocation(Ctx &ctx, RelType type, Symbol &sym,
 unsigned RelocationScanner::handleTlsRelocation(RelExpr expr, RelType type,
                                                 uint64_t offset, Symbol &sym,
                                                 int64_t addend) {
+  bool isAArch64 = ctx.arg.emachine == EM_AARCH64;
+
+  if (isAArch64)
+    if (unsigned processed = handleAArch64PAuthTlsRelocation(
+            sec, expr, type, offset, sym, addend))
+      return processed;
+
   if (expr == R_TPREL || expr == R_TPREL_NEG) {
     if (ctx.arg.shared) {
       auto diag = Err(ctx);
@@ -1336,7 +1364,9 @@ unsigned RelocationScanner::handleTlsRelocation(RelExpr expr, RelType type,
     // R_RISCV_TLSDESC_{LOAD_LO12,ADD_LO12_I,CALL} reference a label. Do not
     // set NEEDS_TLSDESC on the label.
     if (expr != R_TLSDESC_CALL) {
-      if (!isRISCV || type == R_RISCV_TLSDESC_HI20)
+      if (isAArch64)
+        sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_NONAUTH);
+      else if (!isRISCV || type == R_RISCV_TLSDESC_HI20)
         sym.setFlags(NEEDS_TLSDESC);
       sec->addReloc({expr, type, offset, addend, &sym});
     }
@@ -1847,10 +1877,21 @@ void elf::postScanRelocations(Ctx &ctx) {
     GotSection *got = ctx.in.got.get();
 
     if (flags & NEEDS_TLSDESC) {
+      if ((flags & NEEDS_TLSDESC_AUTH) && (flags & NEEDS_TLSDESC_NONAUTH)) {
+        Err(ctx)
+            << "both AUTH and non-AUTH TLSDESC entries for '" << sym.getName()
+            << "' requested, but only one type of TLSDESC entry per symbol is "
+               "supported";
+        return;
+      }
       got->addTlsDescEntry(sym);
+      RelType tlsDescRel = ctx.target->tlsDescRel;
+      if (flags & NEEDS_TLSDESC_AUTH) {
+        got->addTlsDescAuthEntry();
+        tlsDescRel = ELF::R_AARCH64_AUTH_TLSDESC;
+      }
       ctx.mainPart->relaDyn->addAddendOnlyRelocIfNonPreemptible(
-          ctx.target->tlsDescRel, *got, got->getTlsDescOffset(sym), sym,
-          ctx.target->tlsDescRel);
+          tlsDescRel, *got, got->getTlsDescOffset(sym), sym, tlsDescRel);
     }
     if (flags & NEEDS_TLSGD) {
       got->addDynTlsEntry(sym);

lld/ELF/Symbols.h
@@ -53,6 +53,8 @@ enum {
   NEEDS_TLSIE = 1 << 8,
   NEEDS_GOT_AUTH = 1 << 9,
   NEEDS_GOT_NONAUTH = 1 << 10,
+  NEEDS_TLSDESC_AUTH = 1 << 11,
+  NEEDS_TLSDESC_NONAUTH = 1 << 12,
 };
 
 // The base class for real symbol classes.

lld/test/ELF/aarch64-tlsdesc-pauth.s
@@ -1,0 +1,134 @@
+// REQUIRES: aarch64
+// RUN: rm -rf %t && split-file %s %t && cd %t
+
+//--- a.s
+.section .tbss,"awT",@nobits
+.global a
+a:
+.xword 0
+
+//--- ok.s
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth ok.s -o ok.o
+// RUN: ld.lld -shared ok.o -o ok.so
+// RUN: llvm-objdump --no-print-imm-hex -d --no-show-raw-insn ok.so | \
+// RUN:   FileCheck -DP=20 -DA=896 -DB=912 -DC=928 %s
+// RUN: llvm-readobj -r -x .got ok.so | FileCheck --check-prefix=REL \
+// RUN:   -DP1=20 -DA1=380 -DB1=390 -DC1=3A0 -DP2=020 -DA2=380 -DB2=390 -DC2=3a0 %s
+
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth a.s -o a.so.o
+// RUN: ld.lld -shared a.so.o -soname=so -o a.so
+// RUN: ld.lld ok.o a.so -o ok
+// RUN: llvm-objdump --no-print-imm-hex -d --no-show-raw-insn ok | \
+// RUN:   FileCheck -DP=220 -DA=936 -DB=952 -DC=968 %s
+// RUN: llvm-readobj -r -x .got ok | FileCheck --check-prefix=REL \
+// RUN:   -DP1=220 -DA1=3A8 -DB1=3B8 -DC1=3C8 -DP2=220 -DA2=3a8 -DB2=3b8 -DC2=3c8 %s
+
+        .text
+        adrp    x0, :tlsdesc_auth:a
+        ldr     x16, [x0, :tlsdesc_auth_lo12:a]
+        add     x0, x0, :tlsdesc_auth_lo12:a
+        blraa   x16, x0
+
+// CHECK:      adrp    x0, 0x[[P]]000
+// CHECK-NEXT: ldr     x16, [x0, #[[A]]]
+// CHECK-NEXT: add     x0, x0, #[[A]]
+// CHECK-NEXT: blraa   x16, x0
+
+/// Create relocation against local TLS symbols where linker should
+/// create target specific dynamic TLSDESC relocation where addend is
+/// the symbol VMA in tls block.
+
+        adrp    x0, :tlsdesc_auth:local1
+        ldr     x16, [x0, :tlsdesc_auth_lo12:local1]
+        add     x0, x0, :tlsdesc_auth_lo12:local1
+        blraa   x16, x0
+
+// CHECK:      adrp    x0, 0x[[P]]000
+// CHECK-NEXT: ldr     x16, [x0, #[[B]]]
+// CHECK-NEXT: add     x0, x0, #[[B]]
+// CHECK-NEXT: blraa   x16, x0
+
+        adrp    x0, :tlsdesc_auth:local2
+        ldr     x16, [x0, :tlsdesc_auth_lo12:local2]
+        add     x0, x0, :tlsdesc_auth_lo12:local2
+        blraa   x16, x0
+
+// CHECK:      adrp    x0, 0x[[P]]000
+// CHECK-NEXT: ldr     x16, [x0, #[[C]]]
+// CHECK-NEXT: add     x0, x0, #[[C]]
+// CHECK-NEXT: blraa   x16, x0
+
+        .section .tbss,"awT",@nobits
+        .type   local1,@object
+        .p2align 2
+local1:
+        .word   0
+        .size   local1, 4
+
+        .type   local2,@object
+        .p2align 3
+local2:
+        .xword  0
+        .size   local2, 8
+
+
+// R_AARCH64_AUTH_TLSDESC - 0x0 -> start of tls block
+// R_AARCH64_AUTH_TLSDESC - 0x8 -> align (sizeof (local1), 8)
+
+// REL:      Relocations [
+// REL-NEXT:   Section (5) .rela.dyn {
+// REL-NEXT:     0x[[P1]][[B1]] R_AARCH64_AUTH_TLSDESC - 0x0
+// REL-NEXT:     0x[[P1]][[C1]] R_AARCH64_AUTH_TLSDESC - 0x8
+// REL-NEXT:     0x[[P1]][[A1]] R_AARCH64_AUTH_TLSDESC a 0x0
+// REL-NEXT:   }
+// REL-NEXT: ]
+
+// REL:      Hex dump of section '.got':
+// REL-NEXT: 0x00[[P2]][[A2]] 00000000 00000080 00000000 000000a0
+// REL-NEXT: 0x00[[P2]][[B2]] 00000000 00000080 00000000 000000a0
+// REL-NEXT: 0x00[[P2]][[C2]] 00000000 00000080 00000000 000000a0
+///                                          ^^
+///                                          0b10000000 bit 63 address diversity = true, bits 61..60 key = IA
+///                                                            ^^
+///                                                            0b10100000 bit 63 address diversity = true, bits 61..60 key = DA
+
+//--- err1.s
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err1.s -o err1.o
+// RUN: not ld.lld -shared err1.o 2>&1 | FileCheck --check-prefix=ERR1 --implicit-check-not=error: %s
+// ERR1: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
+        .text
+        adrp    x0, :tlsdesc_auth:a
+        ldr     x16, [x0, :tlsdesc_auth_lo12:a]
+        add     x0, x0, :tlsdesc_auth_lo12:a
+        blraa   x16, x0
+
+        adrp    x0, :tlsdesc:a
+        ldr     x1, [x0, :tlsdesc_lo12:a]
+        add     x0, x0, :tlsdesc_lo12:a
+        blr     x1
+
+//--- err2.s
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err2.s -o err2.o
+// RUN: not ld.lld -shared err2.o 2>&1 | FileCheck --check-prefix=ERR2 --implicit-check-not=error: %s
+// ERR2: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
+        .text
+        adrp    x0, :tlsdesc:a
+        ldr     x1, [x0, :tlsdesc_lo12:a]
+        add     x0, x0, :tlsdesc_lo12:a
+        blr     x1
+
+        adrp    x0, :tlsdesc_auth:a
+        ldr     x16, [x0, :tlsdesc_auth_lo12:a]
+        add     x0, x0, :tlsdesc_auth_lo12:a
+        blraa   x16, x0
+
+//--- err3.s
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err3.s -o err3.o
+// RUN: not ld.lld -shared err3.o 2>&1 | FileCheck --check-prefix=ERR3 --implicit-check-not=error: %s
+// ERR3: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
+        .text
+        adrp    x0, :tlsdesc_auth:a
+        ldr     x16, [x0, :tlsdesc_auth_lo12:a]
+        add     x0, x0, :tlsdesc_auth_lo12:a
+        .tlsdesccall a
+        blraa   x16, x0

lld/ELF/InputSection.cpp
@@ -966,12 +966,14 @@ uint64_t InputSectionBase::getRelocTargetVA(Ctx &ctx, const Relocation &r,
   case R_SIZE:
     return r.sym->getSize() + a;
   case R_TLSDESC:
+  case RE_AARCH64_AUTH_TLSDESC:
     return ctx.in.got->getTlsDescAddr(*r.sym) + a;
   case R_TLSDESC_PC:
     return ctx.in.got->getTlsDescAddr(*r.sym) + a - p;
   case R_TLSDESC_GOTPLT:
     return ctx.in.got->getTlsDescAddr(*r.sym) + a - ctx.in.gotPlt->getVA();
   case RE_AARCH64_TLSDESC_PAGE:
+  case RE_AARCH64_AUTH_TLSDESC_PAGE:
     return getAArch64Page(ctx.in.got->getTlsDescAddr(*r.sym) + a) -
            getAArch64Page(p);
   case RE_LOONGARCH_TLSDESC_PAGE_PC:

lld/ELF/Relocations.h
@@ -99,6 +99,8 @@ enum RelExpr {
   RE_AARCH64_PAGE_PC,
   RE_AARCH64_RELAX_TLS_GD_TO_IE_PAGE_PC,
   RE_AARCH64_TLSDESC_PAGE,
+  RE_AARCH64_AUTH_TLSDESC_PAGE,
+  RE_AARCH64_AUTH_TLSDESC,
   RE_AARCH64_AUTH,
   RE_ARM_PCA,
   RE_ARM_SBREL,

lld/ELF/SyntheticSections.cpp
@@ -681,6 +681,11 @@ bool GotSection::addTlsDescEntry(const Symbol &sym) {
   return true;
 }
 
+void GotSection::addTlsDescAuthEntry() {
+  authEntries.push_back({(numEntries - 2) * ctx.arg.wordsize, true});
+  authEntries.push_back({(numEntries - 1) * ctx.arg.wordsize, false});
+}
+
 bool GotSection::addDynTlsEntry(const Symbol &sym) {
   assert(sym.auxIdx == ctx.symAux.size() - 1);
   ctx.symAux.back().tlsGdIdx = numEntries;

lld/ELF/SyntheticSections.h
@@ -114,6 +114,7 @@ public:
   void addEntry(const Symbol &sym);
   void addAuthEntry(const Symbol &sym);
   bool addTlsDescEntry(const Symbol &sym);
+  void addTlsDescAuthEntry();
   bool addDynTlsEntry(const Symbol &sym);
   bool addTlsIndex();
   uint32_t getTlsDescOffset(const Symbol &sym) const;

commit 968d8e0e8cefcc9cba80c41100b9150e47c0c8cd

[PAC][ELF] Remove NEEDS_{GOT,TLSDESC}_NONAUTH (#213452)
Currently we have all of NEEDS_GOT, NEEDS_GOT_AUTH and NEEDS_GOT_NONAUTH
(and similarly for TLSDESC), which takes up three bits, but is only
being used to encode four states (no GOT entry, a normal GOT entry, an
AUTH GOT entry, and an unsupported attempt to have both). This is a
waste of precious bits in the atomic uint16_t flags. Re-encode this in
the more natural manner; effectively NEEDS_GOT is now NEEDS_GOT_NONAUTH
and what was NEEDS_GOT is now implied by having either GOT type. Note
that, as part of this, there are a couple of functional changes.

Firstly, replaceWithDefined previously threw away NEEDS_GOT_AUTH and
NEEDS_GOT_NONAUTH which, under the previous encoding, gave you a
non-AUTH GOT entry regardless of what was requested, i.e. not an AUTH
GOT entry if that was requested (that would presumably then fail the
AUTDA operation in any user of it) nor an error if both were requested.
This may cause real software that currently links without error today to
no longer link, but any such cases are due to LLD emitting incorrect
output instead of detecting cases it doesn't support.

Secondly, this also changes the error checking for TLSDESC. Previously
we'd set NEEDS_TLSDESC_NONAUTH for all non-AUTH TLSDESC relocations,
regardless of whether they're being relaxed, and even for TLSDESC_CALL,
despite that never otherwise affecting symbol flags. Now that doesn't
exist we end up only having NEEDS_TLSDESC if (a) not relaxing and (b)
this isn't a TLSDESC_CALL. (b) should not matter in practice, since any
valid input will have the rest of the TLSDESC sequence. As for (a), this
could be considered a feature, since our lack of support for both GOT
entries for a single symbol is an artificial limitation arising from
only having state to track a single GOT index, and so it's not really an
error in the input, and we can produce correct output for it. One could
argue that it would be nice to detect and inform the user about cases
where we only successfully link due to optimisations like this, but it's
not worth spending one of few remaining flags bits on.

This TLSDESC_CALL invalid input case (i.e. (b) above) is being checked
by err3, which we no longer detect, so remove it; err1 already provides
coverage for when both types are requested.

Whilst here, add the missing .tlsdesccall to err1 so it's a legitimate
TLSDESC code sequence, remove err2 since it's just err1 with the two
sequences swapped (i.e. provides no additional coverage), and rename
err1 to err since it's the only one left.

Fixes: c28dc50369d9 ("[PAC][lld][AArch64][ELF] Support signed TLSDESC (#113817)")

lld/ELF/Arch/AArch64.cpp
@@ -301,20 +301,20 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
       expr = R_GOT_PC;
       break;
 
-    // AUTH GOT relocations. Set NEEDS_GOT_AUTH to detect incompatibility with
-    // NEEDS_GOT_NONAUTH. rs.process does not set the flag.
+    // AUTH GOT relocations. Handle flags here, as rs.process assumes R_GOT is a
+    // normal non-AUTH GOT entry.
     case R_AARCH64_AUTH_LD64_GOT_LO12_NC:
     case R_AARCH64_AUTH_GOT_ADD_LO12_NC:
-      sym.setFlags(NEEDS_GOT | NEEDS_GOT_AUTH);
+      sym.setFlags(NEEDS_GOT_AUTH);
       rs.processAux(R_GOT, type, offset, sym, addend);
       continue;
     case R_AARCH64_AUTH_GOT_LD_PREL19:
     case R_AARCH64_AUTH_GOT_ADR_PREL_LO21:
-      sym.setFlags(NEEDS_GOT | NEEDS_GOT_AUTH);
+      sym.setFlags(NEEDS_GOT_AUTH);
       rs.processAux(R_GOT_PC, type, offset, sym, addend);
       continue;
     case R_AARCH64_AUTH_ADR_GOT_PAGE:
-      sym.setFlags(NEEDS_GOT | NEEDS_GOT_AUTH);
+      sym.setFlags(NEEDS_GOT_AUTH);
       rs.processAux(RE_AARCH64_GOT_PAGE_PC, type, offset, sym, addend);
       continue;
 
@@ -354,7 +354,6 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
       rs.handleTlsDesc(R_TLSDESC, R_GOT, type, offset, addend, sym);
       continue;
     case R_AARCH64_TLSDESC_CALL:
-      sym.setFlags(NEEDS_TLSDESC_NONAUTH);
       if (!ctx.arg.shared)
         sec.addReloc({R_TPREL, type, offset, addend, &sym});
       continue;
@@ -363,12 +362,12 @@ void AArch64::scanSectionImpl(InputSectionBase &sec, Relocs<RelTy> rels,
     // only supports the descriptor based TLS (TLSDESC).
     // https://github.com/ARM-software/abi-aa/blob/main/pauthabielf64/pauthabielf64.rst#general-restrictions
     case R_AARCH64_AUTH_TLSDESC_ADR_PAGE21:
-      sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_AUTH);
+      sym.setFlags(NEEDS_TLSDESC_AUTH);
       sec.addReloc({RE_AARCH64_TLSDESC_PAGE, type, offset, addend, &sym});
       continue;
     case R_AARCH64_AUTH_TLSDESC_LD64_LO12:
     case R_AARCH64_AUTH_TLSDESC_ADD_LO12:
-      sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_AUTH);
+      sym.setFlags(NEEDS_TLSDESC_AUTH);
       sec.addReloc({R_TLSDESC, type, offset, addend, &sym});
       continue;
 

lld/ELF/RelocScan.h
@@ -163,9 +163,7 @@ public:
   void handleTlsDesc(RelExpr sharedExpr, RelExpr ieExpr, RelType type,
                      uint64_t offset, int64_t addend, Symbol &sym) {
     if (ctx.arg.shared) {
-      // NEEDS_TLSDESC_NONAUTH is a no-op for non-AArch64 targets and detects
-      // incompatibility with NEEDS_TLSDESC_AUTH.
-      sym.setFlags(NEEDS_TLSDESC | NEEDS_TLSDESC_NONAUTH);
+      sym.setFlags(NEEDS_TLSDESC);
       sec->addReloc({sharedExpr, type, offset, addend, &sym});
     } else if (sym.isPreemptible) {
       // Optimize to Initial Exec.

lld/ELF/Relocations.cpp
@@ -251,7 +251,7 @@ static void replaceWithDefined(Ctx &ctx, Symbol &sym, SectionBase &sec,
   sym.versionId = versionId;
   sym.isUsedInRegularObj = true;
   // A copy relocated alias may need a GOT entry.
-  sym.flags.fetch_and(NEEDS_GOT, std::memory_order_relaxed);
+  sym.flags.fetch_and(NEEDS_GOT | NEEDS_GOT_AUTH, std::memory_order_relaxed);
 }
 
 // Reserve space in .bss or .bss.rel.ro for copy relocation.
@@ -958,7 +958,7 @@ void RelocScan::process(RelExpr expr, RelType type, uint64_t offset,
     } else if (!sym.isTls() || ctx.arg.emachine != EM_LOONGARCH) {
       // Many LoongArch TLS relocs reuse the RE_LOONGARCH_GOT type, in which
       // case the NEEDS_GOT flag shouldn't get set.
-      sym.setFlags(NEEDS_GOT | NEEDS_GOT_NONAUTH);
+      sym.setFlags(NEEDS_GOT);
     }
   } else if (needsPlt(expr)) {
     sym.setFlags(NEEDS_PLT);
@@ -1243,11 +1243,11 @@ static bool handleNonPreemptibleIfunc(Ctx &ctx, Symbol &sym, uint16_t flags) {
   if (!sym.isGnuIFunc() || sym.isPreemptible || ctx.arg.zIfuncNoplt)
     return false;
   // Skip unreferenced non-preemptible ifunc.
-  if (!(flags & (NEEDS_GOT | NEEDS_PLT | HAS_DIRECT_RELOC)))
+  if (!(flags & (NEEDS_GOT | NEEDS_GOT_AUTH | NEEDS_PLT | HAS_DIRECT_RELOC)))
     return true;
   // We only support one kind of GOT entry, and IPLT entries currently always
   // use non-AUTH GOT entries.
-  if ((flags & NEEDS_GOT) && (flags & NEEDS_GOT_AUTH)) {
+  if (flags & NEEDS_GOT_AUTH) {
     auto diag = Err(ctx);
     diag << "AUTH GOT entry for non-preemptible ifunc '" << sym.getName()
          << "' requested, but R_AARCH64_AUTH_IRELATIVE is not supported yet";
@@ -1298,8 +1298,8 @@ void elf::postScanRelocations(Ctx &ctx) {
       return;
     sym.allocateAux(ctx);
 
-    if (flags & NEEDS_GOT) {
-      if ((flags & NEEDS_GOT_AUTH) && (flags & NEEDS_GOT_NONAUTH)) {
+    if (flags & (NEEDS_GOT | NEEDS_GOT_AUTH)) {
+      if ((flags & NEEDS_GOT) && (flags & NEEDS_GOT_AUTH)) {
         auto diag = Err(ctx);
         diag << "both AUTH and non-AUTH GOT entries for '" << sym.getName()
              << "' requested, but only one type of GOT entry per symbol is "
@@ -1342,8 +1342,8 @@ void elf::postScanRelocations(Ctx &ctx) {
       return;
     GotSection *got = ctx.in.got.get();
 
-    if (flags & NEEDS_TLSDESC) {
-      if ((flags & NEEDS_TLSDESC_AUTH) && (flags & NEEDS_TLSDESC_NONAUTH)) {
+    if (flags & (NEEDS_TLSDESC | NEEDS_TLSDESC_AUTH)) {
+      if ((flags & NEEDS_TLSDESC) && (flags & NEEDS_TLSDESC_AUTH)) {
         Err(ctx)
             << "both AUTH and non-AUTH TLSDESC entries for '" << sym.getName()
             << "' requested, but only one type of TLSDESC entry per symbol is "

lld/ELF/Symbols.h
@@ -56,9 +56,9 @@ enum {
   NEEDS_GOT_DTPREL = 1 << 7,
   NEEDS_TLSIE = 1 << 8,
   NEEDS_GOT_AUTH = 1 << 9,
-  NEEDS_GOT_NONAUTH = 1 << 10,
+  // 1 << 10 unused
   NEEDS_TLSDESC_AUTH = 1 << 11,
-  NEEDS_TLSDESC_NONAUTH = 1 << 12,
+  // 1 << 12 unused
 };
 
 // The base class for real symbol classes.
@@ -352,7 +352,8 @@ public:
   bool needsDynReloc() const {
     return flags.load(std::memory_order_relaxed) &
            (NEEDS_COPY | NEEDS_GOT | NEEDS_PLT | NEEDS_TLSDESC | NEEDS_TLSGD |
-            NEEDS_GOT_DTPREL | NEEDS_TLSIE);
+            NEEDS_GOT_DTPREL | NEEDS_TLSIE | NEEDS_GOT_AUTH |
+            NEEDS_TLSDESC_AUTH);
   }
   void allocateAux(Ctx &ctx) {
     assert(auxIdx == 0);

lld/test/ELF/aarch64-tlsdesc-pauth.s
@@ -92,10 +92,10 @@ local2:
 ///                                                            ^^
 ///                                                            0b10100000 bit 63 address diversity = true, bits 61..60 key = DA
 
-//--- err1.s
-// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err1.s -o err1.o
-// RUN: not ld.lld -shared err1.o 2>&1 | FileCheck --check-prefix=ERR1 --implicit-check-not=error: %s
-// ERR1: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
+//--- err.s
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err.s -o err.o
+// RUN: not ld.lld -shared err.o 2>&1 | FileCheck --check-prefix=ERR --implicit-check-not=error: %s
+// ERR: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
         .text
         adrp    x0, :tlsdesc_auth:a
         ldr     x16, [x0, :tlsdesc_auth_lo12:a]
@@ -105,30 +105,5 @@ local2:
         adrp    x0, :tlsdesc:a
         ldr     x1, [x0, :tlsdesc_lo12:a]
         add     x0, x0, :tlsdesc_lo12:a
-        blr     x1
-
-//--- err2.s
-// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err2.s -o err2.o
-// RUN: not ld.lld -shared err2.o 2>&1 | FileCheck --check-prefix=ERR2 --implicit-check-not=error: %s
-// ERR2: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
-        .text
-        adrp    x0, :tlsdesc:a
-        ldr     x1, [x0, :tlsdesc_lo12:a]
-        add     x0, x0, :tlsdesc_lo12:a
-        blr     x1
-
-        adrp    x0, :tlsdesc_auth:a
-        ldr     x16, [x0, :tlsdesc_auth_lo12:a]
-        add     x0, x0, :tlsdesc_auth_lo12:a
-        blraa   x16, x0
-
-//--- err3.s
-// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-linux -mattr=+pauth err3.s -o err3.o
-// RUN: not ld.lld -shared err3.o 2>&1 | FileCheck --check-prefix=ERR3 --implicit-check-not=error: %s
-// ERR3: error: both AUTH and non-AUTH TLSDESC entries for 'a' requested, but only one type of TLSDESC entry per symbol is supported
-        .text
-        adrp    x0, :tlsdesc_auth:a
-        ldr     x16, [x0, :tlsdesc_auth_lo12:a]
-        add     x0, x0, :tlsdesc_auth_lo12:a
         .tlsdesccall a
-        blraa   x16, x0
+        blr     x1