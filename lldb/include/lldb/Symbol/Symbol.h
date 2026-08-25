nekoshirro/Alchemist-LLVM

commit f023c87198dab9ffc627ccaed3af64e0f61caaea

[lldb] Change the Symbol rep for re-export symbols (#213356)
On Darwin system, we have re-export symbols. A library can have a symbol
table entry for function A() that is a re-export symbol; it's only data
is the name of the actual function to call, B(). When code calls A(),
the dynamic loader will resolve this to B() in some other library.

Previously, Symbol was using its AddressRange's Address object's offset
field to point to lldb memory where the name of the target function,
B(), was stored in the binary symbol table. In December Alex put up a PR
to stop abusing the Address object in this way, and store (1) the name
of the target function, and (2) once it has been looked up, the name of
the target function's library.
https://github.com/llvm/llvm-project/pull/172565

Alex originally added a ConstString target_name, FileSpec solib to
Symbol, which increased the size of this object, and lldb stores many of
them, so this was a problem.

I took Alex's PR and changed Symbol to hold a union of AddressRange or
ReExportInfo; re-export symbols do not have address ranges, so only one
of these can be in use. I used a union of {AddressRange, ReExportInfo}
in a subclass, which has getter/setter methods. Those methods take a
reference to the containing Symbol object so they can consult the
`m_type` and assert if the union is interpreted by the incorrect type
(without duplicating the storage of `m_type` in the subclass). The size
of the ReExportInfo object is less than the size of AddressRange, so
Symbol size is not increased. A `std::variant` would need to track which
variant is currently in use, increasing the size of Symbol.

I also added this to the Decode and Encode methods for correct
DataFileCache serialization of re-export symbols. This necessitates
increasing the DataFileCache version number for platforms that have
re-export symbols.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/include/lldb/Symbol/Symbol.h
@@ -13,11 +13,14 @@
 #include "lldb/Core/Mangled.h"
 #include "lldb/Core/Section.h"
 #include "lldb/Symbol/SymbolContextScope.h"
+#include "lldb/Utility/FileSpec.h"
 #include "lldb/Utility/Stream.h"
 #include "lldb/Utility/UserID.h"
 #include "lldb/lldb-enumerations.h"
 #include "lldb/lldb-private.h"
 #include "llvm/Support/JSON.h"
+
+#include <optional>
 
 namespace lldb_private {
 
@@ -50,6 +53,12 @@ public:
 
   Symbol(const Symbol &rhs);
 
+  ~Symbol() {
+    if (m_type != lldb::eSymbolTypeReExported &&
+        m_type != lldb::eSymbolTypeInvalid)
+      m_addr_or_reexport.GetAddressRange(*this).Clear();
+  }
+
   const Symbol &operator=(const Symbol &rhs);
 
   static llvm::Expected<Symbol> FromJSON(const JSONSymbol &symbol,
@@ -70,9 +79,13 @@ public:
   // an Address object that contains an constant integer value in
   // m_addr_range.m_base_addr.m_offset which could be incorrectly used to
   // represent an absolute address since it has no section.
-  Address &GetAddressRef() { return m_addr_range.GetBaseAddress(); }
+  Address &GetAddressRef() {
+    return m_addr_or_reexport.GetAddressRange(*this).GetBaseAddress();
+  }
 
-  const Address &GetAddressRef() const { return m_addr_range.GetBaseAddress(); }
+  const Address &GetAddressRef() const {
+    return m_addr_or_reexport.GetAddressRange(*this).GetBaseAddress();
+  }
 
   // Makes sure the symbol's value is an address and returns the file address.
   // Returns LLDB_INVALID_ADDRESS if the symbol's value isn't an address.
@@ -96,7 +109,7 @@ public:
     // GetAddress() accessor, we need to hand out an invalid address if the
     // symbol's value isn't an address.
     if (ValueIsAddress())
-      return m_addr_range.GetBaseAddress();
+      return m_addr_or_reexport.GetAddressRange(*this).GetBaseAddress();
     else
       return Address();
   }
@@ -108,7 +121,9 @@ public:
   /// no section, then getting the file address will return the correct value
   /// as it will return the offset in the base address which is the value.
   uint64_t GetRawValue() const {
-    return m_addr_range.GetBaseAddress().GetFileAddress();
+    return m_addr_or_reexport.GetAddressRange(*this)
+        .GetBaseAddress()
+        .GetFileAddress();
   }
 
   // When a symbol's value isn't an address, we need to access the raw value.
@@ -122,7 +137,9 @@ public:
       return fail_value;
     } else {
       // The value is stored in the base address' offset
-      return m_addr_range.GetBaseAddress().GetOffset();
+      return m_addr_or_reexport.GetAddressRange(*this)
+          .GetBaseAddress()
+          .GetOffset();
     }
   }
 
@@ -212,7 +229,7 @@ public:
 
   void SetByteSize(lldb::addr_t size) {
     m_size_is_valid = size > 0;
-    m_addr_range.SetByteSize(size);
+    m_addr_or_reexport.GetAddressRange(*this).SetByteSize(size);
   }
 
   bool GetSizeIsSibling() const { return m_size_is_sibling; }
@@ -319,11 +336,42 @@ protected:
   // modules we've already seen to make sure we don't get caught in a cycle.
 
   Symbol *ResolveReExportedSymbolInModuleSpec(
-      Target &target, ConstString &reexport_name,
+      Target &target, ConstString reexport_name,
       lldb_private::ModuleSpec &module_spec,
       lldb_private::ModuleList &seen_modules) const;
 
   void SynthesizeNameIfNeeded() const;
+
+  // Initially, a ReExportInfo will only have a name: the symbol name
+  // that this Symbol will remap to, at runtime.
+  // We won't have the library that this symbol is defined in,
+  // until later, when we have other binaries loaded in the Target.
+  struct ReExportInfo {
+    ConstString name;
+    std::unique_ptr<FileSpec> library_up;
+    ReExportInfo() : name(), library_up() {}
+    ReExportInfo(const ReExportInfo &rhs) {
+      name = rhs.name;
+      if (rhs.library_up)
+        library_up = std::make_unique<FileSpec>(*rhs.library_up);
+      else
+        library_up.reset();
+    }
+    const ReExportInfo &operator=(const ReExportInfo &rhs) {
+      if (this != &rhs) {
+        name = rhs.name;
+        if (rhs.library_up)
+          library_up = std::make_unique<FileSpec>(*rhs.library_up);
+        else
+          library_up.reset();
+      }
+      return *this;
+    }
+    void Clear() {
+      name.Clear();
+      library_up.reset();
+    }
+  };
 
   uint32_t m_uid = LLDB_INVALID_SYMBOL_ID; // User ID (usually the original
                                            // symbol table index)
@@ -351,9 +399,39 @@ protected:
       m_is_weak : 1,
       m_type : 6;            // Values from the lldb::SymbolType enum.
   mutable Mangled m_mangled; // uniqued symbol name/mangled name pair
-  AddressRange m_addr_range; // Contains the value, or the section offset
-                             // address when the value is an address in a
-                             // section, and the size (if any)
+
+  // A wrapper around a `union {AddressRange, ReExportInfo}` to
+  // enforce the types being get/set match the Symbol's m_type,
+  // assert if there is a type mismatch.
+  struct AddrRangeOrReExport {
+    AddressRange &GetAddressRange(Symbol &sym);
+    const AddressRange &GetAddressRange(const Symbol &sym) const;
+    ReExportInfo &GetReExportInfo(Symbol &sym);
+    const ReExportInfo &GetReExportInfo(const Symbol &sym) const;
+    void SetAddressRange(Symbol &sym, const AddressRange addr_range);
+    void SetReExportInfo(Symbol &sym, const ReExportInfo reexport_info);
+
+    AddrRangeOrReExport(const Symbol &sym) : m_addr_range() {
+      if (sym.GetType() == lldb::eSymbolTypeReExported)
+        m_reexport_info.Clear();
+    }
+    // Proper destruction is handled by Symbol's dtor; supply a no-op
+    // impl to let the compiler know it's handled.
+    ~AddrRangeOrReExport() {}
+
+  private:
+    union {
+      // Contains the value, or the section offset address when the value is an
+      // address in a section, and the size (if known).  For non-re-export
+      // symbols.
+      AddressRange m_addr_range;
+
+      // Stores re-export information if this symbol is of type
+      // eSymbolTypeReExported; no address information for these symbols.
+      ReExportInfo m_reexport_info;
+    };
+  } m_addr_or_reexport;
+
   uint32_t m_flags = 0; // A copy of the flags from the original symbol table,
                         // the ObjectFile plug-in can interpret these
 };

lldb/source/Symbol/Symtab.cpp
@@ -1227,7 +1227,7 @@ bool DecodeCStrMap(const DataExtractor &data, lldb::offset_t *offset_ptr,
 }
 
 constexpr llvm::StringLiteral kIdentifierSymbolTable("SYMB");
-constexpr uint32_t CURRENT_CACHE_VERSION = 1;
+constexpr uint32_t CURRENT_CACHE_VERSION = 2;
 
 // If the size of the Symbol object changes, the serialized
 // format likely also needs to change so the

lldb/unittests/Symbol/SymtabTest.cpp
@@ -618,68 +618,47 @@ LinkEditData:
   // This data was taken from a hex dump of the object file from the above yaml
   // and hexdumped so we can load the cache data in this test.
   const uint8_t symtab_cache_bytes[] = {
-    0x01, 0x10, 0x3e, 0x94, 0x86, 0x6e, 0x0d, 0x1a,
-    0x39, 0xbd, 0x97, 0x5b, 0x64, 0xe8, 0xf1, 0xfd,
-    0xba, 0xae, 0xff, 0x53, 0x54, 0x41, 0x42, 0x91,
-    0x00, 0x00, 0x00, 0x00, 0x2f, 0x55, 0x73, 0x65,
-    0x72, 0x73, 0x2f, 0x67, 0x63, 0x6c, 0x61, 0x79,
-    0x74, 0x6f, 0x6e, 0x2f, 0x44, 0x6f, 0x63, 0x75,
-    0x6d, 0x65, 0x6e, 0x74, 0x73, 0x2f, 0x6f, 0x62,
-    0x6a, 0x66, 0x69, 0x6c, 0x65, 0x73, 0x2f, 0x69,
-    0x6e, 0x64, 0x65, 0x78, 0x2d, 0x6e, 0x61, 0x6d,
-    0x65, 0x73, 0x2f, 0x6d, 0x61, 0x69, 0x6e, 0x2e,
-    0x63, 0x00, 0x2f, 0x55, 0x73, 0x65, 0x72, 0x73,
-    0x2f, 0x67, 0x63, 0x6c, 0x61, 0x79, 0x74, 0x6f,
-    0x6e, 0x2f, 0x44, 0x6f, 0x63, 0x75, 0x6d, 0x65,
-    0x6e, 0x74, 0x73, 0x2f, 0x6f, 0x62, 0x6a, 0x66,
-    0x69, 0x6c, 0x65, 0x73, 0x2f, 0x69, 0x6e, 0x64,
-    0x65, 0x78, 0x2d, 0x6e, 0x61, 0x6d, 0x65, 0x73,
-    0x2f, 0x6d, 0x61, 0x69, 0x6e, 0x2e, 0x6f, 0x00,
-    0x66, 0x6f, 0x6f, 0x00, 0x62, 0x61, 0x72, 0x00,
-    0x62, 0x61, 0x7a, 0x00, 0x6d, 0x61, 0x69, 0x6e,
-    0x00, 0x5f, 0x6d, 0x68, 0x5f, 0x65, 0x78, 0x65,
-    0x63, 0x75, 0x74, 0x65, 0x5f, 0x68, 0x65, 0x61,
-    0x64, 0x65, 0x72, 0x00, 0x53, 0x59, 0x4d, 0x42,
-    0x01, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x2a,
-    0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x64, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x0a, 0x20, 0x01, 0x37, 0x00, 0x00, 0x00, 0x00,
-    0x7b, 0xc3, 0x69, 0x62, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x01, 0x00, 0x66, 0x00, 0x04, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x02, 0x32, 0x01, 0x6d, 0x00, 0x00,
-    0x00, 0x01, 0x64, 0x3f, 0x00, 0x00, 0x01, 0x00,
-    0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x08, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x02, 0x32, 0x01, 0x71,
-    0x00, 0x00, 0x00, 0x01, 0x6c, 0x3f, 0x00, 0x00,
-    0x01, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0f, 0x00,
-    0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x32,
-    0x01, 0x75, 0x00, 0x00, 0x00, 0x01, 0x74, 0x3f,
-    0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x0f, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x02, 0x32, 0x01, 0x79, 0x00, 0x00, 0x00, 0x01,
-    0x7c, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-    0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x0f, 0x00, 0x14, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x04, 0x12, 0x02, 0x7e, 0x00, 0x00,
-    0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
-    0x00, 0x00, 0x64, 0x3f, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x10, 0x00, 0x0f, 0x00, 0x01, 0x00,
-    0x43, 0x4d, 0x41, 0x50, 0x07, 0x00, 0x00, 0x00,
-    0x6d, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00,
-    0x75, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00,
-    0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x79, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
-    0x37, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-    0x71, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00,
-    0x7e, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00
-  };
+      0x01, 0x10, 0x3e, 0x94, 0x86, 0x6e, 0x0d, 0x1a, 0x39, 0xbd, 0x97, 0x5b,
+      0x64, 0xe8, 0xf1, 0xfd, 0xba, 0xae, 0xff, 0x53, 0x54, 0x41, 0x42, 0x91,
+      0x00, 0x00, 0x00, 0x00, 0x2f, 0x55, 0x73, 0x65, 0x72, 0x73, 0x2f, 0x67,
+      0x63, 0x6c, 0x61, 0x79, 0x74, 0x6f, 0x6e, 0x2f, 0x44, 0x6f, 0x63, 0x75,
+      0x6d, 0x65, 0x6e, 0x74, 0x73, 0x2f, 0x6f, 0x62, 0x6a, 0x66, 0x69, 0x6c,
+      0x65, 0x73, 0x2f, 0x69, 0x6e, 0x64, 0x65, 0x78, 0x2d, 0x6e, 0x61, 0x6d,
+      0x65, 0x73, 0x2f, 0x6d, 0x61, 0x69, 0x6e, 0x2e, 0x63, 0x00, 0x2f, 0x55,
+      0x73, 0x65, 0x72, 0x73, 0x2f, 0x67, 0x63, 0x6c, 0x61, 0x79, 0x74, 0x6f,
+      0x6e, 0x2f, 0x44, 0x6f, 0x63, 0x75, 0x6d, 0x65, 0x6e, 0x74, 0x73, 0x2f,
+      0x6f, 0x62, 0x6a, 0x66, 0x69, 0x6c, 0x65, 0x73, 0x2f, 0x69, 0x6e, 0x64,
+      0x65, 0x78, 0x2d, 0x6e, 0x61, 0x6d, 0x65, 0x73, 0x2f, 0x6d, 0x61, 0x69,
+      0x6e, 0x2e, 0x6f, 0x00, 0x66, 0x6f, 0x6f, 0x00, 0x62, 0x61, 0x72, 0x00,
+      0x62, 0x61, 0x7a, 0x00, 0x6d, 0x61, 0x69, 0x6e, 0x00, 0x5f, 0x6d, 0x68,
+      0x5f, 0x65, 0x78, 0x65, 0x63, 0x75, 0x74, 0x65, 0x5f, 0x68, 0x65, 0x61,
+      0x64, 0x65, 0x72, 0x00, 0x53, 0x59, 0x4d, 0x42, 0x02, 0x00, 0x00, 0x00,
+      0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x2a,
+      0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x64, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x20, 0x01, 0x37,
+      0x00, 0x00, 0x00, 0x00, 0x7b, 0xc3, 0x69, 0x62, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x66, 0x00,
+      0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x32, 0x01, 0x6d, 0x00, 0x00,
+      0x00, 0x01, 0x64, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x08, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x02, 0x32, 0x01, 0x71, 0x00, 0x00, 0x00, 0x01,
+      0x6c, 0x3f, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x0c, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x02, 0x32, 0x01, 0x75, 0x00, 0x00, 0x00, 0x01, 0x74, 0x3f,
+      0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x02, 0x32, 0x01, 0x79, 0x00, 0x00, 0x00, 0x01, 0x7c, 0x3f, 0x00, 0x00,
+      0x01, 0x00, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x0f, 0x00, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x12,
+      0x02, 0x7e, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
+      0x00, 0x00, 0x64, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
+      0x0f, 0x00, 0x01, 0x00, 0x43, 0x4d, 0x41, 0x50, 0x07, 0x00, 0x00, 0x00,
+      0x6d, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x75, 0x00, 0x00, 0x00,
+      0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x79, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x37, 0x00, 0x00, 0x00,
+      0x01, 0x00, 0x00, 0x00, 0x71, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00,
+      0x7e, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00};
 
   ASSERT_THAT_EXPECTED(ExpectedFile, llvm::Succeeded());
   auto module_sp = std::make_shared<Module>(ExpectedFile->moduleSpec());

commit deda6eae1540f1bc8d8cf865b4abc449e13995cf

[lldb] Fix `shared_ptr<Section>` leak (#218794)
Introduced by #213356

```
SanitizerError
AddressSanitizer: 2760 byte(s) leaked in 15 allocation(s).
Details

=================================================================
==6162==ERROR: LeakSanitizer: detected memory leaks

Direct leak of 1472 byte(s) in 8 object(s) allocated from:
...
    #8 0x7f4d08980c00 in ObjectFileELF::CreateSections(lldb_private::SectionList&) lldb/source/Plugins/ObjectFile/ELF/ObjectFileELF.cpp:2168:28
    #9 0x7f4d085c5651 in lldb_private::Module::GetSectionList() lldb/source/Core/Module.cpp:1226:17
    #10 0x7f4d08af4f6f in lldb_private::plugin::dwarf::SymbolFileDWARF::SymbolFileDWARF(std::__u::shared_ptr<lldb_private::ObjectFile>, lldb_private::SectionList*) lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp:490:44
    #11 0x7f4d08af331e in lldb_private::plugin::dwarf::SymbolFileDWARF::CreateInstance(std::__u::shared_ptr<lldb_private::ObjectFile>) lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp:347:14
    #12 0x7f4d08cc36af in lldb_private::SymbolFile::FindPlugin(std::__u::shared_ptr<lldb_private::ObjectFile>) lldb/source/Symbol/SymbolFile.cpp:64:51
    #13 0x7f4d08cd2dc9 in lldb_private::SymbolVendor::AddSymbolFileRepresentation(std::__u::shared_ptr<lldb_private::ObjectFile> const&) lldb/source/Symbol/SymbolVendor.cpp:65:27
    #14 0x7f4d08cd2ba0 in lldb_private::SymbolVendor::FindPlugin(std::__u::shared_ptr<lldb_private::Module> const&, lldb_private::Stream*) lldb/source/Symbol/SymbolVendor.cpp:51:16
    #15 0x7f4d085c1d9f in lldb_private::Module::GetSymbolFile(bool, lldb_private::Stream*) lldb/source/Core/Module.cpp:991:13
    #16 0x7f50eadaf896 in lldb_private::YAMLModuleTester::YAMLModuleTester(llvm::StringRef, unsigned long) lldb/unittests/TestingSupport/Symbol/YAMLModuleTester.cpp:24:61
    #17 0x7f50eb471eea in DWARFASTParserClangYAMLTester lldb/unittests/SymbolFile/DWARF/DWARFASTParserClangTests.cpp:53:9

lldb/include/lldb/Symbol/Symbol.h
@@ -53,11 +53,7 @@ public:
 
   Symbol(const Symbol &rhs);
 
-  ~Symbol() {
-    if (m_type != lldb::eSymbolTypeReExported &&
-        m_type != lldb::eSymbolTypeInvalid)
-      m_addr_or_reexport.GetAddressRange(*this).Clear();
-  }
+  ~Symbol() { m_addr_or_reexport.Clear(*this); }
 
   const Symbol &operator=(const Symbol &rhs);
 
@@ -418,6 +414,13 @@ protected:
     // Proper destruction is handled by Symbol's dtor; supply a no-op
     // impl to let the compiler know it's handled.
     ~AddrRangeOrReExport() {}
+
+    void Clear(const Symbol &sym) {
+      if (sym.GetType() == lldb::eSymbolTypeReExported)
+        m_reexport_info.Clear();
+      else
+        m_addr_range.Clear();
+    }
 
   private:
     union {