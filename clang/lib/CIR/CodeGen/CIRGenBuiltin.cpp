nekoshirro/Alchemist-LLVM

commit fe77cb0050130cbfe970f6857f29d41be606247f

[CIR] Implement elementwise min/max/minimum/maximum/minnum/maxnum/etc (#222481)
These ended up being simple translations to llvm intrinsics, so other
than a single small 'magic decoder ring', this is a very trivial
implementation. The tests are taken from classic-codegen with some mild
modifications (as vector types don't work in arguments yet, and we are
testing more appropriately, as those were generated check lines).

This was found in an application we're trying to build.
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

clang/include/clang/CIR/Dialect/IR/CIROps.td
@@ -844,21 +844,6 @@ def CIR_LoadOp : CIR_Op<"load", [
     $addr `:` qualified(type($addr)) `,` type($result) attr-dict
   }];
 
-  let builders = [
-    OpBuilder<(ins "mlir::Value":$addr), [{
-      build($_builder, $_state, addr, /*isDeref=*/false,
-            /*is_volatile=*/false, /*is_nontemporal=*/false,
-            /*alignment=*/mlir::IntegerAttr{}, cir::SyncScopeKindAttr{},
-            cir::MemOrderAttr{}, /*invariant=*/false);
-    }]>,
-    OpBuilder<(ins "mlir::Type":$result, "mlir::Value":$addr), [{
-      build($_builder, $_state, result, addr, /*isDeref=*/false,
-            /*is_volatile=*/false, /*is_nontemporal=*/false,
-            /*alignment=*/mlir::IntegerAttr{}, cir::SyncScopeKindAttr{},
-            cir::MemOrderAttr{}, /*invariant=*/false);
-    }]>
-  ];
-
   // FIXME: add verifier.
 }
 

clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -567,7 +567,8 @@ static RValue emitUnaryMaybeConstrainedFPBuiltin(CIRGenFunction &cgf,
 template <class Operation>
 static RValue emitUnaryFPBuiltin(CIRGenFunction &cgf, const CallExpr &e) {
   mlir::Value arg = cgf.emitScalarExpr(e.getArg(0));
-  auto call = Operation::create(cgf.getBuilder(), arg.getLoc(), arg);
+  auto call =
+      Operation::create(cgf.getBuilder(), arg.getLoc(), arg.getType(), arg);
   return RValue::get(call->getResult(0));
 }
 
@@ -2104,12 +2105,39 @@ RValue CIRGenFunction::emitBuiltinExpr(const GlobalDecl &gd, unsigned builtinID,
                                 cir::OverflowBehavior::Saturated);
     return RValue::get(val);
   }
-  case Builtin::BI__builtin_elementwise_max:
-  case Builtin::BI__builtin_elementwise_min:
+  case Builtin::BI__builtin_elementwise_max: {
+    if (cir::isIntOrVectorOfIntType(convertType(e->getArg(0)->getType()))) {
+      mlir::Location loc = getLoc(e->getExprLoc());
+      mlir::Value op0 = emitScalarExpr(e->getArg(0));
+      mlir::Value op1 = emitScalarExpr(e->getArg(1));
+      return RValue::get(builder.createMax(loc, op0, op1));
+    }
+    return RValue::get(
+        emitBinaryMaybeConstrainedFPBuiltin<cir::FMaxNumOp>(*this, *e));
+  }
+  case Builtin::BI__builtin_elementwise_min: {
+    if (cir::isIntOrVectorOfIntType(convertType(e->getArg(0)->getType()))) {
+      mlir::Location loc = getLoc(e->getExprLoc());
+      mlir::Value op0 = emitScalarExpr(e->getArg(0));
+      mlir::Value op1 = emitScalarExpr(e->getArg(1));
+      return RValue::get(builder.createMin(loc, op0, op1));
+    }
+    return RValue::get(
+        emitBinaryMaybeConstrainedFPBuiltin<cir::FMinNumOp>(*this, *e));
+  }
   case Builtin::BI__builtin_elementwise_maxnum:
+    return RValue::get(
+        emitBinaryMaybeConstrainedFPBuiltin<cir::FMaxNumOp>(*this, *e));
   case Builtin::BI__builtin_elementwise_minnum:
+    return RValue::get(
+        emitBinaryMaybeConstrainedFPBuiltin<cir::FMinNumOp>(*this, *e));
   case Builtin::BI__builtin_elementwise_maximum:
+    return RValue::get(
+        emitBinaryMaybeConstrainedFPBuiltin<cir::FMaximumOp>(*this, *e));
   case Builtin::BI__builtin_elementwise_minimum:
+    return RValue::get(
+        emitBinaryMaybeConstrainedFPBuiltin<cir::FMinimumOp>(*this, *e));
+
   case Builtin::BI__builtin_elementwise_maximumnum:
   case Builtin::BI__builtin_elementwise_minimumnum:
   case Builtin::BI__builtin_reduce_max:

clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -571,9 +571,7 @@ mlir::LogicalResult lowerConstrainableFPOp(
     return op->emitError("expected LLVM result type for floating-point op");
 
   if (!fenv) {
-    rewriter.replaceOpWithNewOp<LLVMOp>(
-        op, mlir::TypeRange{llvmResTy}, operands,
-        cir::getDefaultProperties<LLVMOp>(op->getContext()));
+    rewriter.replaceOpWithNewOp<LLVMOp>(op, llvmResTy, operands);
     return mlir::success();
   }
 

clang/include/clang/CIR/Dialect/Builder/CIRBaseBuilder.h
@@ -892,6 +892,10 @@ public:
     return cir::MaxOp::create(*this, loc, lhs, rhs);
   }
 
+  mlir::Value createMin(mlir::Location loc, mlir::Value lhs, mlir::Value rhs) {
+    return cir::MinOp::create(*this, loc, lhs, rhs);
+  }
+
   cir::CmpOp createCompare(mlir::Location loc, cir::CmpOpKind kind,
                            mlir::Value lhs, mlir::Value rhs) {
     cir::FenvAttr fenv;

clang/test/CIR/CodeGenBuiltins/builtin-maxnum-minnum.c
@@ -1,0 +1,108 @@
+// RUN: %clang_cc1 -x c++ -std=c++20 -disable-llvm-passes -O3 -triple x86_64 %s -emit-cir -o %t.cir
+// RUN: FileCheck %s --input-file=%t.cir --check-prefix=CIR
+// RUN: %clang_cc1 -x c++ -std=c++20 -disable-llvm-passes -O3 -triple x86_64 %s -emit-llvm -fclangir -o %t-cir.ll
+// RUN: FileCheck %s --input-file=%t-cir.ll --check-prefix=LLVM
+// RUN: %clang_cc1 -x c++ -std=c++20 -disable-llvm-passes -O3 -triple x86_64 %s -emit-llvm -o %t-ogcg.ll
+// RUN: FileCheck %s --input-file=%t-ogcg.ll --check-prefix=LLVM
+
+typedef _Float16 half8 __attribute__((ext_vector_type(8)));
+typedef __bf16 bf16x8 __attribute__((ext_vector_type(8)));
+typedef float float4 __attribute__((ext_vector_type(4)));
+typedef double double2 __attribute__((ext_vector_type(2)));
+typedef long double ldouble2 __attribute__((ext_vector_type(2)));
+
+// CIR-LABEL: @_Z7pfmin16Dv8_DF16_S_(
+// CIR:          cir.fminnum %{{.*}}, %{{.*}} : !cir.vector<8 x !cir.f16>
+//
+// LLVM-LABEL: @_Z7pfmin16Dv8_DF16_S_(
+// FIXME(cir): Classic codegen doesn't emit a nsz for all of these.
+// LLVM:         call {{.*}}<8 x half> @llvm.minnum.v8f16(<8 x half> %{{.*}}, <8 x half> %{{.*}})
+//
+half8 pfmin16(half8 a, half8 b) {
+	return __builtin_elementwise_minnum(a, b);
+}
+// CIR-LABEL: @_Z8pfmin16bDv8_DF16bS_(
+// CIR:          cir.fminnum %{{.*}}, %{{.*}} : !cir.vector<8 x !cir.bf16>
+//
+// LLVM-LABEL: @_Z8pfmin16bDv8_DF16bS_(
+// LLVM:         call {{.*}}<8 x bfloat> @llvm.minnum.v8bf16(<8 x bfloat> %{{.*}}, <8 x bfloat> %{{.*}})
+//
+bf16x8 pfmin16b(bf16x8 a, bf16x8 b) {
+	return __builtin_elementwise_minnum(a, b);
+}
+// CIR-LABEL: @_Z7pfmin32Dv4_fS_(
+// CIR:          cir.fminnum %{{.*}}, %{{.*}} : !cir.vector<4 x !cir.float>
+//
+// LLVM-LABEL: @_Z7pfmin32Dv4_fS_(
+// LLVM:         call {{.*}}<4 x float> @llvm.minnum.v4f32(<4 x float> %{{.*}}, <4 x float> %{{.*}})
+//
+float4 pfmin32(float4 a, float4 b) {
+	return __builtin_elementwise_minnum(a, b);
+}
+// CIR-LABEL: @_Z7pfmin64Dv2_dS_(
+// CIR:          cir.fminnum %{{.*}}, %{{.*}} : !cir.vector<2 x !cir.double>
+//
+// LLVM-LABEL: @_Z7pfmin64Dv2_dS_(
+// LLVM:         call {{.*}}<2 x double> @llvm.minnum.v2f64(<2 x double> %{{.*}}, <2 x double> %{{.*}})
+//
+double2 pfmin64(double2 a, double2 b) {
+	return __builtin_elementwise_minnum(a, b);
+}
+// CIR-LABEL: @_Z7pfmin80v(
+// CIR:          cir.fminnum %{{.*}}, %{{.*}} : !cir.vector<2 x !cir.long_double<!cir.f80>>
+//
+// LLVM-LABEL: @_Z7pfmin80v(
+// LLVM:         call {{.*}}<2 x x86_fp80> @llvm.minnum.v2f80(<2 x x86_fp80> %{{.*}}, <2 x x86_fp80> %{{.*}})
+//
+void pfmin80() {
+  ldouble2 a, b;
+	ldouble2 c = __builtin_elementwise_minnum(a, b);
+}
+
+// CIR-LABEL: @_Z7pfmax16Dv8_DF16_S_(
+// CIR:          cir.fmaxnum %{{.*}}, %{{.*}} : !cir.vector<8 x !cir.f16>
+//
+// LLVM-LABEL: @_Z7pfmax16Dv8_DF16_S_(
+// LLVM:         call {{.*}}<8 x half> @llvm.maxnum.v8f16(<8 x half> %{{.*}}, <8 x half> %{{.*}})
+//
+half8 pfmax16(half8 a, half8 b) {
+	return __builtin_elementwise_maxnum(a, b);
+}
+// CIR-LABEL: @_Z8pfmax16bDv8_DF16bS_(
+// CIR:          cir.fmaxnum %{{.*}}, %{{.*}} : !cir.vector<8 x !cir.bf16>
+//
+// LLVM-LABEL: @_Z8pfmax16bDv8_DF16bS_(
+// LLVM:         call {{.*}}<8 x bfloat> @llvm.maxnum.v8bf16(<8 x bfloat> %{{.*}}, <8 x bfloat> %{{.*}})
+//
+bf16x8 pfmax16b(bf16x8 a, bf16x8 b) {
+	return __builtin_elementwise_maxnum(a, b);
+}
+// CIR-LABEL: @_Z7pfmax32Dv4_fS_(
+// CIR:          cir.fmaxnum %{{.*}}, %{{.*}} : !cir.vector<4 x !cir.float>
+//
+// LLVM-LABEL: @_Z7pfmax32Dv4_fS_(
+// LLVM:         call {{.*}}<4 x float> @llvm.maxnum.v4f32(<4 x float> %{{.*}}, <4 x float> %{{.*}})
+//
+float4 pfmax32(float4 a, float4 b) {
+	return __builtin_elementwise_maxnum(a, b);
+}
+// CIR-LABEL: @_Z7pfmax64Dv2_dS_(
+// CIR:          cir.fmaxnum %{{.*}}, %{{.*}} : !cir.vector<2 x !cir.double>
+//
+// LLVM-LABEL: @_Z7pfmax64Dv2_dS_(
+// LLVM:         call {{.*}}<2 x double> @llvm.maxnum.v2f64(<2 x double> %{{.*}}, <2 x double> %{{.*}})
+//
+double2 pfmax64(double2 a, double2 b) {
+	return __builtin_elementwise_maxnum(a, b);
+}
+
+// CIR-LABEL: @_Z7pfmax80v(
+// CIR:          cir.fminnum %{{.*}}, %{{.*}} : !cir.vector<2 x !cir.long_double<!cir.f80>>
+//
+// LLVM-LABEL: @_Z7pfmax80v(
+// LLVM:         call {{.*}}<2 x x86_fp80> @llvm.minnum.v2f80(<2 x x86_fp80> %{{.*}}, <2 x x86_fp80> %{{.*}})
+//
+void pfmax80() {
+  ldouble2 a, b;
+	ldouble2 c = __builtin_elementwise_minnum(a, b);
+}

commit e7566584fde73982a42f8ef870f9dac0b1de7243

[CIR][NFC] Restore LoadOp builders, LowerToLLVM, and CIRGenBuiltin fixes lost in #222481 (#223896)
Commit fe77cb005013 (#222481) accidentally reverted parts of
07f5eed94e34 (#222822) in `CIROps.td`, `LowerToLLVM.cpp`, and
`CIRGenBuiltin.cpp`, which broke Linux CI with deprecation and template
instantiation errors.

This change restores those lost changes:
1. Re-adds `LoadOp` custom builders in `CIROps.td` so calls to
`cir::LoadOp::create(builder, loc, addr)` and
`cir::LoadOp::create(builder, loc, resultTy, addr)` do not resolve to
deprecated MLIR collective builders.
2. Restores `lowerConstrainableFPOp` in `LowerToLLVM.cpp` to pass
default properties and type range to `replaceOpWithNewOp`.
3. Restores `emitUnaryFPBuiltin` in `CIRGenBuiltin.cpp` to use
non-deprecated `Operation::create(cgf.getBuilder(), arg.getLoc(), arg)`.

Diagnosed by Antigravity, formatted with Gemini
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

clang/include/clang/CIR/Dialect/IR/CIROps.td
@@ -844,6 +844,21 @@ def CIR_LoadOp : CIR_Op<"load", [
     $addr `:` qualified(type($addr)) `,` type($result) attr-dict
   }];
 
+  let builders = [
+    OpBuilder<(ins "mlir::Value":$addr), [{
+      build($_builder, $_state, addr, /*isDeref=*/false,
+            /*is_volatile=*/false, /*is_nontemporal=*/false,
+            /*alignment=*/mlir::IntegerAttr{}, cir::SyncScopeKindAttr{},
+            cir::MemOrderAttr{}, /*invariant=*/false);
+    }]>,
+    OpBuilder<(ins "mlir::Type":$result, "mlir::Value":$addr), [{
+      build($_builder, $_state, result, addr, /*isDeref=*/false,
+            /*is_volatile=*/false, /*is_nontemporal=*/false,
+            /*alignment=*/mlir::IntegerAttr{}, cir::SyncScopeKindAttr{},
+            cir::MemOrderAttr{}, /*invariant=*/false);
+    }]>
+  ];
+
   // FIXME: add verifier.
 }
 

clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -567,8 +567,7 @@ static RValue emitUnaryMaybeConstrainedFPBuiltin(CIRGenFunction &cgf,
 template <class Operation>
 static RValue emitUnaryFPBuiltin(CIRGenFunction &cgf, const CallExpr &e) {
   mlir::Value arg = cgf.emitScalarExpr(e.getArg(0));
-  auto call =
-      Operation::create(cgf.getBuilder(), arg.getLoc(), arg.getType(), arg);
+  auto call = Operation::create(cgf.getBuilder(), arg.getLoc(), arg);
   return RValue::get(call->getResult(0));
 }
 

clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -571,7 +571,9 @@ mlir::LogicalResult lowerConstrainableFPOp(
     return op->emitError("expected LLVM result type for floating-point op");
 
   if (!fenv) {
-    rewriter.replaceOpWithNewOp<LLVMOp>(op, llvmResTy, operands);
+    rewriter.replaceOpWithNewOp<LLVMOp>(
+        op, mlir::TypeRange{llvmResTy}, operands,
+        cir::getDefaultProperties<LLVMOp>(op->getContext()));
     return mlir::success();
   }
 