nekoshirro/Alchemist-LLVM

commit da9dab0c336a3db555591ad9fb960f9641b1906c

[GOFFObjectFile] replace snprintf with formatv (#224435)
Use formatv instead of snprintf
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Object/GOFFObjectFile.cpp
@@ -896,9 +896,8 @@ uint64_t GOFFObjectFile::getRelocationType(DataRefImpl Rel) const {
 void GOFFObjectFile::getRelocationTypeName(
     DataRefImpl Rel, SmallVectorImpl<char> &Result) const {
   uint64_t RelType = getRelocationType(Rel);
-  char Buf[16];
-  snprintf(Buf, sizeof(Buf), "R_%08lx", RelType);
-  Result.append(Buf, Buf + strlen(Buf));
+  std::string HexStr = llvm::formatv("R_{0:x-8}", RelType).str();
+  Result.append(HexStr.begin(), HexStr.end());
 }
 
 relocation_iterator GOFFObjectFile::section_rel_begin(DataRefImpl Sec) const {

commit 1165981b74cdec3e7bc8d56b2cb1a817e4d105f0

[GOFF] Add missing include (#224441)
Caused by #224435
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/lib/Object/GOFFObjectFile.cpp
@@ -16,6 +16,7 @@
 #include "llvm/Support/DataExtractor.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/Errc.h"
+#include "llvm/Support/FormatVariadic.h"
 #include "llvm/Support/raw_ostream.h"
 
 #ifndef DEBUG_TYPE