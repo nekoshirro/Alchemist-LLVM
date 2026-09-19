nekoshirro/Alchemist-LLVM

commit 795c5db75cf7ec2ea4ed2201f18e2ffa44f7e84e

[lldb][Process/FreeBSDKernelCore] Set kernel displacement (#183975)
Use `kvm_kerndisp()` on core load to retrieve the kernel displacement,
that is the difference between the kernel's
base virtual address at run time and the kernel base virtual address
specified in the kernel image file. Currently PowerPC is the only
architecture supporting kernel displacement.

---------

Signed-off-by: Minsoo Choo <minsoochoo0122@proton.me>

lldb/source/Plugins/Process/FreeBSD-Kernel-Core/ProcessFreeBSDKernelCore.cpp
@@ -114,6 +114,8 @@ bool ProcessFreeBSDKernelCore::CanDebug(lldb::TargetSP target_sp,
 
 Status ProcessFreeBSDKernelCore::DoLoadCore() {
   // The core is already loaded by CreateInstance().
+  SetKernelDisplacement();
+
   return Status();
 }
 
@@ -363,6 +365,29 @@ lldb::addr_t ProcessFreeBSDKernelCore::FindSymbol(const char *name) {
   ModuleSP mod_sp = GetTarget().GetExecutableModule();
   const Symbol *sym = mod_sp->FindFirstSymbolWithNameAndType(ConstString(name));
   return sym ? sym->GetLoadAddress(&GetTarget()) : LLDB_INVALID_ADDRESS;
+}
+
+void ProcessFreeBSDKernelCore::SetKernelDisplacement() {
+  kssize_t displacement = kvm_kerndisp(m_kvm);
+
+  if (displacement == 0)
+    return;
+
+  Target &target = GetTarget();
+  lldb::ModuleSP kernel_module_sp = target.GetExecutableModule();
+  if (!kernel_module_sp)
+    return;
+
+  bool changed = false;
+  kernel_module_sp->SetLoadAddress(target,
+                                   static_cast<lldb::addr_t>(displacement),
+                                   /*value_is_offset=*/true, changed);
+
+  if (changed) {
+    ModuleList loaded_module_list;
+    loaded_module_list.Append(kernel_module_sp);
+    target.ModulesDidLoad(loaded_module_list);
+  }
 }
 
 void ProcessFreeBSDKernelCore::PrintUnreadMessage() {

lldb/source/Plugins/Process/FreeBSD-Kernel-Core/ProcessFreeBSDKernelCore.h
@@ -64,6 +64,8 @@ protected:
   lldb::addr_t FindSymbol(const char *name);
 
 private:
+  void SetKernelDisplacement();
+
   void PrintUnreadMessage();
 
   const char *GetError();

commit da610f3067b76a742c09de522d26fc9d0055a5d5

[lldb][FreeBSDKernel] Load unrelocated FreeBSD kernel sections (#223589)
Even though displacement is zero, we need to mark the module as loaded.

Fixes: 795c5db75cf7ec2ea4ed2201f18e2ffa44f7e84e
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

lldb/source/Plugins/Process/FreeBSD-Kernel-Core/ProcessFreeBSDKernelCore.cpp
@@ -554,9 +554,6 @@ lldb::addr_t ProcessFreeBSDKernelCore::FindSymbol(const char *name) {
 void ProcessFreeBSDKernelCore::SetKernelDisplacement() {
   kssize_t displacement = kvm_kerndisp(m_kvm);
 
-  if (displacement == 0)
-    return;
-
   Target &target = GetTarget();
   lldb::ModuleSP kernel_module_sp = target.GetExecutableModule();
   if (!kernel_module_sp)